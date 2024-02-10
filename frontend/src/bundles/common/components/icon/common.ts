0ae4209d3786d9b7b78cb644266691e7e8f53e58
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-02-10 17:12:55 +0100

    rg-3: + added icons from fortawesome

---
+++ frontend/src/bundles/common/components/icon/common.ts
@@ -0,0 +1,20 @@
+import {
+    faPlus,
+    faChevronDown,
+} from '@fortawesome/free-solid-svg-icons';
+import { 
+    faFacebookF, 
+    faGoogle 
+} from '@fortawesome/free-brands-svg-icons';
+
+import { IconName } from '~/bundles/common/enums/enums.js';
+  
+  const iconNameToSvgIcon = {
+    [IconName.PLUS]: faPlus,
+    [IconName.CHEVRON_DOWN]: faChevronDown,
+    [IconName.FACEBOOK]: faFacebookF,
+    [IconName.GOOGLE]: faGoogle
+  };
+  
+  export { iconNameToSvgIcon };
+  
