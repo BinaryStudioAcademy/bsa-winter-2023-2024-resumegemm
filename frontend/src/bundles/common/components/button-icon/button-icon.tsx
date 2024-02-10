4461ca0afe6bed488eb54c12dd5759fe29e2d237
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-02-10 17:14:06 +0100

    rg-3: + added icon-button component

---
+++ frontend/src/bundles/common/components/button-icon/button-icon.tsx
@@ -0,0 +1,25 @@
+import { IconName } from '~/bundles/common/enums/enums';
+import { type ValueOf } from '~/bundles/common/types/types';
+
+import { Icon } from '../icon/icon';
+import styles from './styles.module.scss';
+
+
+type IconButtonProperties = {
+  iconName: ValueOf<typeof IconName>;
+  label?: string | number;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+};
+
+const IconButton: React.FC<IconButtonProperties> = ({
+  iconName,
+  label = '',
+  onClick
+}) => (
+  <button className={styles['iconButton']} type="button" onClick={onClick}>
+    <Icon name={iconName} />
+    {label}
+  </button>
+);
+
+export { IconButton };
