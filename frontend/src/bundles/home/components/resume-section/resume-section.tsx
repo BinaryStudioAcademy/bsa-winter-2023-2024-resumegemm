f8c7cc8eb7a323a081515db9c6e4e338d0016df5
Nazarii <hoolyworkacc@gmail.com>
2024-02-12 22:31:27 +0200

    rg-11: * resume section to use panel container

--- frontend/src/bundles/home/components/resume-section/resume-section.tsx
+++ frontend/src/bundles/home/components/resume-section/resume-section.tsx
@@ -1,22 +1,19 @@
-import mockResume from '~/assets/img/mockResume.png';
-
-import { CreateNewCard } from './components/create-new-card/create-new-card';
-import { ResumeCard } from './components/resume-card/resume-card';
+import { PanelContainer } from '../panel-container/panel-container';
 import styles from './styles.module.scss';
 
-const ResumeSection: React.FC = () => {
+type Properties = {
+    children?: React.ReactNode;
+    name: string;
+};
+
+const ResumeSection: React.FC<Properties> = ({
+    children,
+    name,
+}: Properties) => {
     return (
-        <div className={styles.resumeSection}>
-            <div className={styles.resumeSection__topBar}>Zaza</div>
-            <div className={styles.resumeSection__content}>
-                <ResumeCard
-                    title="My Resume"
-                    subtitle="Updated - Jan 25"
-                    image={mockResume}
-                />
-                <CreateNewCard />
-            </div>
-        </div>
+        <PanelContainer name={name} classname={styles.resumeSection}>
+            <div className={styles.resumeSection__cards}>{children}</div>
+        </PanelContainer>
     );
 };
 
