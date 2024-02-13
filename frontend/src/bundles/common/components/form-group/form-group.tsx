a3498bfbb9965696a3bc9e2ce503c5f5322d13fe
Valentyn13 <valentyn13git@gmail.com>
2024-02-13 15:21:35 +0200

    rg-4: + FormGroup component with styles

---
+++ frontend/src/bundles/common/components/form-group/form-group.tsx
@@ -0,0 +1,31 @@
+import { type ReactNode } from 'react';
+import { type FieldError } from 'react-hook-form';
+
+import styles from './styles.module.scss';
+
+type Properties = {
+    width?: string;
+    error: FieldError | undefined;
+    label: string;
+    children: ReactNode;
+};
+
+const FormGroup = ({
+    error,
+    label,
+    children,
+    width = 'auto',
+}: Properties): JSX.Element => {
+    const errorMessage = error?.message;
+    return (
+        <label className={styles.label} style={{ width }}>
+            <span className={styles.label__name}>{label}</span>
+            {children}
+            {errorMessage && (
+                <span className={styles.error}>{errorMessage}</span>
+            )}
+        </label>
+    );
+};
+
+export { FormGroup };
