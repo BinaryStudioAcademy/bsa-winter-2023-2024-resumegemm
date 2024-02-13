bf7576b0d7083a69f0ecbbffb3efdde89f1cabb2
Vladyslav Lazarenko <ezio2506@gmail.com>
2024-02-13 20:42:07 +0200

    rg-18: * changed own implementation to react library, removed hook form

--- frontend/src/bundles/common/components/dropdown/dropdown.tsx
+++ frontend/src/bundles/common/components/dropdown/dropdown.tsx
@@ -1,81 +1,51 @@
-import { useCallback, useState } from 'react';
-import {
-    type Control,
-    type FieldPath,
-    type FieldValues,
-} from 'react-hook-form';
+import clsx from 'clsx';
+import { useCallback } from 'react';
+import Select from 'react-select';
 
-import { useFormController } from '../../hooks/hooks';
-import { Option } from './option/option';
 import styles from './styles.module.scss';
 
-type Properties<T extends FieldValues> = {
+type Option = {
+    value: string;
     label: string;
-    control: Control<T, null>;
-    name: FieldPath<T>;
-    defaultOption: string;
-    options: {
-        value: string;
-        label: string;
-    }[];
 };
 
-const Dropdown = <T extends FieldValues>({
-    control,
+type Properties = {
+    name: string;
+    onChange: (value: string | undefined) => void;
+    options: Option[];
+    label?: string;
+    isDisabled?: boolean;
+    placeholder?: string;
+    className?: string;
+};
+
+const Dropdown: React.FC<Properties> = ({
     name,
     label,
-    defaultOption,
+    onChange,
+    className,
+    placeholder,
+    isDisabled,
     options,
-}: Properties<T>): JSX.Element => {
-    const { field } = useFormController({
-        name,
-        control,
-    });
-
-    const { onChange } = field;
-
-    const [active, setActive] = useState(false);
-    const [selected, setSelected] = useState(defaultOption);
-
-    const dropdownClickHandler = useCallback(() => {
-        setActive((previousActive) => !previousActive);
-    }, []);
-
+}) => {
     const optionChangeHandler = useCallback(
-        ({ value, label }: { value: string; label: string }) => {
-            setSelected(label);
-            onChange(value);
-            setActive(false);
+        (option: Option | null) => {
+            onChange(option?.value);
         },
         [onChange],
     );
 
     return (
-        <div className={styles.dropdown_container}>
-            <span className={styles.label}>{label}</span>
-            <div
-                className={`${styles.dropdown}  ${active ? styles.active : ''}`}
-            >
-                <button
-                    type="button"
-                    className={styles.dropdown_button}
-                    onClick={dropdownClickHandler}
-                >
-                    {selected}
-                </button>
-                {active && (
-                    <ul className={styles.dropdown_list}>
-                        {options.map(({ value, label }, index) => (
-                            <Option
-                                key={index}
-                                onClick={optionChangeHandler}
-                                value={value}
-                                label={label}
-                            />
-                        ))}
-                    </ul>
-                )}
-            </div>
+        <div>
+            <span className={styles.dropdown_label}>{label}</span>
+            <Select
+                className={clsx(styles.dropdown, className)}
+                placeholder={placeholder}
+                name={name}
+                options={options}
+                isDisabled={isDisabled}
+                onChange={optionChangeHandler}
+            />
         </div>
     );
 };
