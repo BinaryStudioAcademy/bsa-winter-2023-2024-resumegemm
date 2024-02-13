9c22acafc589edbbcf090619547ff5e514d6fa49
RevolveStrm <revolvebattlerage@gmail.com>
2024-02-13 09:12:45 +0200

    rg-13: + progress component

--- frontend/src/bundles/common/components/progress/progress.tsx
+++ frontend/src/bundles/common/components/progress/progress.tsx
@@ -1,27 +1,20 @@
 import styles from './styles.module.scss';
 
 type Properties = {
-    fillPercent: number;
+    progressPercentage: number;
 };
 
-const Progress: React.FC<Properties> = ({ fillPercent }) => {
-    // sets valid fill percent in range between 0 and 100
-    const validFillPercent: number = Math.min(100, Math.max(0, fillPercent));
-    // removes right side border radius when a fill percent lower than 95%
-    const innerFillBorderRadius: string =
-        validFillPercent > 95 ? '16px' : '16px 0 0 16px';
-    // extra styling for filling element
-    const innerFillStyle = {
-        width: `${validFillPercent}%`,
-        borderRadius: innerFillBorderRadius,
+const Progress: React.FC<Properties> = ({ progressPercentage }) => {
+    const innerBarStyle = {
+        width: `${progressPercentage}%`
     };
 
     return (
         <div className={styles.progress}>
-            <div className={styles.progress__outerFill}>
-                <div className={styles.progress__innerFill} style={{ ...innerFillStyle }}></div>
+            <div className={styles.progress__outerBar}>
+                <div className={styles.progress__innerBar} style={{ ...innerBarStyle }}></div>
             </div>
-            <div className={styles.progress__percent}>{validFillPercent}%</div>
+            <div className={styles.progress__percentage}>{progressPercentage}%</div>
         </div>
     );
 };
