4cb933eac13306201496c47e7eeb50703f6daa8d
Vladyslav Lazarenko <ezio2506@gmail.com>
2024-02-14 13:55:55 +0200

    rg-10: - removed Layout component

--- frontend/src/bundles/common/components/components.ts
+++ frontend/src/bundles/common/components/components.ts
@@ -1,7 +1,7 @@
 export { App } from '../../../app/app.js';
 export { Button } from './button/button.js';
 export { Input } from './input/input.js';
-export { Layout } from './layout/layout.js';
+export { Footer, Header } from './layout/layout.js';
 export { Link } from './link/link.js';
 export { RouterProvider } from './router-provider/router-provider.js';
 export { Provider as StoreProvider } from 'react-redux';
