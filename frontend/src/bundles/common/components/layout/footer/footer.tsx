2f15bbe9f7064cda5e66596fff9353a9c8365459
Vladyslav Lazarenko <ezio2506@gmail.com>
2024-02-14 16:21:42 +0200

    rg-10: - removed unnecessary element

--- frontend/src/bundles/common/components/layout/footer/footer.tsx
+++ frontend/src/bundles/common/components/layout/footer/footer.tsx
@@ -7,7 +7,7 @@ type Properties = {
 const Footer: React.FC<Properties> = ({ children }) => {
     return (
         <footer className={styles.footer}>
-            <div className={styles.footer__controls}>{children}</div>
+           {children}
         </footer>
     );
 };
