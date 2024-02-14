0ce812a995076db6ebc7bc957bbadd79fe7b8bdb
Icerock10 <dp021190dms@gmail.com>
2024-02-14 02:51:17 +0200

    rg-24: * Updated modal properties type

--- frontend/src/bundles/common/types/modal/modal-properties.type.ts
+++ frontend/src/bundles/common/types/modal/modal-properties.type.ts
@@ -1,9 +1,13 @@
 import { type ReactNode } from 'react';
 
+import { type ModalVariant } from '~/bundles/common/enums/enums.js';
+
 type ModalProperties = {
     isOpen: boolean;
     onClose: () => void;
     children: ReactNode;
+    title?: string;
+    variant: ModalVariant;
 };
 
 export { type ModalProperties };
