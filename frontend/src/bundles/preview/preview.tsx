b4abbe2688dcd232bda64085ca79e25a9d611291
Nazarii <hoolyworkacc@gmail.com>
2024-02-15 00:23:34 +0200

    rg-19: * fix svg

--- frontend/src/bundles/preview/preview.tsx
+++ frontend/src/bundles/preview/preview.tsx
@@ -1,3 +1,4 @@
+import { Checkbox, RadioButton, Switch } from '../common/components/components';
 import styles from './styles.module.scss';
 
 const PreviewPage: React.FC = () => {
@@ -6,6 +7,11 @@ const PreviewPage: React.FC = () => {
             <ul className={styles.list}>
                 <li className={styles.item}>
                     <div className={styles.example}>Component preview</div>
+                </li>
+                <li className={styles.item}>
+                    <Checkbox label="Checkbox" />
+                    <RadioButton label="Radio button" />
+                    <Switch label="Switch" />
                 </li>
             </ul>
         </div>
