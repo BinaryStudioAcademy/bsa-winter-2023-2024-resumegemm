742e4f714a5e79274d657f4f33ede233f6e44c79
Vladyslav Lazarenko <ezio2506@gmail.com>
2024-02-14 13:58:55 +0200

    rg-10: * made header more reusable

--- frontend/src/bundles/common/components/layout/header/header.tsx
+++ frontend/src/bundles/common/components/layout/header/header.tsx
@@ -2,22 +2,19 @@ import { Link } from 'react-router-dom';
 
 import logo from '~/assets/img/logo.svg';
 
-import { Tab } from '../../tabs/tab';
-import { TabItem } from '../../tabs/tab-item';
 import styles from './styles.module.scss';
-import { UserProfile } from './user-profile/user-profile';
 
-const Header: React.FC = () => {
+type Properties = {
+    children?: React.ReactNode;
+};
+
+const Header: React.FC<Properties> = ({ children }) => {
     return (
         <header className={styles.header}>
             <Link className={styles.logo} to="/">
                 <img className={styles.logo__icon} src={logo} alt="Logo icon" />
             </Link>
-            <Tab>
-                <TabItem title="Home">{''}</TabItem>
-                <TabItem title="Templates">{''}</TabItem>
-            </Tab>
-            <UserProfile src="" />
+            {children}
         </header>
     );
 };
