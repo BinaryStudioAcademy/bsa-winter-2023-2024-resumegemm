a65d1e626c6baac6eb001ec25edd20b9b3a67a5d
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-02-14 11:05:04 +0100

    rg-7: + added sign in/up components

--- frontend/src/bundles/auth/components/sign-in-form/sign-in-form.tsx
+++ frontend/src/bundles/auth/components/sign-in-form/sign-in-form.tsx
@@ -1,16 +1,84 @@
-import { Button } from '~/bundles/common/components/components.js';
+import { useCallback } from 'react';
+
+import { Button, Input } from '~/bundles/common/components/components.js';
+import {
+    ButtonSize,
+    ButtonType,
+    ButtonVariant
+} from '~/bundles/common/enums/enums';
+import { useAppForm } from '~/bundles/common/hooks/hooks';
+import {
+    type UserSignUpRequestDto,
+    userSignUpValidationSchema,
+} from '~/bundles/users/users';
+
+import { Password } from '../password/password';
+import { DEFAULT_SIGN_UP_PAYLOAD } from '../sign-up-form/constants/constants';
+import styles from './styles.module.scss';
 
 type Properties = {
     onSubmit: () => void;
 };
 
-const SignInForm: React.FC<Properties> = () => (
-    <>
-        <h1>Sign In</h1>
-        <form>
-            <Button label="Sign in" />
-        </form>
-    </>
-);
+const SignInForm: React.FC<Properties> = ({ onSubmit }) => {
+    // TODO: replace type, payload and validation for sign-in
+    const { control, errors, handleSubmit } = useAppForm<UserSignUpRequestDto>({
+        defaultValues: DEFAULT_SIGN_UP_PAYLOAD,
+        validationSchema: userSignUpValidationSchema,
+    });
+
+    const handleFormSubmit = useCallback(
+        (event_: React.BaseSyntheticEvent): void => {
+            void handleSubmit(onSubmit)(event_);
+        },
+        [handleSubmit, onSubmit],
+    );
+
+    return (
+        <>
+            <div className={styles.login__header}>
+                <h1 className={styles.login__title}>Log In</h1>
+                <p className={styles.login__message}>
+                    No account? Go to  
+                    <span className={styles.login__link}>
+                        {' '}
+                        Sign Up
+                    </span>
+                </p>
+            </div>
+            <form  
+                onSubmit={handleFormSubmit}
+                className={styles.login__form}>
+                    {/* TODO: replace input with merged one and width 100% */}
+                <Input
+                    type="text"
+                    label="Email"
+                    placeholder="Enter your email"
+                    name="email"
+                    control={control}
+                    errors={errors}
+                />
+                <div className={styles.login__form_password}>
+                    <span className={styles.forgot__link}>
+                        Forgot Password?
+                    </span>
+                    <Password 
+                        control={control} 
+                        errors={errors} 
+                        isConfirmPasswordShown={false}
+                    />
+                </div>
+                <Button
+                    size={ButtonSize.MEDIUM}
+                    isFluid={true}
+                    variant={ButtonVariant.PRIMARY}
+                    type={ButtonType.SUBMIT}
+                >
+                    Sign up
+                </Button>
+            </form>
+        </>
+    );
+};
 
 export { SignInForm };
--- frontend/src/bundles/auth/components/sign-up-form/sign-up-form.tsx
+++ frontend/src/bundles/auth/components/sign-up-form/sign-up-form.tsx
@@ -1,17 +1,25 @@
 import { Button, Input } from '~/bundles/common/components/components';
+import {
+    ButtonSize,
+    ButtonType,
+    ButtonVariant
+} from '~/bundles/common/enums/enums';
 import { useAppForm, useCallback } from '~/bundles/common/hooks/hooks';
 import {
     type UserSignUpRequestDto,
     userSignUpValidationSchema,
 } from '~/bundles/users/users';
 
+import { Password } from '../password/password';
 import { DEFAULT_SIGN_UP_PAYLOAD } from './constants/constants';
+import styles from './styles.module.scss';
 
 type Properties = {
     onSubmit: (payload: UserSignUpRequestDto) => void;
 };
 
 const SignUpForm: React.FC<Properties> = ({ onSubmit }) => {
+
     const { control, errors, handleSubmit } = useAppForm<UserSignUpRequestDto>({
         defaultValues: DEFAULT_SIGN_UP_PAYLOAD,
         validationSchema: userSignUpValidationSchema,
@@ -26,29 +34,46 @@ const SignUpForm: React.FC<Properties> = ({ onSubmit }) => {
 
     return (
         <>
-            <h3>Sign Up</h3>
-            <form onSubmit={handleFormSubmit}>
-                <p>
-                    <Input
-                        type="text"
-                        label="Email"
-                        placeholder="Enter your email"
-                        name="email"
-                        control={control}
-                        errors={errors}
-                    />
+            <div className={styles.registration__header}>
+                <h1 className={styles.registration__title}>Sign Up</h1>
+                <p className={styles.registration__message}>
+                    Already have an account? Go to
+                    <span className={styles.registration__link}>
+                        {' '}
+                        Log in
+                    </span>
                 </p>
-                <p>
-                    <Input
-                        type="text"
-                        label="Password"
-                        placeholder="Enter your password"
-                        name="password"
-                        control={control}
-                        errors={errors}
-                    />
-                </p>
-                <Button type="submit" label="Sign up" />
+            </div>
+            <form
+                className={styles.registration__form}
+                onSubmit={handleFormSubmit}
+            >
+                {/* TODO: replace input with merged one */}
+                <Input
+                    type="text"
+                    label="Name"
+                    placeholder="Enter your name"
+                    name="name"
+                    control={control}
+                    errors={errors}
+                />
+                <Input
+                    type="text"
+                    label="Email"
+                    placeholder="Enter your email"
+                    name="email"
+                    control={control}
+                    errors={errors}
+                />
+                <Password control={control} errors={errors} isConfirmPasswordShown={true} />
+                <Button
+                    size={ButtonSize.MEDIUM}
+                    isFluid={true}
+                    variant={ButtonVariant.PRIMARY}
+                    type={ButtonType.SUBMIT}
+                >
+                    Sign up
+                </Button>
             </form>
         </>
     );
