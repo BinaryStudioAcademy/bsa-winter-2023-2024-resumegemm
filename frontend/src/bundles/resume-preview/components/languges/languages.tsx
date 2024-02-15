5c35becf1bf4aad10cd66f76323e1e7ac576b0ae
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-02-15 12:26:10 +0200

    rg-14: + language component with styles

---
+++ frontend/src/bundles/resume-preview/components/languges/languages.tsx
@@ -0,0 +1,36 @@
+import {
+    type LanguagesData,
+    type LanguagesStyles,
+} from '~/bundles/resume-preview/types/types';
+
+import styles from './styles.module.scss';
+
+interface LanguagesProperties {
+    data: LanguagesData;
+    json_styles: LanguagesStyles;
+}
+
+const Languages: React.FC<LanguagesProperties> = ({ data, json_styles }) => {
+    return (
+        <div className={styles.resume_preview__section_wrapper}>
+            <div className={styles.resume_preview__aside_section_header}>
+                <h3 className={styles.section_header__title}>Languages</h3>
+            </div>
+            <ul className={styles.languages__list}>
+                {data.data.map((item, index) => (
+                    <li key={index}>
+                        <span style={json_styles.languages__title}>
+                            {item.language}
+                        </span>{' '}
+                        <span>-</span>{' '}
+                        <span style={json_styles.languages__level}>
+                            {item.level}
+                        </span>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export { Languages };
