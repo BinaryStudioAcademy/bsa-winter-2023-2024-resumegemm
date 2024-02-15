38264446460c5d6a863e941f8017ca473c04d342
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-02-15 02:17:13 +0200

    rg-14: + added file for all svg exports

---
+++ frontend/src/assets/img/resume_preview/resume-svg.ts
@@ -0,0 +1,23 @@
+import emptyCompanyLogo from './empty_company_logo.svg';
+import emptyPhoto from './empty_photo.svg';
+import emptyPortfolioImage from './empty_portfolio_image.svg';
+import envelope from './envelope.svg';
+import figma from './figma_icon.svg';
+import linkedIn from './linkedin.svg';
+import location from './location.svg';
+import notchLogo from './notch_logo.svg';
+import portfolio from './portfolio.svg';
+
+const svg = {
+    emptyCompanyLogo,
+    emptyPhoto,
+    emptyPortfolioImage,
+    envelope,
+    figma,
+    linkedIn,
+    location,
+    notchLogo,
+    portfolio,
+};
+
+export { svg };
