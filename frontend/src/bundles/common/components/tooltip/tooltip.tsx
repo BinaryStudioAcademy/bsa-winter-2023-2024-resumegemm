f1ec8a3e5b71e38153f87c5fc3aa851ff0b4a5aa
Maxik <mpoplavskiy38380@gmail.com>
2024-02-15 12:46:16 +0200

    rg-29: + add dimensions

--- frontend/src/bundles/common/components/tooltip/tooltip.tsx
+++ frontend/src/bundles/common/components/tooltip/tooltip.tsx
@@ -1,20 +1,29 @@
 import clsx from 'clsx';
 import { Tooltip as ReactTooltip } from 'react-tooltip';
 
+import { TooltipDimensions } from '../../enums/enums';
+import { type ValueOf } from '../../types/types';
 import styles from './styles.module.scss';
 
 type Properties = {
     className?: string,
-    children: React.ReactNode,
-    text: string
+    children?: React.ReactNode,
+    text: string,
+    dimensionType?: ValueOf<typeof TooltipDimensions>
+};
+
+const DimensionTypes: Record<TooltipDimensions, string> = {
+    expand100Percent: styles.tooltip__container_100_percent,
+    fitContent: styles.tooltip__container_fit_content
 };
 
 const Tooltip: React.FC<Properties> = ({
     className = '',
-    children,
-    text
+    children = <div className={styles.tooltip__image_container}></div>,
+    text,
+    dimensionType = TooltipDimensions.fitContent
 }) => {
-    return <div className={clsx(className, styles.tooltip__container)}>
+    return <div className={clsx(className, styles.tooltip__container, DimensionTypes[dimensionType])}>
         <div className={styles.tooltip__anchor_container}>
             {children}
         </div>
