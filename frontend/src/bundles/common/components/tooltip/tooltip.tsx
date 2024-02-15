ed1fb88d58faaf62c94c7ad515e34b285a28e62b
Maxik <mpoplavskiy38380@gmail.com>
2024-02-15 09:16:17 +0200

    rg-29: * make tooltip a wrapper

--- frontend/src/bundles/common/components/tooltip/tooltip.tsx
+++ frontend/src/bundles/common/components/tooltip/tooltip.tsx
@@ -5,17 +5,17 @@ import styles from './styles.module.scss';
 
 type Properties = {
     className?: string,
-    text: string
+    children: React.ReactNode
 };
 
 const Tooltip: React.FC<Properties> = ({
     className = '',
-    text
+    children
 }) => {
     return <div className={clsx(className, styles.tooltip__container)}>
         <div className={styles.tooltip__image_container}></div>
         <ReactTooltip anchorSelect={`.${styles.tooltip__image_container}`} className={styles.tooltip__popup} >
-            <p className={styles.tooltip__text}>{text}</p>
+            {children}
         </ReactTooltip>
     </div>;
 };
