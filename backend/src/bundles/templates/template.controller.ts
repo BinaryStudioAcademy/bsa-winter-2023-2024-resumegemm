322f80dd29b2cb97b2a9f65383a82153c77f3bdb
Valentyn13 <valentyn13git@gmail.com>
2024-02-15 20:35:43 +0200

    rg-31: + TemplateController and connect it to server

---
+++ backend/src/bundles/templates/template.controller.ts
@@ -0,0 +1,94 @@
+import  { type PartialModelObject } from 'objection';
+
+import  { type ApiHandlerOptions,type ApiHandlerResponse  } from '~/common/controller/controller.js';
+import { Controller } from '~/common/controller/controller.js';
+import { ApiPath } from '~/common/enums/enums.js';
+import { HttpCode } from '~/common/http/http.js';
+import  { type ILogger } from '~/common/logger/logger.js';
+
+import  { type TemplateModel } from './template.model.js';
+import  { type Template } from './types/template.type.js';
+import { TemplatesApiPath } from './types/template-api-path.enum.js';
+import  { type ITemplateService } from './types/template-service.type.js';
+
+class TemplateController extends Controller {
+    private templateService: ITemplateService<Template>;
+
+    public constructor(logger:ILogger, templateService:ITemplateService<Template>){
+        super(logger, ApiPath.TEMPLATES);
+
+        this.templateService = templateService;
+
+        this.addRoute({
+            path:TemplatesApiPath.ROOT,
+            method:'POST',
+            handler: (options) => this.create(options as ApiHandlerOptions<{ body:Template }>)
+        });
+
+        this.addRoute({
+            path:TemplatesApiPath.ID,
+            method:'GET',
+            handler:(options) => this.findById(options as ApiHandlerOptions<{ params:{ id:number } }>)
+        });
+
+        this.addRoute({
+            path:TemplatesApiPath.ROOT,
+            method:'GET',
+            handler:() => this.findAll()
+        });
+
+        this.addRoute({
+            path:TemplatesApiPath.ID,
+            method:'DELETE',
+            handler:(options) => this.delete(options as ApiHandlerOptions<{ params:{ id:number } }>)
+        });
+
+        this.addRoute({
+            path:TemplatesApiPath.ID,
+            method:'PUT',
+            handler:(options) => this.update(options as ApiHandlerOptions<{ body:PartialModelObject<TemplateModel>, params:{ id:number } }>)
+        });
+    }
+
+    private async create(options:ApiHandlerOptions<{ body:Template }>): Promise<ApiHandlerResponse>{
+        const template = await this.templateService.create(options.body);
+        return {
+            status: HttpCode.CREATED,
+            payload: template
+        };
+    }
+
+    private async findById(options:ApiHandlerOptions<{ params:{ id:number } }>):Promise<ApiHandlerResponse>{
+        const template = await this.templateService.find(options.params.id);
+        return {
+            status:HttpCode.OK,
+            payload:template
+        };
+    }
+    private async findAll():Promise<ApiHandlerResponse> {
+        const templateList = await this.templateService.findAll();
+        return {
+            status:HttpCode.OK,
+            payload:templateList
+        };
+    }
+
+    private async delete(options:ApiHandlerOptions<{ params:{ id:number } }>):Promise<ApiHandlerResponse>{
+        const isDeleted = await this.templateService.delete(options.params.id);
+        return{
+            status:HttpCode.OK,
+            payload:isDeleted
+        };
+    }
+
+    private async update(options:ApiHandlerOptions<{ body:PartialModelObject<TemplateModel>, params:{ id:number } }>):Promise<ApiHandlerResponse>{
+        const newTemplate = await this.templateService.update(options.params.id,options.body);
+        return {
+            status:HttpCode.OK,
+            payload:newTemplate
+        };
+    }
+
+}
+
+export { TemplateController };
