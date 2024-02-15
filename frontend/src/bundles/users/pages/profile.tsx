8e4b157d7d4a666ac4b39d72cfe0b168a36c6e84
ykotsiuba <jurok3x@gmail.com>
2024-02-15 15:27:18 +0200

    rg-22: * Separated profile card component

--- frontend/src/bundles/users/pages/profile.tsx
+++ frontend/src/bundles/users/pages/profile.tsx
@@ -1,6 +1,7 @@
 import { Button } from '~/bundles/common/components/components';
 import { PageTitle } from '~/bundles/common/components/page-title/page-title';
 
+import { ProfileCard } from '../components/profile-card/profile-card';
 import { ProfileForm } from '../components/profile-form/profile-form';
 import styles from './style.module.scss';
 
@@ -18,37 +19,38 @@ const Profile: React.FC = () => {
     return (
         <div className={styles.profile}>
             <div className={styles.profile__container}>
-                <PageTitle title='Account settings' />
-                <div className={styles.profile__content}>
-                    <div className={styles.profile__card__title}>
-                        <span>Your plan</span>
+                <PageTitle title="Account settings" />
+                <ProfileCard title="Your plan">
+                    <div className={styles.profile__card__icon}>
+                        <img
+                            src={
+                                'https://resume.io/assets/media/account_free_newaf14cca16ac227108062.svg'
+                            }
+                            alt="Upgrade for PDF downloads &amp; premium features."
+                        />
                     </div>
-                    <div className={styles.profile__card}>
-                        <div className={styles.profile__card__content}>
-                            <div className={styles.profile__card__icon}>
-                                <img
-                                    src={'https://resume.io/assets/media/account_free_newaf14cca16ac227108062.svg'}
-                                    alt="Upgrade for PDF downloads &amp; premium features."
-                                />
-                            </div>
-                            <div className={styles.profile__card__text}>
-                                <p className={styles.main__bold}>Free account</p>
-                                <p className={styles.main__regular}>You are on free plan. You can save your data and search for jobs. Upgrade for PDF downloads & premium features.</p>
-                            </div>
-                        </div>
-                        <div className={styles.profile__card__actions}>
-                            <Button>Upgrade</Button>
-                        </div>
+                    <div className={styles.profile__card__text}>
+                        <p className={styles.main__bold}>Free account</p>
+                        <p className={styles.main__regular}>
+                            You are on free plan. You can save your data and
+                            search for jobs. Upgrade for PDF downloads & premium
+                            features.
+                        </p>
                     </div>
-                </div>
-                <div className={styles.profile__content}>
-                    <div className={styles.profile__card__title}>
-                        <span>Account</span>
+                    <div className={styles.profile__card__actions}>
+                        <Button>Upgrade</Button>
                     </div>
-                    <div className={styles.profile__card}>
-                        <ProfileForm onSubmit={handleFormSubmit} />
-                    </div>
-                </div>
+                </ProfileCard>
+                <ProfileCard title="Account">
+                    <ProfileForm onSubmit={handleFormSubmit} />
+                </ProfileCard>
+                <ProfileCard title="Socials">bla</ProfileCard>
+                <ProfileCard title="Subsription">
+                    <ProfileForm onSubmit={handleFormSubmit} />
+                </ProfileCard>
+                <ProfileCard title="Delete account">
+                    <ProfileForm onSubmit={handleFormSubmit} />
+                </ProfileCard>
             </div>
         </div>
     );
