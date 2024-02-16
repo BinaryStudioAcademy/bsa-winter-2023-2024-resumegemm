1daa0d3efe226f95a7f5ca9626446fc23566d4a1
Icerock10 <dp021190dms@gmail.com>
2024-02-16 13:40:52 +0200

    rg-9: * generate refresh token with diff signature

--- backend/src/bundles/auth/helpers/token/generate-token/generate-token.ts
+++ backend/src/bundles/auth/helpers/token/generate-token/generate-token.ts
@@ -2,9 +2,25 @@ import jwt from 'jsonwebtoken';
 
 import { config } from '~/common/config/config.js';
 
-const generateToken = <T extends object>(data: T): string => {
-    return jwt.sign(data, config.ENV.JWT.SECRET, {
-        expiresIn: config.ENV.JWT.EXPIRES_IN,
+const generateToken = <T extends object>(
+    data: T,
+    isRefreshToken?: boolean,
+): string => {
+    const {
+        JWT: {
+            ACCESS_TOKEN_SECRET,
+            REFRESH_TOKEN_SECRET,
+            ACCESS_TOKEN_EXPIRES_IN,
+            REFRESH_TOKEN_EXPIRES_IN,
+        },
+    } = config.ENV;
+
+    const expiresIn = isRefreshToken
+        ? REFRESH_TOKEN_EXPIRES_IN
+        : ACCESS_TOKEN_EXPIRES_IN;
+    const secret = isRefreshToken ? REFRESH_TOKEN_SECRET : ACCESS_TOKEN_SECRET;
+    return jwt.sign(data, secret, {
+        expiresIn,
     });
 };
 
