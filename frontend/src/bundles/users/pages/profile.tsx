183202c909cd04326ec6ada2cc5f893e41f9595d
ykotsiuba <jurok3x@gmail.com>
2024-02-16 13:23:52 +0200

    rg-22: + Added profile-info component.

--- frontend/src/bundles/users/pages/profile.tsx
+++ frontend/src/bundles/users/pages/profile.tsx
@@ -1,8 +1,8 @@
-import { Button } from '~/bundles/common/components/components';
 import { PageTitle } from '~/bundles/common/components/page-title/page-title';
 
 import { ProfileCard } from '../components/profile-card/profile-card';
 import { ProfileForm } from '../components/profile-form/profile-form';
+import { ProfileInfo } from '../components/profile-info/profile-info';
 import styles from './style.module.scss';
 
 type User = {
@@ -21,31 +21,13 @@ const Profile: React.FC = () => {
             <div className={styles.profile__container}>
                 <PageTitle title="Account settings" />
                 <ProfileCard title="Your plan">
-                    <div className={styles.profile__card__icon}>
-                        <img
-                            src={
-                                'https://resume.io/assets/media/account_free_newaf14cca16ac227108062.svg'
-                            }
-                            alt="Upgrade for PDF downloads &amp; premium features."
-                        />
-                    </div>
-                    <div className={styles.profile__card__text}>
-                        <p className={styles.main__bold}>Free account</p>
-                        <p className={styles.main__regular}>
-                            You are on free plan. You can save your data and
-                            search for jobs. Upgrade for PDF downloads & premium
-                            features.
-                        </p>
-                    </div>
-                    <div className={styles.profile__card__actions}>
-                        <Button>Upgrade</Button>
-                    </div>
+                    <ProfileInfo />
                 </ProfileCard>
                 <ProfileCard title="Account">
                     <ProfileForm onSubmit={handleFormSubmit} />
                 </ProfileCard>
                 <ProfileCard title="Socials">bla</ProfileCard>
-                <ProfileCard title="Subsription">
+                <ProfileCard title="Subscription">
                     <ProfileForm onSubmit={handleFormSubmit} />
                 </ProfileCard>
                 <ProfileCard title="Delete account">
