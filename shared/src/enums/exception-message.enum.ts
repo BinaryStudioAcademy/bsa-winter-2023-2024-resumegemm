ccaf132e49c20a2478614823b82598d219e408eb
Icerock10 <dp021190dms@gmail.com>
2024-02-18 15:31:02 +0200

    rg-15: + exception message

--- shared/src/enums/exception-message.enum.ts
+++ shared/src/enums/exception-message.enum.ts
@@ -2,7 +2,8 @@ const ExceptionMessage = {
     EMAIL_TAKEN: 'Email is already taken.',
     USER_NOT_FOUND: 'User was not found.',
     INVALID_PASSWORD: 'Invalid password.',
-    INVALID_TOKEN: 'Authorization failed, please sign in again.',
+    AUTH_FAILED: 'Authorization failed, please sign in again.',
+    INVALID_REFRESH_TOKEN: 'Refresh token has expired or invalid.',
 } as const;
 
 export { ExceptionMessage };
