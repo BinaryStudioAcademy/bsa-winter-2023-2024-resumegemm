5b6ff67c29d569aef969f45df571da18c1f61226
Icerock10 <dp021190dms@gmail.com>
2024-02-18 17:07:11 +0200

    rg-15: * public routes keys

--- backend/src/common/server-application/constants/public-routes.ts
+++ backend/src/common/server-application/constants/public-routes.ts
@@ -3,9 +3,9 @@ import { ApiPath, AuthApiPath } from 'shared/build/index.js';
 const API_PREFIX = '/api/v1';
 
 const publicRoutes = {
-    SIGN_IN: `${API_PREFIX}${ApiPath.AUTH}${AuthApiPath.SIGN_IN}`,
-    SIGN_UP: `${API_PREFIX}${ApiPath.AUTH}${AuthApiPath.SIGN_UP}`,
-    TOKEN: `${API_PREFIX}${ApiPath.AUTH}${AuthApiPath.TOKEN}`,
+    [`${AuthApiPath.SIGN_IN}`]: `${API_PREFIX}${ApiPath.AUTH}${AuthApiPath.SIGN_IN}`,
+    [`${AuthApiPath.SIGN_UP}`]: `${API_PREFIX}${ApiPath.AUTH}${AuthApiPath.SIGN_UP}`,
+    [`${AuthApiPath.TOKEN}`]: `${API_PREFIX}${ApiPath.AUTH}${AuthApiPath.TOKEN}`,
 };
 
 export { publicRoutes };
