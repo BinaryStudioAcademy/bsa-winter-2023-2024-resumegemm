1dbeacd09926a08fc9455f4108b1356d897e636f
Icerock10 <dp021190dms@gmail.com>
2024-02-18 14:04:18 +0200

    rg-15: + exception message

--- shared/src/enums/exception-message.enum.ts
+++ shared/src/enums/exception-message.enum.ts
@@ -2,6 +2,7 @@ const ExceptionMessage = {
     EMAIL_TAKEN: 'Email is already taken.',
     USER_NOT_FOUND: 'User was not found.',
     INVALID_PASSWORD: 'Invalid password.',
+    INVALID_TOKEN: 'Authorization failed, please sign in again.',
 } as const;
 
 export { ExceptionMessage };
