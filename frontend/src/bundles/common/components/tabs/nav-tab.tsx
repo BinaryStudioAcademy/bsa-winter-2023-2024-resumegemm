9d4d179cd94652b89b167dfea288beea18a68183
ykotsiuba <jurok3x@gmail.com>
2024-02-19 13:15:49 +0200

    rg-6: * Updated initial tab logic.

--- frontend/src/bundles/common/components/tabs/nav-tab.tsx
+++ frontend/src/bundles/common/components/tabs/nav-tab.tsx
@@ -1,9 +1,10 @@
 import { clsx } from 'clsx';
 import React, { useCallback, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 import { type AppRoute } from '../../enums/app-route.enum';
 import { type ValueOf } from '../../types/types';
-import { Button, Link } from '../components';
+import { BaseButton, Link } from '../components';
 import styles from './style.module.scss';
 
 type NavbarItem = {
@@ -16,7 +17,22 @@ type Properties = {
 };
 
 const NavTabs: React.FC<Properties> = ({ items }) => {
-    const [activeTab, setActiveTab] = useState<number>(0);
+    const location = useLocation();
+
+    const initialActiveTab = items.findIndex(
+        (item) => {
+            if (item) {
+                const routePath = item.path;
+                return (item.path === location.pathname) ||
+                    (routePath && routePath === location.pathname.slice(1));
+        }
+            return false;
+        }
+    );
+    
+    const [activeTab, setActiveTab] = useState<number>(
+        initialActiveTab === -1 ? 0 : initialActiveTab
+    );
 
     const handleTabClick = useCallback((index: number) => {
         setActiveTab(index);
@@ -34,11 +50,11 @@ const NavTabs: React.FC<Properties> = ({ items }) => {
                         key={index}
                         to={item.path}
                     >
-                        <Button
+                        <BaseButton
                             onClick={onTabClick(index)}
                             className={clsx(styles.tab__item, { [styles.active]: index === activeTab })}>
                                 {item.label}
-                        </Button>
+                        </BaseButton>
                     </Link>
                 ))}
             </div>
