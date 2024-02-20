3fe91ed1b410cb027e89d0c7f53448110056e17a
Valentyn13 <valentyn13git@gmail.com>
2024-02-20 15:12:25 +0200

    rg-50: + Step component

---
+++ frontend/src/bundles/common/components/stepper/step/step.tsx
@@ -0,0 +1,26 @@
+import clsx from 'clsx';
+
+import styles from './styles.module.scss';
+
+type Properties = {
+    label: string;
+    index: number;
+    isActive:boolean;
+    isCompleted:boolean
+};
+
+const Step = ({ label, index, isActive, isCompleted }:Properties):JSX.Element => {
+    return(
+        <div className={clsx(styles.step,{
+            [styles.step__active]: isActive || isCompleted
+        })}>
+            <div className={clsx(styles.step__index,{
+                [styles.step__index_active]: isActive,
+                [styles.step__index_done]: isCompleted
+            })}>{!isCompleted && ++index}</div>
+            <div className={styles.step__label}>{label}</div>
+        </div>
+    );
+};
+
+export { Step };
