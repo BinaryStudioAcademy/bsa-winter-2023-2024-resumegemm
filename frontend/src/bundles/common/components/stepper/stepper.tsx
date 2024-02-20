fc7761f32a72638500b1cec5bbe5ec57bd55153d
Valentyn13 <valentyn13git@gmail.com>
2024-02-20 15:32:55 +0200

    rg-50: + Stepper component

---
+++ frontend/src/bundles/common/components/stepper/stepper.tsx
@@ -0,0 +1,29 @@
+import { Step } from './step/step';
+import styles from './styles.module.scss';
+
+type Properties = {
+    steps: { label: string }[];
+    activeStep: number;
+};
+
+const Stepper = ({ steps, activeStep }: Properties): JSX.Element => {
+    return (
+        <div className={styles.stepper}>
+            <div className={styles.stepper__header}>
+                {steps.map(({ label }, index) => {
+                    return (
+                        <Step
+                            key={index}
+                            label={label}
+                            index={index}
+                            isActive={index === activeStep}
+                            isCompleted={index < activeStep}
+                        />
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
+
+export { Stepper };
