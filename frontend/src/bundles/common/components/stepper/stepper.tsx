5e40a960da6843f1111a7c74804ff5f534bb06e7
Valentyn13 <valentyn13git@gmail.com>
2024-02-20 16:38:21 +0200

    rg-50: - stepper_header class

--- frontend/src/bundles/common/components/stepper/stepper.tsx
+++ frontend/src/bundles/common/components/stepper/stepper.tsx
@@ -15,25 +15,23 @@ const Stepper = ({
     className = '',
 }: Properties): JSX.Element => {
     return (
-        <div className={clsx(styles.step, className)}>
-            <div className={styles.stepper__header}>
-                {steps.map(({ label }, index) => {
-                    return (
-                        <>
-                            <Step
-                                key={index}
-                                label={label}
-                                index={index}
-                                isActive={index === activeStep}
-                                isCompleted={index < activeStep}
-                            />
-                            {index !== steps.length - 1 && (
-                                <div className={styles.stepper__line}></div>
-                            )}
-                        </>
-                    );
-                })}
-            </div>
+        <div className={clsx(styles.stepper, className)}>
+            {steps.map(({ label }, index) => {
+                return (
+                    <>
+                        <Step
+                            key={index}
+                            label={label}
+                            index={index}
+                            isActive={index === activeStep}
+                            isCompleted={index < activeStep}
+                        />
+                        {index !== steps.length - 1 && (
+                            <div className={styles.stepper__line}></div>
+                        )}
+                    </>
+                );
+            })}
         </div>
     );
 };
