4612682eb9532c6c7c13604c28cc239159983afd
Valentyn13 <valentyn13git@gmail.com>
2024-02-20 16:01:57 +0200

    rg-50: * step component

--- frontend/src/bundles/common/components/stepper/step/step.tsx
+++ frontend/src/bundles/common/components/stepper/step/step.tsx
@@ -5,20 +5,40 @@ import styles from './styles.module.scss';
 type Properties = {
     label: string;
     index: number;
-    isActive:boolean;
-    isCompleted:boolean
+    isActive: boolean;
+    isCompleted: boolean;
+    labelFontSize?: string;
 };
 
-const Step = ({ label, index, isActive, isCompleted }:Properties):JSX.Element => {
-    return(
-        <div className={clsx(styles.step,{
-            [styles.step__active]: isActive || isCompleted
-        })}>
-            <div className={clsx(styles.step__index,{
-                [styles.step__index_active]: isActive,
-                [styles.step__index_done]: isCompleted
-            })}>{!isCompleted && ++index}</div>
-            <div className={styles.step__label}>{label}</div>
+const Step = ({
+    label,
+    index,
+    isActive,
+    isCompleted,
+    labelFontSize = '16px',
+}: Properties): JSX.Element => {
+    return (
+        <div
+            className={clsx(styles.step, {
+                [styles.step__active]: isActive || isCompleted,
+            })}
+        >
+            <div
+                className={clsx(styles.step__index, {
+                    [styles.step__index_active]: isActive,
+                    [styles.step__index_done]: isCompleted,
+                })}
+            >
+                {!isCompleted && ++index}
+            </div>
+            <div
+                className={styles.step__label}
+                style={{
+                    fontSize: labelFontSize,
+                }}
+            >
+                {label}
+            </div>
         </div>
     );
 };
