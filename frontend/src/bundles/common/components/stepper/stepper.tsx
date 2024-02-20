1d9956a5809289405297c7169fe1f5e3499e9fef
Valentyn13 <valentyn13git@gmail.com>
2024-02-20 15:49:16 +0200

    rg-50: + div separator to stepper component

--- frontend/src/bundles/common/components/stepper/stepper.tsx
+++ frontend/src/bundles/common/components/stepper/stepper.tsx
@@ -12,13 +12,16 @@ const Stepper = ({ steps, activeStep }: Properties): JSX.Element => {
             <div className={styles.stepper__header}>
                 {steps.map(({ label }, index) => {
                     return (
-                        <Step
-                            key={index}
-                            label={label}
-                            index={index}
-                            isActive={index === activeStep}
-                            isCompleted={index < activeStep}
-                        />
+                        <>
+                            <Step
+                                key={index}
+                                label={label}
+                                index={index}
+                                isActive={index === activeStep}
+                                isCompleted={index < activeStep}
+                            />
+                            {index !== steps.length -1 &&(<div className={styles.stepper__line}></div>)}
+                        </>
                     );
                 })}
             </div>
