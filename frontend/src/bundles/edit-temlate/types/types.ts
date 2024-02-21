1ee4dca1fc7e288695eb0deab243cd5c2a3a39cb
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-02-21 21:59:48 +0200

    rg-40: + TemplateDto to export type bundle

--- frontend/src/bundles/edit-temlate/types/types.ts
+++ frontend/src/bundles/edit-temlate/types/types.ts
@@ -1,5 +1,7 @@
 export {
     type TemplateBlockSettings,
+    type TemplateDto,
     type TemplateEditRequestDto,
     type TemplateEditResponseDto,
 } from '../../../../../shared/build/bundles/templates/templates.js';
+export { TemplateBlockTitles } from 'shared/build/bundles/templates/enums/enums.js';
