751c3fea9c7e7632e0d8f98d0d34534ea985c632
ykotsiuba <jurok3x@gmail.com>
2024-02-21 13:47:54 +0200

    rg-6: * Introduced NavLink to tabs component

--- frontend/src/bundles/common/components/tabs/nav-tab.tsx
+++ frontend/src/bundles/common/components/tabs/nav-tab.tsx
@@ -1,10 +1,9 @@
 import { clsx } from 'clsx';
-import React, { useCallback, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import { type AppRoute } from '../../enums/app-route.enum';
 import { type ValueOf } from '../../types/types';
-import { BaseButton, Link } from '../components';
 import styles from './style.module.scss';
 
 type NavbarItem = {
@@ -16,46 +15,21 @@ type Properties = {
     items: NavbarItem[];
 };
 
+const getNavLinkClassName = ({ isActive }: { isActive: boolean }): string =>
+        clsx(styles.tab__item, isActive && styles.active);
+
 const NavTabs: React.FC<Properties> = ({ items }) => {
-    const location = useLocation();
-
-    const initialActiveTab = items.findIndex(
-        (item) => {
-            if (item) {
-                const routePath = item.path;
-                return (item.path === location.pathname) ||
-                    (routePath && routePath === location.pathname.slice(1));
-        }
-            return false;
-        }
-    );
-    
-    const [activeTab, setActiveTab] = useState<number>(
-        initialActiveTab === -1 ? 0 : initialActiveTab
-    );
-
-    const handleTabClick = useCallback((index: number) => {
-        setActiveTab(index);
-    }, [setActiveTab]);
-
-    const onTabClick = (index: number) => () => {
-        handleTabClick(index);
-    };
-
     return (
         <nav>
             <div className={styles.tab__header}>
                 {items.map((item, index) => (
-                    <Link
+                    <NavLink
                         key={index}
                         to={item.path}
+                        className={getNavLinkClassName}
                     >
-                        <BaseButton
-                            onClick={onTabClick(index)}
-                            className={clsx(styles.tab__item, { [styles.active]: index === activeTab })}>
-                                {item.label}
-                        </BaseButton>
-                    </Link>
+                        {item.label}
+                    </NavLink>
                 ))}
             </div>
         </nav>
