b68ffc280700f89edc3c38f029c9687a8bf8ac7f
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-02-21 21:53:06 +0200

    rg-40: * template slice, added initial state for testing

--- frontend/src/bundles/edit-temlate/store/slice.ts
+++ frontend/src/bundles/edit-temlate/store/slice.ts
@@ -3,16 +3,60 @@ import { createSlice, isAnyOf } from '@reduxjs/toolkit';
 import { DataStatus } from '~/bundles/common/enums/enums.js';
 import { type ValueOf } from '~/bundles/common/types/types.js';
 
-import { type TemplateEditResponseDto } from '../types/types.js';
+import { type TemplateDto } from '../types/types.js';
+import { TemplateBlockTitles } from '../types/types.js';
 import { editTemplate } from './actions.js';
 
 type State = {
-    template: TemplateEditResponseDto | null;
+    templates: TemplateDto[];
     dataStatus: ValueOf<typeof DataStatus>;
 };
 
 const initialState: State = {
-    template: null,
+    templates: [
+        {
+            id: '1',
+            isOwner: true,
+            createdAt: '2021-08-09T11:51:00.000Z',
+            updatedAt: '2021-08-09T11:51:00.000Z',
+            deletedAt: null,
+            templateSettings: {
+                [TemplateBlockTitles.Contacts]: {
+                    enabled: true,
+                },
+                [TemplateBlockTitles.Experience]: {
+                    enabled: true,
+                },
+                [TemplateBlockTitles.Education]: {
+                    enabled: true,
+                },
+                [TemplateBlockTitles.Languages]: {
+                    enabled: true,
+                },
+                [TemplateBlockTitles.Portfolio]: {
+                    enabled: true,
+                },
+                [TemplateBlockTitles.Profile]: {
+                    enabled: true,
+                },
+                [TemplateBlockTitles.Recommendations]: {
+                    enabled: true,
+                },
+                [TemplateBlockTitles.Skills]: {
+                    enabled: true,
+                },
+                [TemplateBlockTitles.Socials]: {
+                    enabled: true,
+                },
+                [TemplateBlockTitles.Summary]: {
+                    enabled: true,
+                },
+                [TemplateBlockTitles.TechStack]: {
+                    enabled: true,
+                },
+            },
+        }
+    ],
     dataStatus: DataStatus.IDLE,
 };
 
@@ -27,12 +71,12 @@ const { reducer, actions, name } = createSlice({
 
         builder.addMatcher(isAnyOf(editTemplate.fulfilled), (state, action) => {
             state.dataStatus = DataStatus.FULFILLED;
-            state.template = action.payload as TemplateEditResponseDto;
+            state.templates.push(action.payload as TemplateDto);
         });
 
         builder.addMatcher(isAnyOf(editTemplate.rejected), (state) => {
             state.dataStatus = DataStatus.REJECTED;
-            state.template = null;
+            state.templates = [];
         });
     },
 });
