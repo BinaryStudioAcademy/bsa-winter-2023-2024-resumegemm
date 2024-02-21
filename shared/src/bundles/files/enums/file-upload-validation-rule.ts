b131338ff4f713ec121e7cf92ba85f11ffa436d3
Vladyslav Lazarenko <ezio2506@gmail.com>
2024-02-21 20:06:04 +0200

    rg-33: + file upload validation enums

---
+++ shared/src/bundles/files/enums/file-upload-validation-rule.ts
@@ -0,0 +1,6 @@
+const FileUploadValidationRule = {
+    MAXIMUM_FILE_SIZE: 10_000_000,
+    UPLOAD_FILE_CONTENT_TYPES: ['png', 'jpg', 'jpeg'],
+  } as const;
+  
+  export { FileUploadValidationRule };
