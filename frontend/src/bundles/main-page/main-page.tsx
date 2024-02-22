a20fcdf86d2237b4a59ecf6f890731dc7384e709
Valentyn13 <valentyn13git@gmail.com>
2024-02-22 14:24:25 +0200

    rg-61: + simple header html structure

--- frontend/src/bundles/main-page/main-page.tsx
+++ frontend/src/bundles/main-page/main-page.tsx
@@ -1,7 +1,15 @@
 import styles from './styles.module.scss';
 
 const MainPage = (): JSX.Element => {
-    return <h1 className={styles.main}>Main Page</h1>;
+    return (
+        <div className={styles.main}>
+            <div className={styles.main__header}>
+                <div className={styles.main__logo}></div>
+                <div className={styles.main__tabs}></div>
+                <div className={styles.main__avatar}></div>
+            </div>
+        </div>
+    );
 };
 
 export { MainPage };
