d98376dc04ef0e96787cd395bc2082cb2a3cb127
Valentyn13 <valentyn13git@gmail.com>
2024-02-22 15:08:22 +0200

    rg-61: + wrapper and NavTabs component

--- frontend/src/bundles/main-page/main-page.tsx
+++ frontend/src/bundles/main-page/main-page.tsx
@@ -1,20 +1,30 @@
+import { NavTabs } from '../common/components/components';
+import { AppRoute } from '../common/enums/app-route.enum';
 import styles from './styles.module.scss';
+
+const navbarItems = [
+    { label: 'Home', path: AppRoute.HOME },
+    { label: 'Preview', path: AppRoute.PREVIEW },
+];
 
 const MainPage = (): JSX.Element => {
     return (
-        <div className={styles.main}>
-            <section className={styles.main__header}>
-                <div className={styles.main__logo}>
-                    <img src="/src/assets/img/logo.svg" alt="logo" />
-                </div>
-                <div className={styles.main__tabs}></div>
-                <div className={styles.main__avatar}>
-                    <img
-                        src="/src/assets/img/mock-avatar.png"
-                        alt="user-avatar"
-                    />
-                </div>
-            </section>
+        <div className={styles.wrapper}>
+            <div className={styles.main}>
+                <section className={styles.main__header}>
+                    <div className={styles.main__logo}>
+                        <img src="/src/assets/img/logo.svg" alt="logo" />
+                    </div>
+                    <NavTabs items={navbarItems} />
+                    <div className={styles.main__avatar}>
+                        <img
+                            src="/src/assets/img/mock-avatar.png"
+                            alt="user-avatar"
+                        />
+                    </div>
+                </section>
+            </div>
+            <div className={styles.wrapper__line}></div>
         </div>
     );
 };
