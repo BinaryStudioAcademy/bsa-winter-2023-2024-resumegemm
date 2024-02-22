aa1027b47424aaa7d646668f98de6750fb7f96b6
ykotsiuba <jurok3x@gmail.com>
2024-02-22 16:01:40 +0200

    rg-48: + Added delete profile actions

--- frontend/src/bundles/users/store/actions.ts
+++ frontend/src/bundles/users/store/actions.ts
@@ -2,6 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 import { type AsyncThunkConfig } from '~/bundles/common/types/types.js';
 import { type UserGetAllResponseDto } from '~/bundles/users/users.js';
+import { StorageKey } from '~/framework/storage/storage.js';
 
 import { name as sliceName } from './slice.js';
 
@@ -14,5 +15,21 @@ const loadAll = createAsyncThunk<
 
     return userApi.getAll();
 });
+const deleteProfile = createAsyncThunk<
+    UserGetAllResponseDto,
+    { id: string },
+    AsyncThunkConfig
+>(`${sliceName}/delete`, async (payload, { extra }) => {
+    const { id } = payload;
+    const { userApi, storageApi } = extra;
 
-export { loadAll };
+    const user = await userApi.deleteProfile(id);
+
+    if (user) {
+        await storageApi.drop(StorageKey.ACCESS_TOKEN);
+    }
+
+    return userApi.getAll();
+});
+
+export { deleteProfile, loadAll };
