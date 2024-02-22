f18cd817b159869550e7f5da3ec508d329f3bfaa
Valentyn13 <valentyn13git@gmail.com>
2024-02-22 14:41:56 +0200

    rg-61: + images to main__header section

--- frontend/src/bundles/main-page/main-page.tsx
+++ frontend/src/bundles/main-page/main-page.tsx
@@ -3,11 +3,18 @@ import styles from './styles.module.scss';
 const MainPage = (): JSX.Element => {
     return (
         <div className={styles.main}>
-            <div className={styles.main__header}>
-                <div className={styles.main__logo}></div>
+            <section className={styles.main__header}>
+                <div className={styles.main__logo}>
+                    <img src="/src/assets/img/logo.svg" alt="logo" />
+                </div>
                 <div className={styles.main__tabs}></div>
-                <div className={styles.main__avatar}></div>
-            </div>
+                <div className={styles.main__avatar}>
+                    <img
+                        src="/src/assets/img/mock-avatar.png"
+                        alt="user-avatar"
+                    />
+                </div>
+            </section>
         </div>
     );
 };
