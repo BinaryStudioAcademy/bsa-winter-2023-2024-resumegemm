56b02fa8fd544f38513161ce8ba19db8297e2258
Valentyn13 <valentyn13git@gmail.com>
2024-02-22 19:48:49 +0200

    rg-65: - junk props

--- frontend/src/bundles/common/components/icon-input/icon-input.tsx
+++ frontend/src/bundles/common/components/icon-input/icon-input.tsx
@@ -1,27 +1,25 @@
 import clsx from 'clsx';
-import { type InputHTMLAttributes } from 'react';
+import { type InputHTMLAttributes, type ReactNode } from 'react';
 
-import { IconName, IconSize } from '../../enums/enums';
-import { Icon } from '../components';
 import styles from './styles.module.scss';
 
 interface Properties extends InputHTMLAttributes<HTMLInputElement> {
     className?: string;
-    iconName?: IconName;
-    iconSize?: IconSize;
+    width?: string;
+    children?: ReactNode;
 }
 
 const IconInput = ({
+    children,
     className = '',
-    iconName = IconName.SEARCH,
-    iconSize = IconSize.LARGE,
+    width = '200px',
     ...otherProperties
 }: Properties): JSX.Element => {
     const { onChange } = otherProperties;
 
     return (
-        <label className={styles.input__wrapper}>
-            <Icon size={iconSize} name={iconName} />
+        <label className={styles.input__wrapper} style={{ width }}>
+            {children}
             <input
                 className={clsx(styles.icon_input, className)}
                 {...otherProperties}
