4ece82d7d84427bf7167bbd9d5c61ded7455394c
Valentyn13 <valentyn13git@gmail.com>
2024-02-22 14:02:08 +0200

    rg-61: + styles to MainPage

--- frontend/src/bundles/main-page/main-page.tsx
+++ frontend/src/bundles/main-page/main-page.tsx
@@ -1,5 +1,7 @@
+import styles from './styles.module.scss';
+
 const MainPage = (): JSX.Element => {
-    return <h1>Main Page</h1>;
+    return <h1 className={styles.main}>Main Page</h1>;
 };
 
 export { MainPage };
