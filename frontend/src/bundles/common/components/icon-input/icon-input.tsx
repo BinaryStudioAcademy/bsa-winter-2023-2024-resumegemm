137048b644fef25f8f2ef297003baf3195affa32
Valentyn13 <valentyn13git@gmail.com>
2024-02-23 13:18:28 +0200

    rg-65: + refactor IconInput component

--- frontend/src/bundles/common/components/icon-input/icon-input.tsx
+++ frontend/src/bundles/common/components/icon-input/icon-input.tsx
@@ -1,31 +1,23 @@
 import clsx from 'clsx';
-import { type InputHTMLAttributes, type ReactNode } from 'react';
 
 import styles from './styles.module.scss';
 
-interface Properties extends InputHTMLAttributes<HTMLInputElement> {
+interface Properties {
+    svgIcon: JSX.Element;
+    input: JSX.Element;
     className?: string;
-    width?: string;
-    children?: ReactNode;
 }
 
 const IconInput = ({
-    children,
+    svgIcon,
+    input,
     className = '',
-    width = '200px',
-    ...otherProperties
 }: Properties): JSX.Element => {
-    const { onChange } = otherProperties;
-
     return (
-        <label className={styles.input__wrapper} style={{ width }}>
-            {children}
-            <input
-                className={clsx(styles.icon_input, className)}
-                {...otherProperties}
-                onChange={onChange}
-            />
-        </label>
+        <div className={clsx(styles.input__wrapper, className)}>
+            {svgIcon}
+            {input}
+        </div>
     );
 };
 
