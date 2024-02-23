0b80ac3c3a066bdb48aeea534f40ab59f46d39d5
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-02-23 13:02:22 +0100

    rg-53: + migrations

---
+++ backend/src/migrations/20240222190958_add_industries_table.ts
@@ -0,0 +1,17 @@
+import { type Knex } from 'knex';
+
+import { DatabaseTableName } from '~/common/database/database.package.js';
+import { DatabaseColumnName } from '~/common/database/enums/database-column-name.enum.js';
+
+function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable(DatabaseTableName.INDUSTRIES, (table) => {
+        table.increments(DatabaseColumnName.ID).primary();
+        table.string(DatabaseColumnName.INDUSTRY).notNullable();
+    });
+}
+
+function down(knex: Knex): Promise<void> {
+    return knex.schema.dropTableIfExists(DatabaseTableName.INDUSTRIES);
+}
+
+export { down, up };
