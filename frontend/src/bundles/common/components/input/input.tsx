f9ad219f64dc728d6f04d509d2e46a101da12648
Valentyn13 <valentyn13git@gmail.com>
2024-02-23 13:04:15 +0200

    rg-65: + className to Input

--- frontend/src/bundles/common/components/input/input.tsx
+++ frontend/src/bundles/common/components/input/input.tsx
@@ -6,17 +6,28 @@ import styles from './styles.module.scss';
 interface Properties extends InputHTMLAttributes<HTMLInputElement> {
     hasError?: boolean;
     width?: string;
+    className?: string;
 }
 const Input = forwardRef<HTMLInputElement, Properties>(
     (
-        { hasError = false, width = 'auto', disabled, ...otherProperties },
+        {
+            hasError = false,
+            width = 'auto',
+            className = '',
+            disabled,
+            ...otherProperties
+        },
         reference,
     ) => (
         <input
-            className={clsx(styles.input, {
-                [styles.input__error]: hasError,
-                [styles.input__disabled]: disabled,
-            })}
+            className={clsx(
+                styles.input,
+                {
+                    [styles.input__error]: hasError,
+                    [styles.input__disabled]: disabled,
+                },
+                className,
+            )}
             {...otherProperties}
             style={{ width }}
             ref={reference}
