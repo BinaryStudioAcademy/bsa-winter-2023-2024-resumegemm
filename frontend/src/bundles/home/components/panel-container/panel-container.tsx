1b590eacf3868350271b920fad59df9e6b67dc94
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-02-23 14:00:29 +0100

    rg-53: + pass filter function to panel component

--- frontend/src/bundles/home/components/panel-container/panel-container.tsx
+++ frontend/src/bundles/home/components/panel-container/panel-container.tsx
@@ -1,21 +1,27 @@
 import clsx from 'clsx';
 
+import { Filter } from '../components';
 import styles from './styles.module.scss';
 
 type Properties = {
     children?: React.ReactNode;
     name: string;
     classname?: string;
+    onFilterOptionSelect: (option: number) => void;
 };
 
 const PanelContainer: React.FC<Properties> = ({
     children,
     name,
     classname,
+    onFilterOptionSelect,
 }: Properties) => {
     return (
         <div className={clsx(styles.panel_container, classname)}>
-            <div className={styles.panel_container__top_bar}>{name}</div>
+            <div className={styles.panel_container__top_bar}>
+                {name}
+                <Filter onOptionSelect={onFilterOptionSelect} />
+            </div>
             <div className={styles.panel_container__content}>{children}</div>
         </div>
     );
