0afcfb82dd11ad80c134ba3e7ed69f54e613394e
ykotsiuba <jurok3x@gmail.com>
2024-02-23 16:10:21 +0200

    rg-48: + Added soft delete logic to profile component

--- frontend/src/bundles/users/components/delete-account/delete-account.tsx
+++ frontend/src/bundles/users/components/delete-account/delete-account.tsx
@@ -1,16 +1,64 @@
-import { BaseButton } from '~/bundles/common/components/components';
-import { ButtonVariant } from '~/bundles/common/enums/enums';
+import { useCallback, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
+import { BaseButton, Modal } from '~/bundles/common/components/components';
+import {
+    AppRoute,
+    ButtonVariant,
+    ModalVariant,
+} from '~/bundles/common/enums/enums';
+import { useAppDispatch } from '~/bundles/common/hooks/hooks';
+
+import { actions as userActions } from '../../store/';
 import styles from './style.module.scss';
 
 const DeleteAccount: React.FC = () => {
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleOpenModal = useCallback(() => {
+        setIsModalOpen(true);
+    }, []);
+
+    const handleCloseModal = useCallback(() => {
+        setIsModalOpen(false);
+    }, []);
+
+    const handleDeleteAccount = useCallback(() => {
+        void dispatch(userActions.deleteProfile());
+        setIsModalOpen(false);
+        navigate(AppRoute.SIGN_IN);
+    }, [dispatch, navigate]);
+
     return (
         <div className={styles.profile__delete}>
             <p>
                 Once you delete your account, it cannot be undone. This is
                 permanent.
             </p>
-            <BaseButton variant={ButtonVariant.PRIMARY}>Delete</BaseButton>
+            <BaseButton
+                variant={ButtonVariant.PRIMARY}
+                onClick={handleOpenModal}
+            >
+                Delete
+            </BaseButton>
+            <Modal
+                isOpen={isModalOpen}
+                onClose={handleCloseModal}
+                title="Delete profile"
+                variant={ModalVariant.CONFIRM}
+            >
+                <div className={styles.profile__delete__actions}>
+                    <p>Are you sure you want delete your profile?</p>
+                    <BaseButton
+                        variant={ButtonVariant.PRIMARY}
+                        onClick={handleDeleteAccount}
+                    >
+                        Delete
+                    </BaseButton>
+                </div>
+            </Modal>
         </div>
     );
 };
