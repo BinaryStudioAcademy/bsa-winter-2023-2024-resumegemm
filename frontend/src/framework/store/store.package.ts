2078bb1c9709dc60da14b6ef275e9ae42ad41b00
Valentyn13 <valentyn13git@gmail.com>
2024-02-23 20:52:48 +0200

    rg-61: + templates reducer and api to storage

--- frontend/src/framework/store/store.package.ts
+++ frontend/src/framework/store/store.package.ts
@@ -8,6 +8,8 @@ import { configureStore } from '@reduxjs/toolkit';
 import { authApi } from '~/bundles/auth/auth.js';
 import { reducer as authReducer } from '~/bundles/auth/store/';
 import { AppEnvironment } from '~/bundles/common/enums/enums.js';
+import { templatesApi } from '~/bundles/home/api/templates';
+import { reducer as templatesReducer } from '~/bundles/home/store/slice';
 import { reducer as usersReducer } from '~/bundles/users/store/';
 import { userApi } from '~/bundles/users/users.js';
 import { type IConfig } from '~/framework/config/config.js';
@@ -17,11 +19,13 @@ import { storage } from '../storage/storage';
 type RootReducer = {
     auth: ReturnType<typeof authReducer>;
     users: ReturnType<typeof usersReducer>;
+    templates: ReturnType<typeof templatesReducer>;
 };
 
 type ExtraArguments = {
     authApi: typeof authApi;
     userApi: typeof userApi;
+    templatesApi: typeof templatesApi;
     storageApi: typeof storage;
 };
 
@@ -42,6 +46,7 @@ class Store {
             reducer: {
                 auth: authReducer,
                 users: usersReducer,
+                templates: templatesReducer,
             },
             middleware: (getDefaultMiddleware) => {
                 return getDefaultMiddleware({
@@ -56,6 +61,7 @@ class Store {
     public get extraArguments(): ExtraArguments {
         return {
             authApi,
+            templatesApi,
             userApi,
             storageApi: storage,
         };
