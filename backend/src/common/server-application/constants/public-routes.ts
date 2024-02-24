c7c304f48e5a90c2a7ff32797cefcf6c64d77621
Icerock10 <dp021190dms@gmail.com>
2024-02-24 22:54:03 +0200

    rg-51: * public routes

--- backend/src/common/server-application/constants/public-routes.ts
+++ backend/src/common/server-application/constants/public-routes.ts
@@ -1,4 +1,4 @@
-import { ApiPath, AuthApiPath } from 'shared/build/index.js';
+import { ApiPath, AuthApiPath, OpenAuthApiPath } from 'shared/build/index.js';
 
 const API_PREFIX = '/api/v1';
 
@@ -6,6 +6,9 @@ const publicRoutes = {
     [`${AuthApiPath.SIGN_IN}`]: `${API_PREFIX}${ApiPath.AUTH}${AuthApiPath.SIGN_IN}`,
     [`${AuthApiPath.SIGN_UP}`]: `${API_PREFIX}${ApiPath.AUTH}${AuthApiPath.SIGN_UP}`,
     [`${AuthApiPath.TOKEN}`]: `${API_PREFIX}${ApiPath.AUTH}${AuthApiPath.TOKEN}`,
+    [`${OpenAuthApiPath.REDIRECT_CALLBACK}`]: `${OpenAuthApiPath.REDIRECT_CALLBACK}`,
+    [`${OpenAuthApiPath.GITHUB}`]: `${OpenAuthApiPath.GITHUB}`,
+    [`${ApiPath.OPEN_AUTH}`]: `${API_PREFIX}${ApiPath.OPEN_AUTH}${OpenAuthApiPath.GITHUB}`,
 };
 
 export { publicRoutes };
