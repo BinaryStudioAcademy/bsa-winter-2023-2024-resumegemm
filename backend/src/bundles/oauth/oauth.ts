4742c482f7917d6af602738c4b66e6448a1d4304
Icerock10 <dp021190dms@gmail.com>
2024-02-25 00:29:02 +0200

    rg-51: + init services

---
+++ backend/src/bundles/oauth/oauth.ts
@@ -0,0 +1,17 @@
+import { OpenAuthController } from '~/bundles/oauth/oauth.controller.js';
+import { OauthModel } from '~/bundles/oauth/oauth.model.js';
+import { OauthRepository } from '~/bundles/oauth/oauth.repository.js';
+import { OauthService } from '~/bundles/oauth/oauth.service.js';
+import { profileRepository } from '~/bundles/profile/profile.js';
+import { httpApi } from '~/common/api/api.js';
+import { logger } from '~/common/logger/logger.js';
+
+const openAuthRepository = new OauthRepository(OauthModel, profileRepository);
+const openAuthService = new OauthService(openAuthRepository);
+const openAuthController = new OpenAuthController(
+    logger,
+    openAuthService,
+    httpApi,
+);
+
+export { openAuthController, openAuthRepository, openAuthService };
