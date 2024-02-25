c93371dcca6e6f0ee7ae1294d075a0a571942f4b
Icerock10 <dp021190dms@gmail.com>
2024-02-25 16:17:09 +0200

    rg-51: + oauth repo

--- backend/src/bundles/oauth/oauth.repository.ts
+++ backend/src/bundles/oauth/oauth.repository.ts
@@ -1,106 +1,35 @@
-import { Guid as guid } from 'guid-typescript';
-import { type Transaction } from 'objection';
-import { HttpCode, HttpError } from 'shared/build/index.js';
+import { type UserGithubLoginResponseDto } from 'shared/build/index.js';
 
 import { type OauthModel } from '~/bundles/oauth/oauth.model.js';
-import { type ProfileRepository } from '~/bundles/profile/profile.repository';
-import { type IRepository } from '~/common/interfaces/interfaces.js';
+import { AbstractRepository } from '~/common/database/abstract.repository.js';
 
-import {
-    type GithubUserWithProfileRelation,
-    type OauthUserEntityFields,
-    type UserGithubLoginRequestDto,
-} from './types/types.js';
+import { type OauthUserEntityFields } from './types/types.js';
 
-class OauthRepository implements Partial<IRepository> {
-    private oauthModel: typeof OauthModel;
-    private profileRepository: ProfileRepository;
+type TOauthRepository = {
+    findByOauthId(oauthId: number): Promise<OauthUserEntityFields | null>;
+};
 
-    public constructor(
-        oauthModel: typeof OauthModel,
-        profileRepository: ProfileRepository,
-    ) {
-        this.oauthModel = oauthModel;
-        this.profileRepository = profileRepository;
+class OauthRepository
+    extends AbstractRepository<
+        typeof OauthModel,
+        UserGithubLoginResponseDto | OauthUserEntityFields
+    >
+    implements TOauthRepository
+{
+    public constructor({
+        oauthModel,
+    }: Record<'oauthModel', typeof OauthModel>) {
+        super(oauthModel);
     }
 
     public async findByOauthId(
         oauthId: number,
     ): Promise<OauthUserEntityFields | null> {
-        const foundUser = await this.oauthModel
+        const foundUser = await this.model
             .query()
             .where('oauth_id', oauthId)
             .first();
         return foundUser ?? null;
-    }
-
-    public async getUserWithProfile(
-        id: string,
-    ): Promise<GithubUserWithProfileRelation> {
-        return this.oauthModel
-            .query()
-            .findById(id)
-            .withGraphFetched('[user_profile]')
-            .castTo<GithubUserWithProfileRelation>();
-    }
-
-    public async createUserWithProfile({
-        email,
-        firstName,
-        avatar,
-        oauthId,
-        oauthStrategy,
-    }: UserGithubLoginRequestDto): Promise<OauthUserEntityFields> {
-        const transaction = await this.oauthModel.startTransaction();
-        try {
-            const { id } = await this.profileRepository.create({
-                firstName,
-                avatar,
-                transaction,
-            });
-
-            const user = await this.create({
-                email,
-                oauthId,
-                oauthStrategy,
-                id: guid.raw(),
-                profileId: id,
-                transaction,
-            });
-            await transaction.commit();
-            return user;
-        } catch (error: unknown) {
-            const { message } = error as HttpError;
-            await transaction.rollback();
-            throw new HttpError({
-                message,
-                status: HttpCode.INTERNAL_SERVER_ERROR,
-            });
-        }
-    }
-
-    public async create({
-        email,
-        oauthId,
-        oauthStrategy,
-        id,
-        profileId,
-        transaction,
-    }: OauthUserEntityFields & {
-        transaction: Transaction;
-    }): Promise<OauthUserEntityFields> {
-        return this.oauthModel
-            .query()
-            .insert({
-                id,
-                email,
-                oauthId,
-                oauthStrategy,
-                profileId,
-            })
-            .returning('*')
-            .transacting(transaction)
-            .execute();
     }
 }
 
