16ad78ddde76f98398f697b218e9bd96630a806c
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-02-26 00:29:23 +0200

    rg-40: * removed relative path for shared

--- frontend/src/bundles/edit-temlate/types/types.ts
+++ frontend/src/bundles/edit-temlate/types/types.ts
@@ -1,7 +1,7 @@
+export { TemplateBlockTitles } from 'shared/build/bundles/templates/enums/enums.js';
 export {
     type TemplateBlockSettings,
     type TemplateDto,
     type TemplateUpdateItemRequestDto,
     type TemplateUpdateItemResponseDto,
-} from '../../../../../shared/build/bundles/templates/templates.js';
-export { TemplateBlockTitles } from 'shared/build/bundles/templates/enums/enums.js';
+} from 'shared/build/bundles/templates/templates.js';
