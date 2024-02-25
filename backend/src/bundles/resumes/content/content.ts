00c5385a1fb76506fa4f0063aaa25365b6948204
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-02-26 00:09:57 +0200

    rg-66: + export resume content  models

---
+++ backend/src/bundles/resumes/content/content.ts
@@ -0,0 +1,5 @@
+export { ContactsModel } from './contacts/contacts';
+export { EducationModel } from './education/education';
+export { ExperienceModel } from './experience/experience';
+export { PersonalInformationModel } from './personal-info/personal-info';
+export { TechnicalSkillModel } from './technical-skills/technical-skills';
