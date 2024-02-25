1f89184265906183ba35ff57196ddad6375b3029
Icerock10 <dp021190dms@gmail.com>
2024-02-25 16:10:44 +0200

    rg-51: - unused methods

--- backend/src/common/interfaces/service.interface.ts
+++ backend/src/common/interfaces/service.interface.ts
@@ -1,19 +1,15 @@
 interface IService<T = unknown> {
-    find(): Promise<T>;
-
     findAll(): Promise<{
         items: T[];
     }>;
+
+    getById(id: string): Promise<T>;
 
     findByEmail(email: string): Promise<T>;
 
     getUserWithProfile(id: string): Promise<T>;
 
     create(payload: T): Promise<T>;
-
-    update(): Promise<T>;
-
-    delete(): Promise<boolean>;
 }
 
 export { type IService };
