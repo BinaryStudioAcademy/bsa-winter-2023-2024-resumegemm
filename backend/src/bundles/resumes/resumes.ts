1b718cbd92d056eb3d6b0f1f9f48b5a034e69a5c
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-02-26 00:17:43 +0200

    rg-66: + export resume types

--- backend/src/bundles/resumes/resumes.ts
+++ backend/src/bundles/resumes/resumes.ts
@@ -1,1 +1,15 @@
+import { logger } from '~/common/logger/logger.js';
+
+import { ResumeController } from './resume.controller.js';
+import { ResumeModel } from './resume.model.js';
+import { ResumeRepository } from './resume.repository.js';
+import { ResumeService } from './resume.service.js';
+
+const resumeRepository = new ResumeRepository(ResumeModel);
+
+const resumeService = new ResumeService(resumeRepository);
+
+const resumeController = new ResumeController(logger, resumeService);
+
+export { resumeController, resumeService };
 export { ResumeModel } from './resume.model.js';
---
+++ backend/src/bundles/resumes/types/resume.type.ts
@@ -0,0 +1,17 @@
+export { type IResumeRepository } from './resume-repository.type.js';
+export {
+    type Contacts,
+    type Education,
+    type Experience,
+    type PersonalInformation,
+    type Resume,
+    type ResumeCreateItemRequestDto,
+    type ResumeGetAllRequestDto,
+    type ResumeGetAllResponseDto,
+    type ResumeGetItemRequestDto,
+    type ResumeGetItemResponseDto,
+    type ResumeUpdateItemRequestDto,
+    type TechnicalSkill,
+    ResumesApiPath,
+    SkillLevel,
+} from 'shared/build/index.js';
