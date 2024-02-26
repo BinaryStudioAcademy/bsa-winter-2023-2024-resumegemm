e67c5259ae207533faeb507257cb60c341859f3d
Nazarii <hoolyworkacc@gmail.com>
2024-02-26 10:56:12 +0200

    rg-52: * minor spelling mistake

--- backend/src/bundles/open-ai/constants/prompts.ts
+++ backend/src/bundles/open-ai/constants/prompts.ts
@@ -1,6 +1,6 @@
 const Prompts = {
     RESUME_SCORE: `You are a resume reviewer. Give this resume a score from 1 to 100,
-    providing a brief overview of your thoughts. Decrease a score by a lot if resume doenst contain essential
+    providing a brief overview of your thoughts. Decrease a score by a lot if resume doesn't contain essential
     information, such as Experience, Education, Technical Skills, and Personal Information.
     Return a json containing the score and overview. The resume is attached`,
 };
