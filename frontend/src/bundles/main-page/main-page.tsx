d5a5da84c5e49fe0838adcf61900d61e418e0db1
Valentyn13 <valentyn13git@gmail.com>
2024-02-26 08:14:54 +0200

    rg-61: + useEffect and spinner to MainPage

--- frontend/src/bundles/main-page/main-page.tsx
+++ frontend/src/bundles/main-page/main-page.tsx
@@ -1,6 +1,12 @@
-import { Header, NavTabs } from '../common/components/components';
+import { signIn } from '../auth/store/actions';
+import { Header, NavTabs, Spinner } from '../common/components/components';
 import { UserProfile } from '../common/components/layout/header/user-profile/user-profile';
 import { AppRoute } from '../common/enums/app-route.enum';
+import {
+    useAppDispatch,
+    useAppSelector,
+    useEffect,
+} from '../common/hooks/hooks';
 import { Home } from '../home/pages/home';
 
 const navbarItems = [
@@ -9,11 +15,42 @@ const navbarItems = [
 ];
 
 const MainPage = (): JSX.Element => {
+    const dispatch = useAppDispatch();
+    const { user, dataStatus } = useAppSelector((state) => state.auth);
+
+    useEffect(() => {
+        void dispatch(
+            signIn({ email: 'gbottoms1@arizona.edu', password: 'pxlxvUyyUjE' }),
+        );
+    }, [dispatch]);
+
     return (
         <>
+            {dataStatus === 'pending' && (
+                <div
+                    style={{
+                        position: 'absolute',
+                        width: '100%',
+                        height: '100%',
+                        backgroundColor: 'whitesmoke',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        zIndex: 100,
+                    }}
+                >
+                    <Spinner />
+                </div>
+            )}
             <Header>
                 <NavTabs items={navbarItems} />
-                <UserProfile image="/src/assets/img/mock-avatar.png" />
+                <UserProfile
+                    image={
+                        user
+                            ? user.user_profile.avatar
+                            : '/src/assets/img/mock-avatar.png'
+                    }
+                />
             </Header>
             <Home />
         </>
