6bc5e3fadd6b37fd7ac6d59cf02118e5e0884e89
Nazarii <hoolyworkacc@gmail.com>
2024-02-26 14:41:10 +0200

    rg-68: + public unsubscribe path

--- backend/src/common/server-application/constants/public-routes.ts
+++ backend/src/common/server-application/constants/public-routes.ts
@@ -1,4 +1,8 @@
-import { ApiPath, AuthApiPath } from 'shared/build/index.js';
+import {
+    ApiPath,
+    AuthApiPath,
+    EmailSubscriptionsApiPath,
+} from 'shared/build/index.js';
 
 const API_PREFIX = '/api/v1';
 
@@ -6,6 +10,7 @@ const publicRoutes = {
     [`${AuthApiPath.SIGN_IN}`]: `${API_PREFIX}${ApiPath.AUTH}${AuthApiPath.SIGN_IN}`,
     [`${AuthApiPath.SIGN_UP}`]: `${API_PREFIX}${ApiPath.AUTH}${AuthApiPath.SIGN_UP}`,
     [`${AuthApiPath.TOKEN}`]: `${API_PREFIX}${ApiPath.AUTH}${AuthApiPath.TOKEN}`,
+    [`${EmailSubscriptionsApiPath.UNSUBSCRIBE}`]: `${API_PREFIX}${ApiPath.EMAIL_SUBSCRIPTIONS}${EmailSubscriptionsApiPath.UNSUBSCRIBE}`,
 };
 
 export { publicRoutes };
