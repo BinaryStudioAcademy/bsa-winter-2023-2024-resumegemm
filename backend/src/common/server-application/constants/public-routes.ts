ca722c7488c7badad9b82d8d7d6da0498b2736bb
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-02-26 18:58:18 +0100

    rg-79: - from public routes

--- backend/src/common/server-application/constants/public-routes.ts
+++ backend/src/common/server-application/constants/public-routes.ts
@@ -1,9 +1,4 @@
-import {
-    ApiPath,
-    AuthApiPath,
-    PDFApiPath,
-    RecentlyViewedApiPath,
-} from 'shared/build/index.js';
+import { ApiPath, AuthApiPath, PDFApiPath } from 'shared/build/index.js';
 
 const API_PREFIX = '/api/v1';
 
@@ -12,8 +7,6 @@ const publicRoutes = {
     [`${AuthApiPath.SIGN_UP}`]: `${API_PREFIX}${ApiPath.AUTH}${AuthApiPath.SIGN_UP}`,
     [`${AuthApiPath.TOKEN}`]: `${API_PREFIX}${ApiPath.AUTH}${AuthApiPath.TOKEN}`,
     [`${PDFApiPath.GENERATE}`]: `${API_PREFIX}${ApiPath.PDF}${PDFApiPath.GENERATE}`,
-    [`${RecentlyViewedApiPath.ID}`]: `${API_PREFIX}${ApiPath.RECENTLY_VIEWED}${RecentlyViewedApiPath.ID}`,
-    [`${RecentlyViewedApiPath.ROOT}`]: `${API_PREFIX}${ApiPath.RECENTLY_VIEWED}${RecentlyViewedApiPath.ROOT}`,
 };
 
 export { publicRoutes };
