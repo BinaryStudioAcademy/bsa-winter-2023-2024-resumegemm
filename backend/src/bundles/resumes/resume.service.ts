edffbf26f1d91d46aba651adb8186b0f00435368
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-02-27 15:45:48 +0200

    rg-80: * resume service

--- backend/src/bundles/resumes/resume.service.ts
+++ backend/src/bundles/resumes/resume.service.ts
@@ -1,6 +1,7 @@
 import {
     type ResumeCreateItemRequestDto,
     type ResumeGetAllResponseDto,
+    type ResumeGetItemResponseDto,
     type ResumeUpdateItemRequestDto,
 } from 'shared/build';
 
@@ -8,32 +9,46 @@ import { type Resume } from './types/resume.type';
 import { type IResumeRepository } from './types/resume-repository.type';
 import { type IResumeService } from './types/resume-service.type';
 
-class ResumeService implements IResumeService<Resume> {
+class ResumeService implements IResumeService {
     private resumeRepository: IResumeRepository;
 
     public constructor(resumeRepository: IResumeRepository) {
         this.resumeRepository = resumeRepository;
     }
+
     public async find(id: string): Promise<Resume | undefined> {
         return await this.resumeRepository.find(id);
     }
+
+    public async findWithRelations(
+        id: string,
+    ): Promise<ResumeGetItemResponseDto | undefined> {
+        return await this.resumeRepository.findWithRelations(id);
+    }
+
     public async findAll(): Promise<ResumeGetAllResponseDto> {
         return await this.resumeRepository.findAll();
     }
+
     public async findAllByUserId(
         userId: string,
     ): Promise<ResumeGetAllResponseDto> {
         return await this.resumeRepository.findAllByUserId(userId);
     }
-    public async create(payload: ResumeCreateItemRequestDto): Promise<Resume> {
+
+    public async create(
+        payload: ResumeCreateItemRequestDto,
+    ): Promise<ResumeGetItemResponseDto> {
         return await this.resumeRepository.create(payload);
     }
+
     public async update(
         id: string,
         data: ResumeUpdateItemRequestDto,
-    ): Promise<Resume> {
+    ): Promise<ResumeGetItemResponseDto> {
         return await this.resumeRepository.update(id, data);
     }
+
     public async delete(id: string): Promise<boolean> {
         return await this.resumeRepository.delete(id);
     }
