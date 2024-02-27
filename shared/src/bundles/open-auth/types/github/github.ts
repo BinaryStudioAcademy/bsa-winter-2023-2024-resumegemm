945e2f6097f4cdb62825c5170a3eb65400ad5c40
Icerock10 <dp021190dms@gmail.com>
2024-02-27 20:24:32 +0200

    rg-71: * refactored base oauth types

--- shared/src/bundles/open-auth/types/github/github.ts
+++
@@ -1,5 +0,0 @@
-export { type GithubUserWithProfileRelation } from './github-user-with-profile-relation.type.js';
-export { type OauthUserEntityFields } from './oauth-user-entity-fields.type.js';
-export { type UserGithubDataResponseDto } from './user-github-data-response-dto.type.js';
-export { type UserGithubLoginRequestDto } from './user-github-login-request-dto.type.js';
-export { type UserGithubLoginResponseDto } from './user-github-login-response-dto.type.js';
--- shared/src/bundles/open-auth/types/github/user-github-login-response-dto.type.ts
+++
@@ -1,5 +0,0 @@
-import { type GithubUserWithProfileRelation } from './github-user-with-profile-relation.type.js';
-
-type UserGithubLoginResponseDto = GithubUserWithProfileRelation;
-
-export { type UserGithubLoginResponseDto };
