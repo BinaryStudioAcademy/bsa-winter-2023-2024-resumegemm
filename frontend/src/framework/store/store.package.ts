8dc7a0f771e6838a9926a351755f710386dd9953
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-02-27 10:32:24 +0100

    rg-53: * sort imports

--- frontend/src/framework/store/store.package.ts
+++ frontend/src/framework/store/store.package.ts
@@ -2,16 +2,16 @@ import {
     type AnyAction,
     type MiddlewareArray,
     type ThunkMiddleware,
+    configureStore,
 } from '@reduxjs/toolkit';
-import { configureStore } from '@reduxjs/toolkit';
 
 import { authApi } from '~/bundles/auth/auth.js';
 import { reducer as authReducer } from '~/bundles/auth/store/';
-import { industriesApi } from '~/bundles/industries/industries';
-import { reducer as industriesReducer } from '~/bundles/industries/store/';
 import { AppEnvironment } from '~/bundles/common/enums/enums.js';
 import { reducer as templatesReducer } from '~/bundles/edit-temlate/store/';
 import { templateApi } from '~/bundles/edit-temlate/templates.js';
+import { industriesApi } from '~/bundles/industries/industries';
+import { reducer as industriesReducer } from '~/bundles/industries/store/';
 import { reducer as usersReducer } from '~/bundles/users/store/';
 import { userApi } from '~/bundles/users/users.js';
 import { type IConfig } from '~/framework/config/config.js';
