36c1e0c3aa8f7bf6ab045070b4868d615e693011
Icerock10 <dp021190dms@gmail.com>
2024-02-27 20:26:54 +0200

    rg-71: + google enums

--- shared/src/bundles/open-auth/enums/open-auth-api-get-user-url.enum.ts
+++ shared/src/bundles/open-auth/enums/open-auth-api-get-user-url.enum.ts
@@ -1,5 +1,6 @@
 const OpenAuthApiGetUserUrl = {
     GITHUB: 'https://api.github.com/user',
-};
+    GOOGLE: 'https://www.googleapis.com/oauth2/v1/userinfo',
+} as const;
 
 export { OpenAuthApiGetUserUrl };
