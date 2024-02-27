b2c716210efc9ed3bd72d255119e15d4abc26f22
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-02-27 15:48:32 +0200

    rg-80: * resume model

--- backend/src/bundles/resumes/resume.model.ts
+++ backend/src/bundles/resumes/resume.model.ts
@@ -7,11 +7,26 @@ import {
     DatabaseTableName,
 } from '~/common/database/database.js';
 
+import {
+    ContactsModel,
+    EducationModel,
+    ExperienceModel,
+    PersonalInformationModel,
+    TechnicalSkillModel,
+} from './content/content';
+
 class ResumeModel extends AbstractModel {
     public 'resumeTitle': string;
     public 'image': string;
     public 'userId': string;
     public 'templateId': string;
+    public 'deletedAt': string | null;
+
+    public education?: EducationModel[];
+    public experience?: ExperienceModel[];
+    public technicalSkills?: TechnicalSkillModel[];
+    public contacts?: ContactsModel;
+    public personalInformation?: PersonalInformationModel;
 
     public static override get tableName(): typeof DatabaseTableName.RESUMES {
         return DatabaseTableName.RESUMES;
@@ -28,11 +43,51 @@ class ResumeModel extends AbstractModel {
                 },
             },
             templates: {
-                relation: Model.HasManyRelation,
+                relation: Model.HasOneRelation,
                 modelClass: TemplateModel,
                 join: {
                     from: `${DatabaseTableName.RESUMES}.templateId`,
                     to: `${DatabaseTableName.TEMPLATES}.id`,
+                },
+            },
+            education: {
+                relation: Model.HasManyRelation,
+                modelClass: EducationModel,
+                join: {
+                    from: `${DatabaseTableName.RESUMES}.id`,
+                    to: `${DatabaseTableName.EDUCATION}.resumeId`,
+                },
+            },
+            experience: {
+                relation: Model.HasManyRelation,
+                modelClass: ExperienceModel,
+                join: {
+                    from: `${DatabaseTableName.RESUMES}.id`,
+                    to: `${DatabaseTableName.EXPERIENCE}.resumeId`,
+                },
+            },
+            technicalSkills: {
+                relation: Model.HasManyRelation,
+                modelClass: TechnicalSkillModel,
+                join: {
+                    from: `${DatabaseTableName.RESUMES}.id`,
+                    to: `${DatabaseTableName.TECHNICAL_SKILLS}.resumeId`,
+                },
+            },
+            contacts: {
+                relation: Model.HasOneRelation,
+                modelClass: ContactsModel,
+                join: {
+                    from: `${DatabaseTableName.RESUMES}.id`,
+                    to: `${DatabaseTableName.CONTACT_DETAILS}.resumeId`,
+                },
+            },
+            personalInformation: {
+                relation: Model.HasOneRelation,
+                modelClass: PersonalInformationModel,
+                join: {
+                    from: `${DatabaseTableName.RESUMES}.id`,
+                    to: `${DatabaseTableName.PERSONAL_INFORMATION}.resumeId`,
                 },
             },
         };
