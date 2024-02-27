8c6d5cedba6db9b7d6f10d8e49bf9be5cf7b1dda
Valentyn13 <valentyn13git@gmail.com>
2024-02-27 09:03:00 +0200

    rg-81: + useEffect with getUser action to App

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -1,4 +1,5 @@
 import reactLogo from '~/assets/img/react.svg';
+import { getUser } from '~/bundles/auth/store/actions';
 import { Link, RouterOutlet } from '~/bundles/common/components/components.js';
 import { AppRoute } from '~/bundles/common/enums/enums.js';
 import {
@@ -27,6 +28,10 @@ const App: React.FC = () => {
             void dispatch(userActions.loadAll());
         }
     }, [isRoot, dispatch]);
+
+    useEffect(() => {
+        void dispatch(getUser());
+    }, [dispatch]);
 
     return (
         <>
