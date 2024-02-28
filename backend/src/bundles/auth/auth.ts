cec768efaca832f6fe931440ca0f4f67bdc35dba
Icerock10 <dp021190dms@gmail.com>
2024-02-29 00:27:19 +0200

    rg-87: + open auth service

--- backend/src/bundles/auth/auth.ts
+++ backend/src/bundles/auth/auth.ts
@@ -1,3 +1,4 @@
+import { openAuthService } from '~/bundles/oauth/oauth.js';
 import { userService } from '~/bundles/users/users.js';
 import { logger } from '~/common/logger/logger.js';
 
@@ -5,6 +6,6 @@ import { AuthController } from './auth.controller.js';
 import { AuthService } from './auth.service.js';
 
 const authService = new AuthService(userService);
-const authController = new AuthController(logger, authService);
+const authController = new AuthController(logger, authService, openAuthService);
 
 export { authController, authService };
