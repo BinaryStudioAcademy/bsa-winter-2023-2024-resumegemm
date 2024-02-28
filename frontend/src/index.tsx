7bb26f853faf2669261fa0003fac4aafc30a181a
Valentyn13 <valentyn13git@gmail.com>
2024-02-28 11:50:15 +0200

    rg-81: * routes structure

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -16,13 +16,11 @@ import { ToastProvider } from '~/bundles/toast/components/toast-provider';
 import { store } from '~/framework/store/store';
 
 import { PrivateRoute } from './bundles/common/components/private-route/private-route';
+import { PublicRoute } from './bundles/common/components/publick-route/public-route';
 import { LandingPage } from './bundles/landing-page/landing-page';
 import { NotFoundPage } from './bundles/not-found-page/not-found-page';
 import { PreviewPage } from './bundles/preview/preview';
 import { Profile } from './bundles/users/pages/profile';
-import { StorageKey } from './framework/storage/storage';
-
-const accesToken = window.localStorage.getItem(StorageKey.ACCESS_TOKEN);
 
 createRoot(document.querySelector('#root') as HTMLElement).render(
     <StrictMode>
@@ -45,37 +43,28 @@ createRoot(document.querySelector('#root') as HTMLElement).render(
                                     element: <LandingPage />,
                                 },
                                 {
-                                    path: AppRoute.SIGN_IN,
-                                    element: (
-                                        <PrivateRoute
-                                            redirectPath={AppRoute.ROOT}
-                                            statement={!accesToken}
-                                        >
-                                            <Auth />
-                                        </PrivateRoute>
-                                    ),
+                                    path: AppRoute.ROOT,
+                                    element: <PublicRoute />,
+                                    children: [
+                                        {
+                                            path: AppRoute.SIGN_IN,
+                                            element: <Auth />,
+                                        },
+                                        {
+                                            path: AppRoute.SIGN_UP,
+                                            element: <Auth />,
+                                        },
+                                    ],
                                 },
                                 {
-                                    path: AppRoute.SIGN_UP,
-                                    element: (
-                                        <PrivateRoute
-                                            redirectPath={AppRoute.ROOT}
-                                            statement={!accesToken}
-                                        >
-                                            <Auth />
-                                        </PrivateRoute>
-                                    ),
-                                },
-                                {
-                                    path: AppRoute.PROFILE,
-                                    element: (
-                                        <PrivateRoute
-                                            statement={Boolean(accesToken)}
-                                            redirectPath={AppRoute.ROOT}
-                                        >
-                                            <Profile />
-                                        </PrivateRoute>
-                                    ),
+                                    path: AppRoute.ROOT,
+                                    element: <PrivateRoute />,
+                                    children: [
+                                        {
+                                            path: AppRoute.PROFILE,
+                                            element: <Profile />,
+                                        },
+                                    ],
                                 },
                             ],
                         },
