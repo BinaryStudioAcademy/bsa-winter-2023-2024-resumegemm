03ef3389ac60c6e1beec52703d18ce285106e245
Valentyn13 <valentyn13git@gmail.com>
2024-02-28 11:46:23 +0200

    rg-81: + PublicRoute component

---
+++ frontend/src/bundles/common/components/publick-route/public-route.tsx
@@ -0,0 +1,17 @@
+import { type FC } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+
+import { StorageKey } from '~/framework/storage/storage';
+
+import { AppRoute } from '../../enums/app-route.enum';
+
+const PublicRoute: FC = () => {
+    const accesToken = window.localStorage.getItem(StorageKey.ACCESS_TOKEN);
+    if (accesToken) {
+        return <Navigate to={AppRoute.ROOT} />;
+    }
+
+    return <Outlet />;
+};
+
+export { PublicRoute };
