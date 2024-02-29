af16374f63ec94a8e4825c0cb3061f98912255c9
Valentyn13 <valentyn13git@gmail.com>
2024-02-29 12:22:26 +0200

    rg-61: + input and sort-button

--- frontend/src/bundles/home/components/panel-container/panel-container.tsx
+++ frontend/src/bundles/home/components/panel-container/panel-container.tsx
@@ -1,4 +1,11 @@
 import clsx from 'clsx';
+import { useCallback } from 'react';
+import { type SortType } from 'shared/build';
+
+import { Icon, Input } from '~/bundles/common/components/components';
+import { IconInput } from '~/bundles/common/components/icon-input/icon-input';
+import { SortButton } from '~/bundles/common/components/sort-button/sort-button';
+import { IconName, IconSize } from '~/bundles/common/enums/enums';
 
 import styles from './styles.module.scss';
 
@@ -13,9 +20,30 @@ const PanelContainer: React.FC<Properties> = ({
     name,
     classname,
 }: Properties) => {
+    const sortHandle = useCallback((sortMethod: SortType): void => {
+        // TODO: handle sort action
+        sortMethod;
+    }, []);
+
     return (
         <div className={clsx(styles.panel_container, classname)}>
-            <div className={styles.panel_container__top_bar}>{name}</div>
+            <div className={styles.panel_container__top_bar}>
+                {name}
+                <div className={styles.panel_container__options_wrapper}>
+                    <IconInput
+                        prependedIcon={
+                            <Icon
+                                size={IconSize.MEDIUM}
+                                name={IconName.SEARCH}
+                            />
+                        }
+                        input={
+                            <Input className={styles.panel_container__input} />
+                        }
+                    />
+                    <SortButton onSort={sortHandle}>Sort</SortButton>
+                </div>
+            </div>
             <div className={styles.panel_container__content}>{children}</div>
         </div>
     );
