1c00d071e455b128b29645c4eb74a8510a366d27
Vladyslav Lazarenko <ezio2506@gmail.com>
2024-02-29 21:03:23 +0200

    rg-33: - removed unused methods

--- backend/src/common/files/file.service.ts
+++ backend/src/common/files/file.service.ts
@@ -1,43 +1,24 @@
-import  { type IService } from '../interfaces/service.interface.js';
 import { type IFileUploadClient } from './interfaces/file-upload-client.interface.js';
-import  { type FileUploadRequestDto } from './types/file-upload-request-dto.type.js';
-import  { type FileUploadResponseDto } from './types/file-upload-response-dto.type.js';
+import { type FileUploadRequestDto } from './types/file-upload-request-dto.type.js';
+import { type FileUploadResponseDto } from './types/file-upload-response-dto.type.js';
 
-class FileService  {
+class FileService {
     private fileUploadClient: IFileUploadClient;
 
-    public constructor(
-      fileUploadClient: IFileUploadClient,
-    ) {
-      this.fileUploadClient = fileUploadClient;
+    public constructor(fileUploadClient: IFileUploadClient) {
+        this.fileUploadClient = fileUploadClient;
     }
 
     public async create(
-      fileBuffer: FileUploadRequestDto,
+        fileBuffer: FileUploadRequestDto,
     ): Promise<FileUploadResponseDto> {
-      const uploadedFileUrl = await this.fileUploadClient.upload(
-        fileBuffer.buffer,
-        fileBuffer.contentType,
-      );
+        const uploadedFileUrl = await this.fileUploadClient.upload(
+            fileBuffer.buffer,
+            fileBuffer.contentType,
+        );
 
-      return { url: uploadedFileUrl };
+        return { url: uploadedFileUrl };
     }
+}
 
-    public find(): ReturnType<IService['find']> {
-      return Promise.resolve(null);
-    }
-
-    public findAll(): ReturnType<IService['findAll']> {
-      return Promise.resolve({ items: [] });
-    }
-
-    public update(): ReturnType<IService['update']> {
-      return Promise.resolve(null);
-    }
-
-    public delete(): ReturnType<IService['delete']> {
-      return Promise.resolve(true);
-    }
-  }
-
-  export { FileService };
+export { FileService };
