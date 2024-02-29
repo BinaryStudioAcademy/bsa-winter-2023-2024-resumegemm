0feb2a7e792e5c550342ea6fe3fa626d39e1f81f
Valentyn13 <valentyn13git@gmail.com>
2024-02-29 16:50:18 +0200

    rg-61: + userAvatar helper

--- frontend/src/bundles/main-page/main-page.tsx
+++ frontend/src/bundles/main-page/main-page.tsx
@@ -1,9 +1,8 @@
-import { type UserWithProfileRelation } from 'shared/build';
-
 import { getUser } from '../auth/store/actions';
 import { Header, NavTabs, Spinner } from '../common/components/components';
 import { UserProfile } from '../common/components/layout/header/user-profile/user-profile';
 import { AppRoute } from '../common/enums/app-route.enum';
+import { getUserAvatart } from '../common/helpers/get-user-avatar';
 import {
     useAppDispatch,
     useAppSelector,
@@ -14,15 +13,8 @@ import styles from './styles.module.scss';
 
 const navbarItems = [
     { label: 'Home', path: AppRoute.HOME },
-    { label: 'Preview', path: AppRoute.PREVIEW },
+    { label: 'Templates', path: AppRoute.TEMPLATES },
 ];
-
-const getUserAvatart = (user: UserWithProfileRelation | null): string => {
-    if (user?.user_profile.avatar) {
-        return user.user_profile.avatar;
-    }
-    return '/src/assets/img/mock-avatar.png';
-};
 
 const MainPage = (): JSX.Element => {
     const dispatch = useAppDispatch();
