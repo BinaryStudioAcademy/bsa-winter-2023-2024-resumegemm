352c4b932b2893350c6183df07fe9c5d0e12d077
RevolveStrm <revolvebattlerage@gmail.com>
2024-02-29 11:51:31 +0200

    rg-83: * updated public routes with new route

--- backend/src/common/server-application/constants/public-routes.ts
+++ backend/src/common/server-application/constants/public-routes.ts
@@ -4,6 +4,7 @@ import {
     PaymentApiPath,
     PDFApiPath,
     RecentlyViewedApiPath,
+    StripeEventsApiPath,
 } from 'shared/build/index.js';
 
 const API_PREFIX = '/api/v1';
@@ -15,6 +16,7 @@ const publicRoutes = {
     [`${PaymentApiPath.PRICES}`]: `${API_PREFIX}${ApiPath.PAYMENT}${PaymentApiPath.PRICES}`,
     [`${PaymentApiPath.CONFIG}`]: `${API_PREFIX}${ApiPath.PAYMENT}${PaymentApiPath.CONFIG}`,
     [`${PaymentApiPath.CREATE_SUBSCRIPTION}`]: `${API_PREFIX}${ApiPath.PAYMENT}${PaymentApiPath.CREATE_SUBSCRIPTION}`,
+    [`${StripeEventsApiPath.WEBHOOKS}`]: `${API_PREFIX}${ApiPath.WEBHOOKS}`,
     [`${PDFApiPath.GENERATE}`]: `${API_PREFIX}${ApiPath.PDF}${PDFApiPath.GENERATE}`,
     [`${RecentlyViewedApiPath.ID}`]: `${API_PREFIX}${ApiPath.RECENTLY_VIEWED}${RecentlyViewedApiPath.ID}`,
     [`${RecentlyViewedApiPath.ROOT}`]: `${API_PREFIX}${ApiPath.RECENTLY_VIEWED}${RecentlyViewedApiPath.ROOT}`,
