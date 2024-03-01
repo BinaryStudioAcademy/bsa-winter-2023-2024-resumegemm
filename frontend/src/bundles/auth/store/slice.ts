2f88e4bea6cd70505fd64e9157b279e3488e7c7a
Nazarii <hoolyworkacc@gmail.com>
2024-03-01 14:45:20 +0200

    rg-68: + state updating on email requests

--- frontend/src/bundles/auth/store/slice.ts
+++ frontend/src/bundles/auth/store/slice.ts
@@ -1,8 +1,13 @@
-import { createSlice, isAnyOf } from '@reduxjs/toolkit';
+import { type PayloadAction, createSlice, isAnyOf } from '@reduxjs/toolkit';
 import { type UserWithRelations } from 'shared/build/index.js';
 
 import { DataStatus } from '~/bundles/common/enums/enums.js';
 import { type ValueOf } from '~/bundles/common/types/types.js';
+import {
+    subscribe,
+    unsubscribe,
+} from '~/bundles/email-subscription/store/actions.js';
+import { type EmailSubscription } from '~/bundles/email-subscription/types/types.js';
 
 import { getUser, signIn, signUp } from './actions.js';
 
@@ -21,6 +26,21 @@ const { reducer, actions, name } = createSlice({
     name: 'auth',
     reducers: {},
     extraReducers(builder) {
+        builder.addCase(
+            subscribe.fulfilled,
+            (state, action: PayloadAction<EmailSubscription>) => {
+                if (state.user) {
+                    state.user.emailSubscription = action.payload;
+                }
+            },
+        );
+
+        builder.addCase(unsubscribe.fulfilled, (state) => {
+            if (state.user) {
+                state.user.emailSubscription = null;
+            }
+        });
+
         builder.addMatcher(
             isAnyOf(signUp.pending, signIn.pending, getUser.pending),
             (state) => {
