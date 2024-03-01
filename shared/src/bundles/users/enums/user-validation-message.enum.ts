de6272a4635d45b17b52f3366e9d280a922afe67
Maria Petrukhnova <kurasava90@gmail.com>
2024-03-01 12:19:39 +0200

    rg-91: + fix validation on first and last name to restrict min and max length

--- shared/src/bundles/users/enums/user-validation-message.enum.ts
+++ shared/src/bundles/users/enums/user-validation-message.enum.ts
@@ -1,6 +1,10 @@
 enum UserValidationMessage {
     FIRSTNAME_REQUIRE = 'First name is required',
+    FIRSTNAME_SHORT = 'First name length must be at least 2 characters long',
+    FIRSTNAME_LONG = 'First name length must be less than or equal to 50 characters long',
     LASTNAME_REQUIRE = 'Last name is required',
+    LASTNAME_SHORT = 'Last name length must be at least 2 characters long',
+    LASTNAME_LONG = 'Last name length must be less than or equal to 50 characters long',
     EMAIL_REQUIRE = 'Email is required',
     EMAIL_WRONG = 'Email is wrong',
     PASSWORD_REQUIRED = 'Password is required',
