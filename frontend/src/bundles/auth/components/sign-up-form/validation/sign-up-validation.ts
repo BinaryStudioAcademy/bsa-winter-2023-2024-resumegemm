f8a45081dcc61d620eb92a15dcabdb4f0a2e98fd
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-03-01 02:54:53 +0100

    rg-98: + validation

--- frontend/src/bundles/auth/components/sign-up-form/validation/sign-up-validation.ts
+++ frontend/src/bundles/auth/components/sign-up-form/validation/sign-up-validation.ts
@@ -32,9 +32,10 @@ const userSignUpValidationFrontend = joi.object<
             'string.email': UserValidationMessage.EMAIL_WRONG,
             'string.empty': UserValidationMessage.EMAIL_REQUIRE,
         }),
-    password: joi.string().trim().regex(/^\S*$/).required().messages({
+    password: joi.string().trim().max(64).regex(/^\S*$/).required().messages({
         'string.empty': UserValidationMessage.PASSWORD_REQUIRED,
-        'string.pattern.base': UserValidationMessage.PASSWORD_NO_SPACES,
+        'string.pattern.base': UserValidationMessage.PASSWORD_INVALID,
+        'string.max': UserValidationMessage.PASSWORD_INVALID,
     }),
     confirm_password: joi
         .string()
