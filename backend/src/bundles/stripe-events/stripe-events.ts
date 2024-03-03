7fa8055131585baa42f0e46c41c8d3c033130ce0
RevolveStrm <revolvebattlerage@gmail.com>
2024-03-03 20:48:00 +0200

    rg-83: * added new subscription plans repository for service

--- backend/src/bundles/stripe-events/stripe-events.ts
+++ backend/src/bundles/stripe-events/stripe-events.ts
@@ -1,9 +1,19 @@
+import { SubscriptionPlanModel } from '~/bundles/stripe-events/models/subscription-plan.model.js';
+import { SubscriptionPlanRepository } from '~/bundles/stripe-events/repositories/subscription-plan.repository.js';
 import { StripeEventsController } from '~/bundles/stripe-events/stripe-events.controller.js';
 import { StripeEventsService } from '~/bundles/stripe-events/stripe-events.service.js';
 import { config } from '~/common/config/config.js';
 import { logger } from '~/common/logger/logger.js';
 
-const stripeEventsService = new StripeEventsService(config);
+const subscriptionPlanRepository = new SubscriptionPlanRepository(
+    SubscriptionPlanModel,
+);
+
+const stripeEventsService = new StripeEventsService(
+    config,
+    subscriptionPlanRepository,
+);
+
 const stripeEventsController = new StripeEventsController(
     logger,
     stripeEventsService,
