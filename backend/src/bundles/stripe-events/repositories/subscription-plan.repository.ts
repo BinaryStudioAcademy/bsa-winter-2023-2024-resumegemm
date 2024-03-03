c0786485c8a82c90035d8039ef1f693691c35f1e
RevolveStrm <revolvebattlerage@gmail.com>
2024-03-03 20:33:48 +0200

    rg-83: + added subscription plans repository

---
+++ backend/src/bundles/stripe-events/repositories/subscription-plan.repository.ts
@@ -0,0 +1,43 @@
+import { Guid as guid } from 'guid-typescript';
+
+import { type ISubscriptionPlanRepository } from '~/bundles/stripe-events/interfaces/interfaces.js';
+import {
+    type SubscriptionPlan,
+    type SubscriptionPlanCreateDto,
+} from '~/bundles/stripe-events/types/types.js';
+
+import { type SubscriptionPlanModel } from '../models/subscription-plan.model.js';
+
+class SubscriptionPlanRepository implements ISubscriptionPlanRepository {
+    private subscriptionPlanModel: typeof SubscriptionPlanModel;
+
+    public constructor(subscriptionPlanModel: typeof SubscriptionPlanModel) {
+        this.subscriptionPlanModel = subscriptionPlanModel;
+    }
+
+    public async find(query: {
+        stripePlanId: string;
+    }): Promise<SubscriptionPlan | undefined> {
+        return await this.subscriptionPlanModel.query().findOne(query);
+    }
+
+    public async create(
+        data: SubscriptionPlanCreateDto,
+    ): Promise<SubscriptionPlan | undefined> {
+        const newSubscriptionPlan = { ...data, id: guid.raw() };
+
+        return await this.subscriptionPlanModel
+            .query()
+            .insert(newSubscriptionPlan)
+            .returning('*');
+    }
+
+    public async delete(id: string): Promise<boolean> {
+        const response = await this.subscriptionPlanModel
+            .query()
+            .deleteById(id);
+        return response === 1;
+    }
+}
+
+export { SubscriptionPlanRepository };
