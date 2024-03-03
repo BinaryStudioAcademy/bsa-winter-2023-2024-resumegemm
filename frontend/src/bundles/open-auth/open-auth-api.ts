dfdd4e5061a1cff327a2d2648429a26d0cd8b1a3
Icerock10 <dp021190dms@gmail.com>
2024-03-03 18:55:45 +0200

    rg-75: + disconnect method to oauth api

--- frontend/src/bundles/open-auth/open-auth-api.ts
+++ frontend/src/bundles/open-auth/open-auth-api.ts
@@ -1,4 +1,8 @@
-import { ApiPath } from '~/bundles/common/enums/enums.js';
+import {
+    ApiPath,
+    ContentType,
+    OpenAuthApiPath,
+} from '~/bundles/common/enums/enums.js';
 import { HttpApi } from '~/framework/api/api.js';
 import { type IHttp } from '~/framework/http/http.js';
 import { type IStorage } from '~/framework/storage/storage.js';
@@ -12,6 +16,20 @@ type Constructor = {
 class OpenAuthApi extends HttpApi {
     public constructor({ baseUrl, http, storage }: Constructor) {
         super({ path: ApiPath.OPEN_AUTH, baseUrl, http, storage });
+    }
+
+    public async disconnectSocialMedia(id: string): Promise<boolean> {
+        const response = await this.load(
+            this.getFullEndpoint(`${OpenAuthApiPath.ROOT}${id}`, {}),
+            {
+                method: 'DELETE',
+                contentType: ContentType.JSON,
+                hasAuth: true,
+                payload: JSON.stringify({}),
+            },
+        );
+
+        return await response.json<boolean>();
     }
 }
 
