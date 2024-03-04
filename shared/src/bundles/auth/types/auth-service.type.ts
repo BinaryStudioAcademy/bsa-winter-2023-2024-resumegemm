89041cc2f91cfc656e2d71fee7448c667735aa05
Vladyslav Lazarenko <ezio2506@gmail.com>
2024-03-04 16:31:11 +0200

    rg-63: + new methods

--- shared/src/bundles/auth/types/auth-service.type.ts
+++ shared/src/bundles/auth/types/auth-service.type.ts
@@ -1,8 +1,13 @@
 import {
+    type UserEntityFields,
+    type UserForgotPasswordRequestDto,
+    type UserResetPasswordRequestDto,
+    type UserResetPasswordResponse,
     type UserSignInRequestDto,
     type UserSignInResponseDto,
     type UserSignUpRequestDto,
     type UserSignUpResponseDto,
+    type UserVerifyResetTokenRequestDto,
     type UserWithProfileRelation,
 } from '../../users/users.js';
 import { type EncryptionDataPayload } from './encryption-data-payload.type.js';
@@ -22,6 +27,14 @@ type AuthService = {
     compare(encryptionData: EncryptionDataPayload): Promise<boolean>;
     getUserWithProfile(id: string): Promise<UserWithProfileRelation>;
     verifyToken<T>(token: string, tokenSecret: string): T;
+    verifyResetToken<T>(token: string): T;
+    tokenEqualsEmail(
+        payload: UserVerifyResetTokenRequestDto,
+    ): Promise<UserEntityFields>;
+    createResetToken(email: UserForgotPasswordRequestDto): Promise<string>;
+    resetPassword(
+        payload: UserResetPasswordRequestDto,
+    ): Promise<Omit<UserResetPasswordResponse, 'message'>>;
 };
 
 export { type AuthService };
