6a712e395b6b5267b5dc0ac1da3ce3a24a71756e
Vladyslav Lazarenko <ezio2506@gmail.com>
2024-03-04 16:06:28 +0200

    rg-63: + reset token correct enum

--- backend/src/bundles/auth/enums/auth-message.ts
+++ backend/src/bundles/auth/enums/auth-message.ts
@@ -1,6 +1,7 @@
 enum AuthMessage {
     RESET_TOKEN_SENT = 'Password reset token sent to email',
     PASSWORD_RESET = 'Password has been reset',
+    RESET_TOKEN_CORRECT = 'Reset token is correct',
 }
 
 export { AuthMessage };
