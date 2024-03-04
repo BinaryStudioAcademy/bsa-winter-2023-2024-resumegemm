4169939a27241a9cc3192c469aff73b8071e81e7
RevolveStrm <revolvebattlerage@gmail.com>
2024-03-04 18:37:19 +0200

    rg-83: + added new enum for stripe event headers

--- backend/src/bundles/stripe-events/enums/enums.ts
+++ backend/src/bundles/stripe-events/enums/enums.ts
@@ -1,3 +1,4 @@
+export { StripeEventsHeaders } from './stripe-events-headers.enum.js';
 export { StripePlanEvents } from './stripe-plan-events.enum.js';
 export { StripePriceEvents } from './stripe-price-events.enum.js';
 export { StripeProductEvents } from './stripe-product-events.enum.js';
