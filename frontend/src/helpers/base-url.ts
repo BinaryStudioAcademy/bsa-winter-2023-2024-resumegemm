7489e367bac8ba45993652e094943ffb1e732f00
Maxik <mpoplavskiy38380@gmail.com>
2024-03-04 21:17:09 +0200

    rg-69: * rework baseUrl

--- frontend/src/helpers/base-url.ts
+++ frontend/src/helpers/base-url.ts
@@ -1,7 +1,11 @@
-const baseUrl = (): string =>
-    window.location.protocol +
-    '//' +
-    window.location.hostname +
-    (window.location.port ? ':' + window.location.port : '');
+const baseUrl = (): string => {
+    const url = window.location.protocol + '//' + window.location.hostname;
+
+    if (window.location.port) {
+        return url + ':' + window.location.port;
+    }
+
+    return url;
+};
 
 export { baseUrl };
