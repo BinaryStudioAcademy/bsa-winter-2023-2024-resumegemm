1f974a5e4c5d52f74187836f1e315dcb250a7aca
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-03-05 12:29:21 +0100

    rg-133: + recentlyViewedApi

---
+++ frontend/src/bundles/recently-viewed/recently-viewed-api.ts
@@ -0,0 +1,40 @@
+import { ApiPath, ContentType } from '~/bundles/common/enums/enums.js';
+import { HttpApi } from '~/framework/api/api.js';
+import { type IHttp } from '~/framework/http/http.js';
+import { type IStorage } from '~/framework/storage/storage.js';
+
+import { RecentlyViewedApiPath } from './enums/enums.js';
+import { type RecentlyViewedTemplatesResponseDto } from './types/types.js';
+
+type Constructor = {
+    baseUrl: string;
+    http: IHttp;
+    storage: IStorage;
+};
+
+class RecentlyViewedApi extends HttpApi {
+    public constructor({ baseUrl, http, storage }: Constructor) {
+        super({ path: ApiPath.RECENTLY_VIEWED, baseUrl, http, storage });
+    }
+
+    public async getRecentlyViewedTemplates(
+        limit = '10',
+    ): Promise<RecentlyViewedTemplatesResponseDto[]> {
+        const query = `?limit=${limit}`;
+        const response = await this.load(
+            this.getFullEndpoint(
+                `${RecentlyViewedApiPath.RECENTLY_VIEWED_TEMPLATES}${query}`,
+                {},
+            ),
+            {
+                method: 'GET',
+                contentType: ContentType.JSON,
+                hasAuth: true,
+            },
+        );
+
+        return await response.json<RecentlyViewedTemplatesResponseDto[]>();
+    }
+}
+
+export { RecentlyViewedApi };
