05e0e992bf152a23e13de3e8836f17d8c43e9b61
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-03-06 20:35:14 +0100

    rg-42: + resume type

--- backend/src/bundles/recently-viewed/types/recently-viewed-resumes-query-result.type.ts
+++ backend/src/bundles/recently-viewed/types/recently-viewed-resumes-query-result.type.ts
@@ -1,11 +1,10 @@
+import { type Resume } from 'shared/build/index.js';
+
 import { type RecentlyViewedModel } from '../recently-viewed.model';
 
 interface RecentlyViewedResumesQueryResult extends RecentlyViewedModel {
     count: number;
-    resumes: {
-        id: string;
-        userId: string;
-    };
+    resumes: Resume;
 }
 
 export { type RecentlyViewedResumesQueryResult };
