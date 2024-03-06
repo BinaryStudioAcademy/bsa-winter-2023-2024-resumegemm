d7c61d82bcf2a662a4616b8de9b708f10eeb0f75
Icerock10 <dp021190dms@gmail.com>
2024-03-07 00:10:54 +0200

    rg-154: * message for invalid password

--- shared/src/enums/exception-message.enum.ts
+++ shared/src/enums/exception-message.enum.ts
@@ -3,7 +3,7 @@ enum ExceptionMessage {
     INVALID_EMAIL = 'Invalid email. Please check and try again.',
     USER_EXISTS = 'Such a user exists.',
     USER_NOT_FOUND = 'User was not found.',
-    INVALID_PASSWORD = 'Invalid password.',
+    INVALID_PASSWORD = 'Invalid password. Please check and try again.',
     AUTH_FAILED = 'Authorization failed, please sign in again.',
     INVALID_REFRESH_TOKEN = 'Refresh token has expired or invalid.',
 }
