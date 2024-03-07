b0ae9ab1ab5941a1108d39b84d0fa6e1d8434b28
Nazarii <hoolyworkacc@gmail.com>
2024-03-08 01:35:18 +0200

    rg-134: * move type to other dir

--- frontend/src/bundles/templates-page/types/types.ts
+++ frontend/src/bundles/templates-page/types/types.ts
@@ -1,7 +1,12 @@
 export { TemplateBlockTitles } from 'shared/build/bundles/templates/enums/enums.js';
 export {
+    type Block,
+    type Container,
+    type CSSProperties,
+    type Item,
     type TemplateBlockSettings,
     type TemplateDto,
+    type TemplateSettings,
     type TemplateUpdateItemRequestDto,
     type TemplateUpdateItemResponseDto,
 } from 'shared/build/bundles/templates/templates.js';
