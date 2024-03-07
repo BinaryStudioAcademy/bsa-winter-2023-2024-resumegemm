9cfcb597f307a79f235768206ec3b850674e97dd
ykotsiuba <jurok3x@gmail.com>
2024-03-07 17:35:14 +0200

    rg-85: * Changed email template

--- backend/src/bundles/stripe-events/constants/email-payload.ts
+++ backend/src/bundles/stripe-events/constants/email-payload.ts
@@ -1,55 +1,58 @@
 const EmailPayload = {
     HTML: `
-    <html>
-        <head>
-            <style>
-                body {
-                    font-family: 'Arial', sans-serif;
-                    background-color: #f0f0f0;
-                    text-align: center;
-                    padding: 20px;
-                }
+<html>
+    <head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap">
+        <style>
+            body {
+                font-family: 'Poppins', sans-serif;
+                background-color: #768FBE;
+                text-align: center;
+                padding: 20px;
+            }
 
-                .container {
-                    max-width: 600px;
-                    margin: 0 auto;
-                    background-color: #ffffff;
-                    padding: 20px;
-                    border-radius: 10px;
-                    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-                }
+            .container {
+                max-width: 600px;
+                margin: 0 auto;
+            }
+            
+            .content {
+                background-color: #FDF3D3;
+                padding: 10px 40px;
+                border-radius: 40px;
+                text-align: justify;
+            }
+            
+            img {
+                margin-bottom: 20px;
+            }
 
-                h1 {
-                    color: #3498db;
-                }
+            h1 {
+                color: #000;
+                text-align: center;
+            }
 
-                p {
-                    color: #555;
-                }
+            p {
+                color: #555;
+            }
 
-                .footer {
-                    margin-top: 20px;
-                    color: #888;
-                }
-            </style>
-        </head>
-        <body>
-            <div class="container">
-                <h1>Congratulations {name} on successful subscription!</h1>
-                <p>We're excited to have you on board.</p>
-                <p>Thank you for choosing our service.</p>
-                <p>Your subscription details:</p>
-                <ul>
-                    <li><strong>Start Date:</strong> {startDate}</li>
-                    <li><strong>End Date:</strong> {endDate}</li>
-                </ul>
-                <div class="footer">
-                    <p>Best regards,</p>
-                    <p>Resumegemm</p>
-                </div>
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <img src="https://resumegemm.space/assets/logo-2c9d3d5a.svg" alt="Logo icon">
+            <div class="content">
+                <h1>You're now subscribed!</h1>
+                <p><strong>Dear {name},</strong></p>
+                <p>Welcome to ResumeGemm! We're delighted to have you on board as a subscriber.</p>
+                <p>Your subscription is now active, <strong>starting from {startDate}</strong> and <strong>ending on {endDate}</strong>. During this time, you'll enjoy exclusive access to premium content, early updates, and special offers tailored just for you.</p>
+                <p>If you have any questions or need assistance, feel free to contact our support team.</p>
+                <p>Thank you for choosing ResumeGemm!</p>
             </div>
-        </body>
-    </html>
+        </div>
+        
+    </body>
+</html>
 `,
 };
 
