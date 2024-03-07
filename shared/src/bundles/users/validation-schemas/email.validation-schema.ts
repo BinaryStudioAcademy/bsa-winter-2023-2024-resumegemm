e64090e9edf2c9e6c7a6643ddd64f4b65c07c569
Vladyslav Lazarenko <ezio2506@gmail.com>
2024-03-07 11:41:16 +0200

    rg-63: * updated validation rules

--- shared/src/bundles/users/validation-schemas/email.validation-schema.ts
+++ shared/src/bundles/users/validation-schemas/email.validation-schema.ts
@@ -3,6 +3,7 @@ import joi from 'joi';
 import {
     type UserForgotPasswordRequestDto,
     UserValidationMessage,
+    UserValidationRule,
 } from '../users.js';
 
 const emailValidationSchema = joi.object<UserForgotPasswordRequestDto, true>({
@@ -15,9 +16,24 @@ const emailValidationSchema = joi.object<UserForgotPasswordRequestDto, true>({
             },
         })
         .required()
+        .custom((value, helpers) => {
+            const [localPart, domain] = value.split('@')[0];
+
+            if (localPart.length <= 1) {
+                return helpers.error('string.emailInvalid');
+            }
+
+            if (domain.length > UserValidationRule.EMAIL_MAX_DOMAIN_LENGTH) {
+                return helpers.error('domainLengthInvalid');
+            }
+
+            return value;
+        })
         .messages({
             'string.email': UserValidationMessage.EMAIL_WRONG,
             'string.empty': UserValidationMessage.EMAIL_REQUIRE,
+            'domainLengthInvalid': UserValidationMessage.EMAIL_INVALID,
+            'string.emailInvalid': UserValidationMessage.EMAIL_INVALID,
         }),
 });
 
--- shared/src/bundles/users/validation-schemas/restore-password.validation-schema.ts
+++ shared/src/bundles/users/validation-schemas/restore-password.validation-schema.ts
@@ -1,15 +1,23 @@
 import joi from 'joi';
 
 import { type UserResetPassword } from '../types/user-reset-password.type.js';
-import { UserValidationMessage } from '../users.js';
+import { UserValidationMessage, UserValidationRule } from '../users.js';
 
 const passwordValidationSchema = joi.object<UserResetPassword, true>({
-    password: joi.string().regex(/^\S*$/).min(8).max(64).messages({
-        'string.empty': UserValidationMessage.PASSWORD_REQUIRED,
-        'string.min': UserValidationMessage.PASSWORD_INVALID,
-        'string.max': UserValidationMessage.PASSWORD_INVALID,
-        'string.pattern.base': UserValidationMessage.PASSWORD_INVALID,
-    }),
+    password: joi
+        .string()
+        .regex(
+            /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!"#$%&'()*+,./:;<=>?@[\\\]^_`{|}~])(?=\S{8,64}$).*$/,
+        )
+        .min(UserValidationRule.PASSWORD_MIN_LENGTH)
+        .max(UserValidationRule.PASSWORD_MAX_LENGTH)
+        .required()
+        .messages({
+            'string.empty': UserValidationMessage.PASSWORD_REQUIRED,
+            'string.pattern.base': UserValidationMessage.PASSWORD_INVALID,
+            'string.min': UserValidationMessage.PASSWORD_INVALID,
+            'string.max': UserValidationMessage.PASSWORD_INVALID,
+        }),
     confirm_password: joi
         .string()
         .required()
