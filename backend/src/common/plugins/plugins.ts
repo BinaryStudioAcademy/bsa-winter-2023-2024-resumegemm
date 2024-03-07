ed8e4b6355741da8f1755906ee714a2d492efc40
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-03-07 17:16:19 +0200

    rg-137: * exports related to linkedin

--- backend/src/common/plugins/plugins.ts
+++ backend/src/common/plugins/plugins.ts
@@ -1,3 +1,4 @@
 export { authorization as authorizationPlugin } from './authorization/authorization-plugin.js';
+export { oauthLinkedinPlugin } from './open-authorization/oauth-linkedin-plugin.js';
 export { oauthCallbackHandler } from './open-authorization/oauth-plugin.js';
 export { preParsing as preParsingPlugin } from './preparsing/preparsing-plugin.js';
