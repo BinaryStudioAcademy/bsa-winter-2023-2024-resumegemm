5030ff55a82618e31f2315b71d0edaffeae36192
Nazarii <hoolyworkacc@gmail.com>
2024-03-08 02:19:23 +0200

    rg-134: + temp button for template creating

--- frontend/src/bundles/home/pages/templates.tsx
+++ frontend/src/bundles/home/pages/templates.tsx
@@ -1,19 +1,41 @@
 import clsx from 'clsx';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
-import { Icon, Input } from '~/bundles/common/components/components';
+import {
+    Icon,
+    Input,
+    RegularButton,
+} from '~/bundles/common/components/components';
 import { IconInput } from '~/bundles/common/components/icon-input/icon-input';
-import { IconName, IconSize } from '~/bundles/common/enums/enums';
+import { AppRoute, IconName, IconSize } from '~/bundles/common/enums/enums';
+import { useAppDispatch, useCallback } from '~/bundles/common/hooks/hooks';
 import { useLoadTemplates } from '~/bundles/common/hooks/use-load-templates/use-load-templates.hook';
+import { createTemplate } from '~/bundles/edit-temlate/store/actions';
 import {
     HomeTopSection,
     ResumeCard,
     TemplateSection,
 } from '~/bundles/home/components/components';
+import { type TemplateDto } from '~/bundles/templates-page/types/types';
 
 import styles from './styles.module.scss';
 
 const Templates: React.FC = () => {
     const { templates } = useLoadTemplates();
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
+
+    const handleCreateTemplate = useCallback(() => {
+        dispatch(createTemplate())
+            .unwrap()
+            .then((newTemplate: TemplateDto) => {
+                navigate(`${AppRoute.TEMPLATE_EDITOR}/${newTemplate.id}`);
+            })
+            .catch(() => {
+                toast.error('Failed to create template');
+            });
+    }, [dispatch, navigate]);
 
     return (
         <div className={clsx(styles.layout, styles.templates_layout)}>
@@ -30,6 +52,9 @@ const Templates: React.FC = () => {
                     }
                     className={styles.icon_input_wrapper}
                 />
+                <RegularButton onClick={handleCreateTemplate}>
+                    Create template
+                </RegularButton>
             </HomeTopSection>
             <TemplateSection
                 name="Templates"
