16ceb29c9f858c40b40add99fb1f7afb6224d39e
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-03-08 18:29:12 +0200

    rg-180: * migration add id generation to database

--- backend/src/migrations/20240304112515_add_id_generation_to_database.ts
+++ backend/src/migrations/20240304112515_add_id_generation_to_database.ts
@@ -50,6 +50,17 @@ async function up(knex: Knex): Promise<void> {
             .onUpdate(RelationRule.CASCADE)
             .onDelete(RelationRule.SET_NULL);
     });
+    await knex.schema.alterTable(
+        DatabaseTableName.OAUTH_CONNECTIONS,
+        (table) => {
+            table
+                .uuid(DatabaseColumnName.NEW_USER_ID)
+                .references(DatabaseColumnName.NEW_ID)
+                .inTable(DatabaseTableName.USERS)
+                .onUpdate(RelationRule.CASCADE)
+                .onDelete(RelationRule.SET_NULL);
+        },
+    );
 
     await knex.schema.alterTable(DatabaseTableName.RESUMES, (table) => {
         table.dropForeign([DatabaseColumnName.USER_ID]);
@@ -81,6 +92,22 @@ async function up(knex: Knex): Promise<void> {
             DatabaseColumnName.USER_ID,
         );
     });
+    await knex.schema.alterTable(
+        DatabaseTableName.OAUTH_CONNECTIONS,
+        (table) => {
+            table.dropForeign([DatabaseColumnName.USER_ID]);
+        },
+    );
+    await knex.schema.alterTable(
+        DatabaseTableName.OAUTH_CONNECTIONS,
+        (table) => {
+            table.dropColumn(DatabaseColumnName.USER_ID);
+            table.renameColumn(
+                DatabaseColumnName.NEW_USER_ID,
+                DatabaseColumnName.USER_ID,
+            );
+        },
+    );
 
     await knex.schema.alterTable(DatabaseTableName.USERS, (table) => {
         table.dropPrimary();
@@ -333,6 +360,7 @@ async function up(knex: Knex): Promise<void> {
     await knex(DatabaseTableName.RESUMES).update({
         new_id: knex.raw('??', [DatabaseColumnName.ID]),
     });
+
     await knex.schema.alterTable(DatabaseTableName.REVIEWS, (table) => {
         table
             .uuid(DatabaseColumnName.NEW_RESUME_ID)
@@ -395,6 +423,41 @@ async function up(knex: Knex): Promise<void> {
             .onUpdate(RelationRule.CASCADE)
             .onDelete(RelationRule.SET_NULL);
     });
+    await knex.schema.alterTable(DatabaseTableName.CERTIFICATION, (table) => {
+        table
+            .uuid(DatabaseColumnName.NEW_RESUME_ID)
+            .references(DatabaseColumnName.NEW_ID)
+            .inTable(DatabaseTableName.RESUMES)
+            .onUpdate(RelationRule.CASCADE)
+            .onDelete(RelationRule.SET_NULL);
+    });
+    await knex.schema.alterTable(DatabaseTableName.LANGUAGES, (table) => {
+        table
+            .uuid(DatabaseColumnName.NEW_RESUME_ID)
+            .references(DatabaseColumnName.NEW_ID)
+            .inTable(DatabaseTableName.RESUMES)
+            .onUpdate(RelationRule.CASCADE)
+            .onDelete(RelationRule.SET_NULL);
+    });
+    await knex.schema.alterTable(DatabaseTableName.CUSTOM_SECTIONS, (table) => {
+        table
+            .uuid(DatabaseColumnName.NEW_RESUME_ID)
+            .references(DatabaseColumnName.NEW_ID)
+            .inTable(DatabaseTableName.RESUMES)
+            .onUpdate(RelationRule.CASCADE)
+            .onDelete(RelationRule.SET_NULL);
+    });
+    await knex.schema.alterTable(
+        DatabaseTableName.RESUME_SHARE_LINK,
+        (table) => {
+            table
+                .uuid(DatabaseColumnName.NEW_RESUME_ID)
+                .references(DatabaseColumnName.NEW_ID)
+                .inTable(DatabaseTableName.RESUMES)
+                .onUpdate(RelationRule.CASCADE)
+                .onDelete(RelationRule.SET_NULL);
+        },
+    );
 
     await knex.schema.alterTable(DatabaseTableName.REVIEWS, (table) => {
         table.dropForeign([DatabaseColumnName.RESUME_ID]);
@@ -458,6 +521,41 @@ async function up(knex: Knex): Promise<void> {
             DatabaseColumnName.RESUME_ID,
         );
     });
+    await knex.schema.alterTable(DatabaseTableName.CERTIFICATION, (table) => {
+        table.dropForeign([DatabaseColumnName.RESUME_ID]);
+        table.dropColumn(DatabaseColumnName.RESUME_ID);
+        table.renameColumn(
+            DatabaseColumnName.NEW_RESUME_ID,
+            DatabaseColumnName.RESUME_ID,
+        );
+    });
+    await knex.schema.alterTable(DatabaseTableName.LANGUAGES, (table) => {
+        table.dropForeign([DatabaseColumnName.RESUME_ID]);
+        table.dropColumn(DatabaseColumnName.RESUME_ID);
+        table.renameColumn(
+            DatabaseColumnName.NEW_RESUME_ID,
+            DatabaseColumnName.RESUME_ID,
+        );
+    });
+    await knex.schema.alterTable(DatabaseTableName.CUSTOM_SECTIONS, (table) => {
+        table.dropForeign([DatabaseColumnName.RESUME_ID]);
+        table.dropColumn(DatabaseColumnName.RESUME_ID);
+        table.renameColumn(
+            DatabaseColumnName.NEW_RESUME_ID,
+            DatabaseColumnName.RESUME_ID,
+        );
+    });
+    await knex.schema.alterTable(
+        DatabaseTableName.RESUME_SHARE_LINK,
+        (table) => {
+            table.dropForeign([DatabaseColumnName.RESUME_ID]);
+            table.dropColumn(DatabaseColumnName.RESUME_ID);
+            table.renameColumn(
+                DatabaseColumnName.NEW_RESUME_ID,
+                DatabaseColumnName.RESUME_ID,
+            );
+        },
+    );
 
     await knex.schema.alterTable(DatabaseTableName.RESUMES, (table) => {
         table.dropPrimary();
@@ -562,6 +660,162 @@ async function up(knex: Knex): Promise<void> {
     await knex.schema.alterTable(DatabaseTableName.USER_TEMPLATES, (table) => {
         table.primary([DatabaseColumnName.ID]);
     });
+    // =====================================================================
+    // CERTIFICATION
+    await knex.schema.alterTable(DatabaseTableName.CERTIFICATION, (table) => {
+        table
+            .uuid(DatabaseColumnName.NEW_ID)
+            .defaultTo(knex.raw(UUID_GENERATION_FUNCTION));
+    });
+    await knex(DatabaseTableName.CERTIFICATION).update({
+        new_id: knex.raw('??', [DatabaseColumnName.ID]),
+    });
+    await knex.schema.alterTable(DatabaseTableName.CERTIFICATION, (table) => {
+        table.dropPrimary();
+    });
+    await knex.schema.alterTable(DatabaseTableName.CERTIFICATION, (table) => {
+        table.dropColumn(DatabaseColumnName.ID);
+        table.renameColumn(DatabaseColumnName.NEW_ID, DatabaseColumnName.ID);
+    });
+    await knex.schema.alterTable(DatabaseTableName.CERTIFICATION, (table) => {
+        table.primary([DatabaseColumnName.ID]);
+    });
+    // =====================================================================
+    // LANGUAGES
+    await knex.schema.alterTable(DatabaseTableName.LANGUAGES, (table) => {
+        table
+            .uuid(DatabaseColumnName.NEW_ID)
+            .defaultTo(knex.raw(UUID_GENERATION_FUNCTION));
+    });
+    await knex(DatabaseTableName.LANGUAGES).update({
+        new_id: knex.raw('??', [DatabaseColumnName.ID]),
+    });
+    await knex.schema.alterTable(DatabaseTableName.LANGUAGES, (table) => {
+        table.dropPrimary();
+    });
+    await knex.schema.alterTable(DatabaseTableName.LANGUAGES, (table) => {
+        table.dropColumn(DatabaseColumnName.ID);
+        table.renameColumn(DatabaseColumnName.NEW_ID, DatabaseColumnName.ID);
+    });
+    await knex.schema.alterTable(DatabaseTableName.LANGUAGES, (table) => {
+        table.primary([DatabaseColumnName.ID]);
+    });
+    // =====================================================================
+    // CUSTOM_SECTIONS
+    await knex.schema.alterTable(DatabaseTableName.CUSTOM_SECTIONS, (table) => {
+        table
+            .uuid(DatabaseColumnName.NEW_ID)
+            .defaultTo(knex.raw(UUID_GENERATION_FUNCTION));
+    });
+    await knex(DatabaseTableName.CUSTOM_SECTIONS).update({
+        new_id: knex.raw('??', [DatabaseColumnName.ID]),
+    });
+    await knex.schema.alterTable(DatabaseTableName.CUSTOM_SECTIONS, (table) => {
+        table.dropPrimary();
+    });
+    await knex.schema.alterTable(DatabaseTableName.CUSTOM_SECTIONS, (table) => {
+        table.dropColumn(DatabaseColumnName.ID);
+        table.renameColumn(DatabaseColumnName.NEW_ID, DatabaseColumnName.ID);
+    });
+    await knex.schema.alterTable(DatabaseTableName.CUSTOM_SECTIONS, (table) => {
+        table.primary([DatabaseColumnName.ID]);
+    });
+    // =====================================================================
+    // RESUME_SHARE_LINK
+    await knex.schema.alterTable(
+        DatabaseTableName.RESUME_SHARE_LINK,
+        (table) => {
+            table
+                .uuid(DatabaseColumnName.NEW_ID)
+                .unique()
+                .defaultTo(knex.raw(UUID_GENERATION_FUNCTION));
+        },
+    );
+    await knex(DatabaseTableName.RESUME_SHARE_LINK).update({
+        new_id: knex.raw('??', [DatabaseColumnName.ID]),
+    });
+
+    await knex.schema.alterTable(
+        DatabaseTableName.RESUME_SHARE_ACCESS,
+        (table) => {
+            table
+                .uuid(DatabaseColumnName.NEW_RESUME_SHARE_LINK_ID)
+                .references(DatabaseColumnName.NEW_ID)
+                .inTable(DatabaseTableName.RESUME_SHARE_LINK)
+                .onUpdate(RelationRule.CASCADE)
+                .onDelete(RelationRule.SET_NULL);
+        },
+    );
+
+    await knex.schema.alterTable(
+        DatabaseTableName.RESUME_SHARE_ACCESS,
+        (table) => {
+            table.dropForeign([DatabaseColumnName.RESUME_SHARE_LINK_ID]);
+            table.dropColumn(DatabaseColumnName.RESUME_SHARE_LINK_ID);
+            table.renameColumn(
+                DatabaseColumnName.NEW_RESUME_SHARE_LINK_ID,
+                DatabaseColumnName.RESUME_SHARE_LINK_ID,
+            );
+        },
+    );
+
+    await knex.schema.alterTable(
+        DatabaseTableName.RESUME_SHARE_LINK,
+        (table) => {
+            table.dropPrimary();
+        },
+    );
+    await knex.schema.alterTable(
+        DatabaseTableName.RESUME_SHARE_LINK,
+        (table) => {
+            table.dropColumn(DatabaseColumnName.ID);
+            table.renameColumn(
+                DatabaseColumnName.NEW_ID,
+                DatabaseColumnName.ID,
+            );
+        },
+    );
+    await knex.schema.alterTable(
+        DatabaseTableName.RESUME_SHARE_LINK,
+        (table) => {
+            table.primary([DatabaseColumnName.ID]);
+        },
+    );
+    // =====================================================================
+    // RESUME_SHARE_ACCESS
+    await knex.schema.alterTable(
+        DatabaseTableName.RESUME_SHARE_ACCESS,
+        (table) => {
+            table
+                .uuid(DatabaseColumnName.NEW_ID)
+                .defaultTo(knex.raw(UUID_GENERATION_FUNCTION));
+        },
+    );
+    await knex(DatabaseTableName.RESUME_SHARE_ACCESS).update({
+        new_id: knex.raw('??', [DatabaseColumnName.ID]),
+    });
+    await knex.schema.alterTable(
+        DatabaseTableName.RESUME_SHARE_ACCESS,
+        (table) => {
+            table.dropPrimary();
+        },
+    );
+    await knex.schema.alterTable(
+        DatabaseTableName.RESUME_SHARE_ACCESS,
+        (table) => {
+            table.dropColumn(DatabaseColumnName.ID);
+            table.renameColumn(
+                DatabaseColumnName.NEW_ID,
+                DatabaseColumnName.ID,
+            );
+        },
+    );
+    await knex.schema.alterTable(
+        DatabaseTableName.RESUME_SHARE_ACCESS,
+        (table) => {
+            table.primary([DatabaseColumnName.ID]);
+        },
+    );
 }
 
 async function down(knex: Knex): Promise<void> {
@@ -712,6 +966,93 @@ async function down(knex: Knex): Promise<void> {
     await knex.schema.alterTable(DatabaseTableName.USER_TEMPLATES, (table) => {
         table.primary([DatabaseColumnName.ID]);
     });
+
+    await knex.schema.alterTable(DatabaseTableName.CERTIFICATION, (table) => {
+        table.dropPrimary();
+    });
+    await knex.schema.alterTable(DatabaseTableName.CERTIFICATION, (table) => {
+        table.uuid(DatabaseColumnName.ID).notNullable().alter();
+    });
+    await knex.schema.alterTable(DatabaseTableName.CERTIFICATION, (table) => {
+        table.primary([DatabaseColumnName.ID]);
+    });
+
+    await knex.schema.alterTable(DatabaseTableName.LANGUAGES, (table) => {
+        table.dropPrimary();
+    });
+    await knex.schema.alterTable(DatabaseTableName.LANGUAGES, (table) => {
+        table.uuid(DatabaseColumnName.ID).notNullable().alter();
+    });
+    await knex.schema.alterTable(DatabaseTableName.LANGUAGES, (table) => {
+        table.primary([DatabaseColumnName.ID]);
+    });
+
+    await knex.schema.alterTable(DatabaseTableName.CUSTOM_SECTIONS, (table) => {
+        table.dropPrimary();
+    });
+    await knex.schema.alterTable(DatabaseTableName.CUSTOM_SECTIONS, (table) => {
+        table.uuid(DatabaseColumnName.ID).notNullable().alter();
+    });
+    await knex.schema.alterTable(DatabaseTableName.CUSTOM_SECTIONS, (table) => {
+        table.primary([DatabaseColumnName.ID]);
+    });
+
+    await knex.schema.alterTable(
+        DatabaseTableName.RESUME_SHARE_LINK,
+        (table) => {
+            table.dropPrimary();
+        },
+    );
+    await knex.schema.alterTable(
+        DatabaseTableName.RESUME_SHARE_LINK,
+        (table) => {
+            table.uuid(DatabaseColumnName.ID).notNullable().alter();
+        },
+    );
+    await knex.schema.alterTable(
+        DatabaseTableName.RESUME_SHARE_LINK,
+        (table) => {
+            table.primary([DatabaseColumnName.ID]);
+        },
+    );
+
+    await knex.schema.alterTable(
+        DatabaseTableName.OAUTH_CONNECTIONS,
+        (table) => {
+            table.dropPrimary();
+        },
+    );
+    await knex.schema.alterTable(
+        DatabaseTableName.OAUTH_CONNECTIONS,
+        (table) => {
+            table.uuid(DatabaseColumnName.ID).notNullable().alter();
+        },
+    );
+    await knex.schema.alterTable(
+        DatabaseTableName.OAUTH_CONNECTIONS,
+        (table) => {
+            table.primary([DatabaseColumnName.ID]);
+        },
+    );
+
+    await knex.schema.alterTable(
+        DatabaseTableName.RESUME_SHARE_ACCESS,
+        (table) => {
+            table.dropPrimary();
+        },
+    );
+    await knex.schema.alterTable(
+        DatabaseTableName.RESUME_SHARE_ACCESS,
+        (table) => {
+            table.uuid(DatabaseColumnName.ID).notNullable().alter();
+        },
+    );
+    await knex.schema.alterTable(
+        DatabaseTableName.RESUME_SHARE_ACCESS,
+        (table) => {
+            table.primary([DatabaseColumnName.ID]);
+        },
+    );
 }
 
 export { down, up };
