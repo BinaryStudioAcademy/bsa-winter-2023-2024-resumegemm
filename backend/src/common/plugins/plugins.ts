0a33a4e8c2422222ed7d6f0a0a1ebc8330ae815a
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-03-08 11:05:00 +0200

    rg-137: * exports plugins

--- backend/src/common/plugins/plugins.ts
+++ backend/src/common/plugins/plugins.ts
@@ -1,4 +1,3 @@
 export { authorization as authorizationPlugin } from './authorization/authorization-plugin.js';
-export { oauthLinkedinPlugin } from './open-authorization/oauth-linkedin-plugin.js';
 export { oauthCallbackHandler } from './open-authorization/oauth-plugin.js';
 export { preParsing as preParsingPlugin } from './preparsing/preparsing-plugin.js';
