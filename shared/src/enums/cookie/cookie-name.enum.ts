19fdf1a52a356d5b6dd2973d67529162c6e18350
Icerock10 <dp021190dms@gmail.com>
2024-03-08 10:15:42 +0200

    rg-75: + cookie name enum

--- shared/src/enums/cookie/cookie-name.enum.ts
+++ shared/src/enums/cookie/cookie-name.enum.ts
@@ -1,8 +1,8 @@
-const CookieName = {
-    REFRESH_TOKEN: 'refresh_token',
-    ACCESS_TOKEN: 'access_token',
-    OAUTH_TOKEN: 'oauth_token',
-    REDIRECT_PATH: 'redirect_path',
-};
+enum CookieName {
+    REFRESH_TOKEN = 'refresh_token',
+    ACCESS_TOKEN = 'access_token',
+    OAUTH_TOKEN = 'oauth_token',
+    REDIRECT_PATH = 'redirect_path',
+}
 
 export { CookieName };
