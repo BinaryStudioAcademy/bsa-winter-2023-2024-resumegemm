712c61d805f5fcc4617ca280e30ccf92fe49d6e1
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-03-09 00:04:58 +0200

    rg-180: * resume share access link  seeds

---
+++ backend/src/seeds/seed-data/resume-shared-access-seed.ts
@@ -0,0 +1,16 @@
+const resumeSharedAccessSeed = [
+    {
+        resume_share_access_ip: '10b3f3e0-3e3e-4e3e-8e3e-3e3e3e3e3e3e',
+    },
+    {
+        resume_share_access_ip: '20b3f3e0-3e3e-4e3e-8e3e-3e3e3e3e3e3e',
+    },
+    {
+        resume_share_access_ip: '30b3f3e0-3e3e-4e3e-8e3e-3e3e3e3e3e3e',
+    },
+    {
+        resume_share_access_ip: '40b3f3e0-3e3e-4e3e-8e3e-3e3e3e3e3e3e',
+    },
+];
+
+export { resumeSharedAccessSeed };
