94fc6443cc0ad3660a65d46e2b0144d25556e448
Nazarii <hoolyworkacc@gmail.com>
2024-03-08 16:02:38 +0200

    rg-134: - unnessesary if statement

--- frontend/src/bundles/edit-template/components/template-container/template-container.tsx
+++ frontend/src/bundles/edit-template/components/template-container/template-container.tsx
@@ -6,11 +6,12 @@ type Properties = Container;
 const TemplateContainer: React.FC<Properties> = ({ id, blocks, styles }) => {
     return (
         <div key={id} style={styles}>
-            {blocks.map((block) => {
-                if (block.enabled) {
-                    return <TemplateBlock key={block.id} {...block} />;
-                }
-            })}
+            {blocks.map(
+                (block) =>
+                    block.enabled && (
+                        <TemplateBlock key={block.id} {...block} />
+                    ),
+            )}
         </div>
     );
 };
