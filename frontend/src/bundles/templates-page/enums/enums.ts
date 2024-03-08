d9ca8a9865772e481cf3850ea65bf80fbfce3154
Nazarii <hoolyworkacc@gmail.com>
2024-03-08 02:10:38 +0200

    rg-134: * reexprorts

---
+++ frontend/src/bundles/templates-page/enums/enums.ts
@@ -0,0 +1,1 @@
+export { TemplatesApiPath } from 'shared/build';
--- frontend/src/bundles/templates-page/types/types.ts
+++ frontend/src/bundles/templates-page/types/types.ts
@@ -6,6 +6,7 @@ export {
     type Item,
     type TemplateBlockSettings,
     type TemplateDto,
+    type TemplateGetAllResponseDto,
     type TemplateSettings,
     type TemplateUpdateItemRequestDto,
     type TemplateUpdateItemResponseDto,
