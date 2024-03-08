446697f3a08b950c6ba1ea29d143c8cd14c1f941
Vladyslav Lazarenko <ezio2506@gmail.com>
2024-03-08 15:39:39 +0200

    rg-151: * changed logic a little so it can work with backend

--- frontend/src/bundles/common/components/user-photo-uploader/user-photo-wrapper.tsx
+++ frontend/src/bundles/common/components/user-photo-uploader/user-photo-wrapper.tsx
@@ -1,64 +1,61 @@
 import React from 'react';
 
-import { useAppDispatch } from '~/bundles/common/hooks/hooks.js';
-import { updateUserAvatar } from '~/bundles/profile/store/actions';
+import {
+    useAppDispatch,
+    useAppSelector,
+} from '~/bundles/common/hooks/hooks.js';
+import {
+    deleteUserAvatar,
+    updateUserAvatar,
+} from '~/bundles/profile/store/actions';
 
 import { useCallback, useState } from '../../hooks/hooks';
 import styles from './styles.module.scss';
+import { type ImageBlob } from './types/image-blob.type';
 import { UserPhotoMockup } from './user-photo-mockup';
 import { PhotoUploaderModal } from './user-photo-modal';
 import { UserPhotoPreview } from './user-photo-preview';
 
 const UserPhotoWrapper: React.FC = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
-    const [currentPhoto, setCurrentPhoto] = useState('');
 
-    // need future redux integration
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const [currentImage, setCurrentImage] = useState<{
-        src: string;
-        blob: Blob;
-    } | null>(null);
+    const avatar = useAppSelector(({ auth }) => {
+        return auth.user?.userProfile.avatar;
+    });
 
     const dispatch = useAppDispatch();
 
-    // need future redux integration
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const onHandleImage = useCallback(
-        (event: React.ChangeEvent<HTMLInputElement>) => {
-            const reader = new FileReader();
-            const [newImage] = event.target.files ?? [];
+    const onHandlePhoto = useCallback(
+        (payload: ImageBlob | null) => {
+            if (payload) {
+                const formData = new FormData();
+                formData.append('file', payload.blob);
 
-            reader.addEventListener('load', () => {
-                setCurrentImage({
-                    src: reader.result as string,
-                    blob: newImage,
-                });
-            });
-            reader.readAsDataURL(newImage);
-            const formData = new FormData();
-            formData.append('file', newImage);
-            void dispatch(updateUserAvatar(formData));
+                void dispatch(updateUserAvatar(formData));
+            }
         },
-        [setCurrentImage, dispatch],
+        [dispatch],
     );
+
+    const onCurrentPhotoHandler = useCallback(() => {
+        void dispatch(deleteUserAvatar());
+    }, [dispatch]);
 
     return (
         <section className={styles.uploader_wrapper__section}>
-            {!currentPhoto && (
+            {avatar ? (
+                <UserPhotoPreview
+                    currentPhoto={avatar}
+                    onToggleModal={setIsModalOpen}
+                    onHandleCurrentPhoto={onCurrentPhotoHandler}
+                />
+            ) : (
                 <UserPhotoMockup onClickUpload={setIsModalOpen} />
             )}
             {isModalOpen && (
                 <PhotoUploaderModal
                     onToggleModal={setIsModalOpen}
-                    onHandleCurrentPhoto={setCurrentPhoto}
-                />
-            )}
-            {currentPhoto && (
-                <UserPhotoPreview
-                    currentPhoto={currentPhoto}
-                    onToggleModal={setIsModalOpen}
-                    onHandleCurrentPhoto={setCurrentPhoto}
+                    onHandleCurrentPhoto={onHandlePhoto}
                 />
             )}
         </section>
