6c1f0db1d12db5af714905161272e292a8a4e2b0
Valentyn13 <valentyn13git@gmail.com>
2024-03-08 10:50:38 +0200

    rg-130: - junk logic in app

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -1,20 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
-import { updateAccessToken } from '~/bundles/auth/store/actions';
 import { actions as authActions } from '~/bundles/auth/store/auth.store';
 import { RouterOutlet } from '~/bundles/common/components/components.js';
 import { updatePageTab } from '~/bundles/common/helpers/update-page-tab';
 import { useAppDispatch } from '~/bundles/common/hooks/use-app-dispatch/use-app-dispatch.hook';
-import { StorageKey } from '~/framework/storage/storage';
 
 const App: React.FC = () => {
     const location = useLocation();
     const dispatch = useAppDispatch();
-
-    const [accessToken] = useState(
-        localStorage.getItem(StorageKey.ACCESS_TOKEN),
-    );
 
     useEffect(() => {
         void dispatch(authActions.getUser());
@@ -23,14 +17,6 @@ const App: React.FC = () => {
     useEffect(() => {
         updatePageTab();
     }, [location]);
-
-    useEffect(() => {
-        const expirationCheckInterval = setInterval(() => {
-            void dispatch(updateAccessToken());
-        }, 1000 * 60 * 60 * 23);
-
-        return () => clearInterval(expirationCheckInterval);
-    }, [accessToken, dispatch]);
 
     return <RouterOutlet />;
 };
