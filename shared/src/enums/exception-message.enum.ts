24d55e03124664858eeaf40d6b9b0d474a40fc37
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-03-09 17:09:37 +0100

    rg-62: + exception messages

--- shared/src/enums/exception-message.enum.ts
+++ shared/src/enums/exception-message.enum.ts
@@ -1,10 +1,12 @@
 enum ExceptionMessage {
     EMAIL_TAKEN = 'Email is already taken.',
+    EMAIL_CONFIRM = 'Please, confirm email.',
     USER_EXISTS = 'Such a user exists.',
     USER_NOT_FOUND = 'User was not found.',
     INVALID_PASSWORD = 'Invalid password.',
     AUTH_FAILED = 'Authorization failed, please sign in again.',
     INVALID_REFRESH_TOKEN = 'Refresh token has expired or invalid.',
+    INVALID_EMAIL_CONFIRM_TOKEN = 'Email confirm token has expired or invalid.',
 }
 
 export { ExceptionMessage };
