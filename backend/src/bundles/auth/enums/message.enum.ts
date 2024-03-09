8885d40049976b655cbca0643e31aef85ec68a8b
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-03-09 17:45:34 +0100

    rg-62: + enums

--- backend/src/bundles/auth/enums/message.enum.ts
+++ backend/src/bundles/auth/enums/message.enum.ts
@@ -1,6 +1,7 @@
-enum userMessages {
+enum EmailConfirmMessages {
     EMAIL_CONFIRMED = 'Email confirmed successfully',
     SUCCESSFULLY_REGISTERED = 'You have successfully registered on ResumeGemm',
+    EMAIL_CONFIRM_TOKEN_IS_CORRECT = 'Email confirmed token is correct',
 }
 
-export { userMessages };
+export { EmailConfirmMessages };
