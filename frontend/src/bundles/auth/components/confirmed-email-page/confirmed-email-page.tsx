1095b7b4a043e86d3df88a6779ee003145684cf0
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-03-09 17:22:30 +0100

    rg-62: + confirm email page

--- frontend/src/bundles/auth/components/confirmed-email-page/confirmed-email-page.tsx
+++ frontend/src/bundles/auth/components/confirmed-email-page/confirmed-email-page.tsx
@@ -1,6 +1,10 @@
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useCallback, useEffect, useState } from 'react';
+import { Link, useNavigate, useSearchParams } from 'react-router-dom';
 
+import { AppRoute } from '~/bundles/common/enums/app-route.enum';
+import { useAppDispatch } from '~/bundles/common/hooks/hooks';
+
+import { actions as authActionCreator } from '../../store/auth.store';
 import { Logo } from '../components';
 import {
     CONFIRM_EMAIL_TIME,
@@ -9,43 +13,85 @@ import {
 import styles from './styles.module.scss';
 
 const EmailConfirmedPage = (): JSX.Element => {
-    const [counter, setCounter] = useState(CONFIRM_EMAIL_TIME);
+    const dispatch = useAppDispatch();
     const navigate = useNavigate();
 
-    useEffect(() => {
-        const timer = setInterval(() => {
-            setCounter((previousCounter) => previousCounter - 1);
-        }, CONFIRM_EMAIL_TIMER_INTERVAL);
+    const [searchParameters] = useSearchParams();
+    const token = searchParameters.get('token');
 
-        if (counter === 0) {
-            clearInterval(timer);
-            navigate('/');
+    const [isEmailConfirmed, setIsEmailConfirmed] = useState(false);
+    const [counter, setCounter] = useState(CONFIRM_EMAIL_TIME);
+
+    const handleConfirmEmail = useCallback(() => {
+        if (token) {
+            void dispatch(
+                authActionCreator.confirmEmail({ emailConfirmToken: token }),
+            );
         }
+        setIsEmailConfirmed(true);
+    }, [token, dispatch]);
 
-        return () => clearInterval(timer);
-    }, [counter, navigate]);
+    useEffect(() => {
+        handleConfirmEmail();
+    }, [handleConfirmEmail]);
+
+    useEffect(() => {
+        if (isEmailConfirmed) {
+            const timer = setInterval(() => {
+                setCounter((previousCounter) => previousCounter - 1);
+            }, CONFIRM_EMAIL_TIMER_INTERVAL);
+
+            if (counter === 0) {
+                clearInterval(timer);
+                navigate(AppRoute.HOME);
+            }
+
+            return () => clearInterval(timer);
+        }
+    }, [isEmailConfirmed, counter, navigate]);
 
     return (
         <section className={styles.confirmed_page__section}>
             <div className={styles.confirmed_page__block}>
-                <div className={styles.confirmed_page__paragraph}>
-                    <h3 className={styles.confirmed_page__title}>
-                        Congratulations! You are successfully signed up on
-                        <div className={styles.confirmed_page__logo_wrapper}>
-                            <Logo />
-                        </div>
-                    </h3>
-                </div>
-                <div className={styles.confirmed_page__paragraph}>
-                    <p className={styles.confirmed_page__text}>
-                        You will be redirected on main page in
-                        <span className={styles.confirmed_page__counter}>
-                            {' '}
-                            {counter}{' '}
-                        </span>
-                        seconds
-                    </p>
-                </div>
+                {isEmailConfirmed ? (
+                    <div className={styles.confirmed_page__paragraph}>
+                        <h3 className={styles.confirmed_page__title}>
+                            Congratulations! You are successfully signed up on
+                            <div
+                                className={styles.confirmed_page__logo_wrapper}
+                            >
+                                <Logo />
+                            </div>
+                        </h3>
+                        <p className={styles.confirmed_page__text}>
+                            You will be redirected to the main page in
+                            <span className={styles.confirmed_page__counter}>
+                                {' '}
+                                {counter}{' '}
+                            </span>
+                            seconds
+                        </p>
+                    </div>
+                ) : (
+                    <div className={styles.confirmed_page__paragraph}>
+                        <h3 className={styles.confirmed_page__title}>
+                            Email Confirmation Failed
+                        </h3>
+                        <p className={styles.confirmed_page__text}>
+                            Email confirmation failed. Please try again later.
+                        </p>
+                        <p className={styles.confirmed_page__text}>
+                            You can{' '}
+                            <Link
+                                to={AppRoute.SIGN_UP}
+                                className={styles.sing_up__link}
+                            >
+                                Sign Up here
+                            </Link>
+                            .
+                        </p>
+                    </div>
+                )}
             </div>
         </section>
     );
