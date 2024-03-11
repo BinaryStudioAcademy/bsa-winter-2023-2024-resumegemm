617164c1df987167ee38969da3a4d97ef65a70ff
Valentyn13 <valentyn13git@gmail.com>
2024-03-11 19:36:35 +0200

    rg-191: * seed names

--- backend/src/seeds/fill-database.ts
+++ backend/src/seeds/fill-database.ts
@@ -23,12 +23,12 @@ import {
 import {
     certificationsSeed,
     contactsSeed,
-    customSectionSeed,
-    educationSeed,
-    experienceSeed,
+    customSectionsSeed,
+    educationsSeed,
+    experiencesSeed,
     imagesSeed,
-    personalInformationSeed,
-    profileSeed,
+    personalInformationsSeed,
+    profilesSeed,
     resumesSeed,
     reviewsSeed,
     technicalSkillsSeed,
@@ -66,7 +66,7 @@ async function seed(knex: Knex): Promise<void> {
 
         // PROFILE
 
-        const profileMappedSeed = profileSeed.map((profile, index) => ({
+        const profileMappedSeed = profilesSeed.map((profile, index) => ({
             ...profile,
             [DatabaseColumnName.ID]: guid.raw(),
             [DatabaseColumnName.AVATAR]: imagesSeed[index].image,
@@ -129,7 +129,7 @@ async function seed(knex: Knex): Promise<void> {
         // EDUCATION
 
         await trx<Education>(DatabaseTableName.EDUCATION)
-            .insert(mapResumeContent(educationSeed))
+            .insert(mapResumeContent(educationsSeed))
             .returning('*');
 
         // CONTACT_DETAILS
@@ -141,7 +141,7 @@ async function seed(knex: Knex): Promise<void> {
         // EXPERIENCE
 
         await trx<Experience>(DatabaseTableName.EXPERIENCE)
-            .insert(mapResumeContent(experienceSeed))
+            .insert(mapResumeContent(experiencesSeed))
             .returning('*');
 
         // // TECHNICAL_SKILLS
@@ -153,7 +153,7 @@ async function seed(knex: Knex): Promise<void> {
         // PERSONAL_INFORMATION
 
         await trx<PersonalInformation>(DatabaseTableName.PERSONAL_INFORMATION)
-            .insert(mapResumeContent(personalInformationSeed))
+            .insert(mapResumeContent(personalInformationsSeed))
             .returning('*');
 
         // CERTIFICATION
@@ -165,7 +165,7 @@ async function seed(knex: Knex): Promise<void> {
         // CUSTOM_SECTION
 
         await trx<CustomSection>(DatabaseTableName.CUSTOM_SECTIONS)
-            .insert(mapResumeContent(customSectionSeed))
+            .insert(mapResumeContent(customSectionsSeed))
             .returning('*');
 
         // USER_TEMPLATES junction table
