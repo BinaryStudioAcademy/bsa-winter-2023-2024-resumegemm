7af2ff9f88380f2e970a921134d212b69c9ea416
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-03-13 00:36:19 +0200

    rg-192: + resume-card component  with styles

--- frontend/src/bundles/home/components/cards/resume-card/resume-card.tsx
+++ frontend/src/bundles/home/components/cards/resume-card/resume-card.tsx
@@ -1,23 +1,47 @@
+import { Icon } from '~/bundles/common/components/components';
+import { IconName, IconSize } from '~/bundles/common/enums/enums';
+
 import styles from './styles.module.scss';
 
 type Properties = {
     title: string;
     image: string;
     subtitle?: string;
+    viewedResume?: number;
 };
 
 const ResumeCard: React.FC<Properties> = ({
     title,
     image,
     subtitle,
+    viewedResume,
 }: Properties) => {
     return (
         <div className={styles.resume_card}>
             <img src={image} alt="Resume" className={styles.resume_card__img} />
             <div className={styles.resume_card__content}>
-                <span className={styles.resume_card__content_title}>
+                <div className={styles.resume_card__content_title}>
                     {title}
-                </span>
+                    {viewedResume && (
+                        <div
+                            className={
+                                styles.resume_card__resume_views_container
+                            }
+                        >
+                            <Icon
+                                size={IconSize.SMALL}
+                                name={IconName.EYE_OPEN}
+                            />
+                            <span
+                                className={
+                                    styles.resume_card__resume_views_number
+                                }
+                            >
+                                {viewedResume}
+                            </span>
+                        </div>
+                    )}
+                </div>
                 {subtitle && (
                     <span className={styles.resume_card__content_text}>
                         {subtitle}
