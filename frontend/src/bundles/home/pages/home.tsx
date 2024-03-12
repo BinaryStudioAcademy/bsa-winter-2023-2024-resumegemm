407ae98ee611d4995e2c9bc7978dfe1a9c37b93b
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-03-13 00:38:16 +0200

    rg-192: + home component

--- frontend/src/bundles/home/pages/home.tsx
+++ frontend/src/bundles/home/pages/home.tsx
@@ -1,4 +1,5 @@
 import mockResume from '~/assets/img/mock-resume.png';
+import { useAppSelector } from '~/bundles/common/hooks/hooks';
 import { useLoadTemplates } from '~/bundles/common/hooks/use-load-templates/use-load-templates.hook';
 import {
     CreateNewCard,
@@ -9,11 +10,46 @@ import {
     ResumeSection,
     TemplateSection,
 } from '~/bundles/home/components/components';
+import { useLoadViewedResumes } from '~/bundles/resume/use-load-viewed-resumes.hook';
 
 import styles from './styles.module.scss';
 
+type AdaptResumeProperties = {
+    id: string;
+    image: string;
+    title: string;
+    resumeTitle?: string;
+    createdAt: string;
+    updatedAt?: string | undefined;
+    deletedAt: string | null;
+    userId: string;
+    templateId: string;
+};
+
+const adaptResume = (resume: AdaptResumeProperties): AdaptResumeProperties => {
+    return {
+        ...resume,
+        title: resume.resumeTitle ?? '',
+    };
+};
+
+const formatDate = (date: string): string => {
+    const unpatedAtDate = new Date(date);
+    const options: Intl.DateTimeFormatOptions = {
+        month: 'short',
+        day: '2-digit',
+    };
+    return new Intl.DateTimeFormat('en-US', options).format(unpatedAtDate);
+};
+
 const Home: React.FC = () => {
+    const { resumes } = useAppSelector((state) => state.resumes);
     const { templates } = useLoadTemplates();
+    const { resumeViewHistory } = useLoadViewedResumes();
+
+    const getViewedResumeCount = (resumeId: string): number => {
+        return resumeViewHistory[resumeId]?.length || 0;
+    };
 
     return (
         <div className={styles.layout}>
@@ -30,11 +66,25 @@ const Home: React.FC = () => {
                 <CreateNewCard />
             </ResumeSection>
             <ResumeSection name="Users' resume">
-                <ResumeCard
-                    title="My Resume"
-                    subtitle="Updated - Jan 25"
-                    image={mockResume}
-                />
+                {resumes.length > 0 &&
+                    resumes.map((resume) => {
+                        const adaptedResume = adaptResume(resume.resume);
+                        const { id, image, title, updatedAt } = adaptedResume;
+                        const viewedResumeCount = getViewedResumeCount(id);
+                        let formattedDate = '';
+                        if (updatedAt) {
+                            formattedDate = formatDate(updatedAt);
+                        }
+                        return (
+                            <ResumeCard
+                                key={id}
+                                title={title}
+                                image={image}
+                                subtitle={`Updated - ${formattedDate}`}
+                                viewedResume={viewedResumeCount}
+                            />
+                        );
+                    })}
             </ResumeSection>
             <TemplateSection name="Templates">
                 {templates.length > 0 &&
