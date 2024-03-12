2d2c57b8692418cd1974eb8a84ef0639987767e8
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-03-12 15:02:10 +0200

    rg-192: + instance resumeApi

---
+++ frontend/src/bundles/resume/resume.ts
@@ -0,0 +1,13 @@
+import { config } from '~/framework/config/config.js';
+import { http } from '~/framework/http/http.js';
+import { storage } from '~/framework/storage/storage.js';
+
+import { ResumeApi } from './resume-api.js';
+
+const resumeApi = new ResumeApi({
+    baseUrl: config.ENV.API.ORIGIN_URL,
+    storage,
+    http,
+});
+
+export { resumeApi };
