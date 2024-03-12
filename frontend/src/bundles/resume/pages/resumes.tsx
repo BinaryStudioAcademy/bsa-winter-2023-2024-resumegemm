af5eb80e0860ff1ef91167f0b16071d4ba3cadf5
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-03-13 01:36:52 +0200

    rg-192: * ressume component

--- frontend/src/bundles/resume/pages/resumes.tsx
+++ frontend/src/bundles/resume/pages/resumes.tsx
@@ -1,20 +1,32 @@
 import { Icon } from '~/bundles/common/components/components';
 import { IconName, IconSize } from '~/bundles/common/enums/enums';
+import { useAppSelector } from '~/bundles/common/hooks/hooks';
 import { ResumePreview } from '~/bundles/resume-preview/components/components';
 
 import { useLoadViewedResumes } from '../use-load-viewed-resumes.hook';
 import styles from './styles.module.scss';
 
 const Resume: React.FC = () => {
-    const { viewedResume } = useLoadViewedResumes();
+    const { resumes } = useAppSelector((state) => state.resumes);
+    const { resumeViewHistory } = useLoadViewedResumes();
+
+    const id = resumes[0]?.resume?.id;
+
+    const getViewedResumeCount = (resumeId: string): number => {
+        return resumeViewHistory[resumeId].length > 0
+            ? resumeViewHistory[resumeId].length
+            : 0;
+    };
+
+    const viewedResumeCount = getViewedResumeCount(id);
 
     return (
         <div>
-            <div>Number of resume views: {viewedResume.details.length}</div>
+            <div>Number of resume views: {viewedResumeCount}</div>
             <div className={styles.resume__views_container}>
                 <Icon size={IconSize.MEDIUM} name={IconName.EYE_OPEN} />
                 <span className={styles.resume__views_number}>
-                    {viewedResume.details.length}
+                    {viewedResumeCount}
                 </span>
             </div>
             <ResumePreview />
