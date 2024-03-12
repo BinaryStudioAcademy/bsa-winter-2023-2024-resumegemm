8c1de127147e99959b48f5f331f71940b1f0e021
Vladyslav Lazarenko <ezio2506@gmail.com>
2024-03-12 21:14:31 +0200

    rg-197: + added forward ref

--- frontend/src/bundles/edit-template/components/template-editor/template-editor.tsx
+++ frontend/src/bundles/edit-template/components/template-editor/template-editor.tsx
@@ -1,3 +1,5 @@
+import { forwardRef } from 'react';
+
 import { type TemplateSettings } from '~/bundles/templates-page/types/types';
 
 import { TemplateContainer } from '../template-container/template-container';
@@ -7,14 +9,22 @@ type Properties = {
     settings: TemplateSettings;
 };
 
-const TemplateEditor: React.FC<Properties> = ({ settings }) => {
-    return (
-        <div style={settings.styles} className={styles.template_editor}>
-            {settings.containers.map((container) => (
-                <TemplateContainer key={container.id} {...container} />
-            ))}
-        </div>
-    );
-};
+const TemplateEditor = forwardRef<HTMLDivElement, Properties>(
+    ({ settings }, reference) => {
+        return (
+            <div
+                style={settings.styles}
+                ref={reference}
+                className={styles.template_editor}
+            >
+                {settings.containers.map((container) => (
+                    <TemplateContainer key={container.id} {...container} />
+                ))}
+            </div>
+        );
+    },
+);
+
+TemplateEditor.displayName = 'TemplateEditor';
 
 export { TemplateEditor };
