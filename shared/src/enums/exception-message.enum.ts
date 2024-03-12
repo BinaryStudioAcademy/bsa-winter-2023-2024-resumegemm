93c17a9d01652344b9b1e5fe72dddcafe6099783
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-03-12 11:57:49 +0100

    rg-62: + exception message

--- shared/src/enums/exception-message.enum.ts
+++ shared/src/enums/exception-message.enum.ts
@@ -1,6 +1,7 @@
 enum ExceptionMessage {
     EMAIL_TAKEN = 'Email is already taken.',
     EMAIL_CONFIRM = 'Please, confirm email.',
+    EMAIL_CONFIRM_TOKEN_NOT_FOUND = 'Email confirm token not found',
     USER_EXISTS = 'Such a user exists.',
     USER_NOT_FOUND = 'User was not found.',
     INVALID_PASSWORD = 'Invalid password.',
