bd8f234555233d7dd5007e58b1d4962e7c6ee6f2
Vladyslav Lazarenko <ezio2506@gmail.com>
2024-03-12 21:17:02 +0200

    rg-197: + validate url helper

--- shared/src/helpers/helpers.ts
+++ shared/src/helpers/helpers.ts
@@ -1,2 +1,3 @@
 export { configureString } from './configure-string/configure-string.helper.js';
+export { validateUrl } from './validate-url/validate-url.helper.js';
 export { getCookie, setCookie } from '~/helpers/cookie/cookie.js';
