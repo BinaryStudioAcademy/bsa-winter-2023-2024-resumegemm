c3f18f8947ad70e584937158dfd9ee19144aeda3
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-03-13 14:21:28 +0200

    rg-192: * home component

--- frontend/src/bundles/home/pages/home.tsx
+++ frontend/src/bundles/home/pages/home.tsx
@@ -10,89 +10,18 @@ import {
     Greeting,
     HomeTopSection,
     ResumeCard,
+    ResumeList,
     ResumeSection,
+    TemplateList,
     TemplateSection,
 } from '~/bundles/home/components/components';
 
 import styles from './styles.module.scss';
 
-type AdaptResumeProperties = {
-    id: string;
-    image: string;
-    title: string;
-    resumeTitle?: string;
-    createdAt: string;
-    updatedAt?: string | undefined;
-    deletedAt: string | null;
-    userId: string;
-    templateId: string;
-};
-
-const adaptResume = (resume: AdaptResumeProperties): AdaptResumeProperties => {
-    return {
-        ...resume,
-        title: resume.resumeTitle ?? '',
-    };
-};
-
-const formatDate = (date: string): string => {
-    const unpatedAtDate = new Date(date);
-    const options: Intl.DateTimeFormatOptions = {
-        month: 'short',
-        day: '2-digit',
-    };
-    return new Intl.DateTimeFormat('en-US', options).format(unpatedAtDate);
-};
-
 const Home: React.FC = () => {
     const { resumes } = useAppSelector((state) => state.resumes);
     const { templates } = useLoadTemplates();
     const { resumeViewHistory } = useLoadViewedResumes();
-
-    const getViewedResumeCount = (resumeId: string): number => {
-        return resumeViewHistory[resumeId]?.length || 0;
-    };
-
-    const renderResumes = (): JSX.Element[] | null => {
-        if (resumes.length === 0) {
-            return null;
-        }
-
-        return resumes.map((resume) => {
-            const adaptedResume = adaptResume(resume.resume);
-            const { id, image, title, updatedAt } = adaptedResume;
-            const viewedResumeCount = getViewedResumeCount(id);
-            let formattedDate = '';
-            if (updatedAt) {
-                formattedDate = formatDate(updatedAt);
-            }
-            return (
-                <ResumeCard
-                    key={id}
-                    title={title}
-                    image={image}
-                    subtitle={`Updated - ${formattedDate}`}
-                    viewedResume={viewedResumeCount}
-                />
-            );
-        });
-    };
-
-    const renderTemplates = (): JSX.Element[] | null => {
-        if (templates.length === 0) {
-            return null;
-        }
-
-        return templates.map((template) => {
-            return (
-                <ResumeCard
-                    key={template.id}
-                    title="My Resume"
-                    image={template.image}
-                />
-            );
-        });
-    };
 
     return (
         <div className={styles.layout}>
@@ -109,10 +38,13 @@ const Home: React.FC = () => {
                 <CreateNewCard />
             </ResumeSection>
             <ResumeSection name="Users' resume">
-                {renderResumes()}
+                <ResumeList
+                    resumes={resumes}
+                    resumeViewHistory={resumeViewHistory}
+                />
             </ResumeSection>
             <TemplateSection name="Templates">
-                {renderTemplates()}
+                <TemplateList templates={templates} />
             </TemplateSection>
         </div>
     );
