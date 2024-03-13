5e15569e4978bc4e4c6312f4d674fa21dfc8a97f
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-03-13 20:58:02 +0100

    rg-62: + check email page to components

--- frontend/src/bundles/auth/components/components.tsx
+++ frontend/src/bundles/auth/components/components.tsx
@@ -1,3 +1,4 @@
+export { CheckEmailPage } from '../components/check-email-page/check-email-page.js';
 export { EmailConfirmationPage } from './confirmed-email-page/email-confirmation-page.js';
 export { EmailConfirmationFailPage } from './email-confirmation-fail-page/email-confirmation-fail-page.js';
 export { EmailConfirmationSuccessPage } from './email-confirmation-success-page/email-confirmation-success-page.js';
