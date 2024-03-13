98eaf0843b484b2fe4672164ee26045e1360bd7c
Valentyn13 <valentyn13git@gmail.com>
2024-03-13 09:40:29 +0200

    rg-206: * Navtabs items on MainPage

--- frontend/src/bundles/main-page/main-page.tsx
+++ frontend/src/bundles/main-page/main-page.tsx
@@ -2,17 +2,12 @@ import { DataStatus } from '~/bundles/common/enums/enums';
 import { SpinnerVariant } from '~/bundles/common/enums/spinner/spinner-variant.enum';
 
 import { Header, NavTabs, Spinner } from '../common/components/components';
+import { headerItems } from '../common/components/layout/header/constants/header-items';
 import { UserProfile } from '../common/components/layout/header/user-profile/user-profile';
-import { AppRoute } from '../common/enums/app-route.enum';
 import { getUserAvatar } from '../common/helpers/get-user-avatar';
 import { useAppSelector } from '../common/hooks/hooks';
 import { Home } from '../home/pages/home';
 import styles from './styles.module.scss';
-
-const navbarItems = [
-    { label: 'Home', path: AppRoute.HOME },
-    { label: 'Templates', path: AppRoute.TEMPLATES },
-];
 
 const MainPage = (): JSX.Element => {
     const { user, dataStatus } = useAppSelector((state) => state.auth);
@@ -25,7 +20,7 @@ const MainPage = (): JSX.Element => {
                 </div>
             )}
             <Header>
-                <NavTabs items={navbarItems} />
+                <NavTabs items={headerItems} />
                 <UserProfile image={getUserAvatar(user)} />
             </Header>
             <Home />
