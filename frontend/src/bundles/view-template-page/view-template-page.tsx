743b476aa5990e569da955454f3bd4802ff2b744
Vladyslav Lazarenko <ezio2506@gmail.com>
2024-03-13 20:34:47 +0200

    rg-208: + created view template page

---
+++ frontend/src/bundles/view-template-page/view-template-page.tsx
@@ -0,0 +1,72 @@
+import { useNavigate, useParams } from 'react-router-dom';
+
+import { Footer, Header, RegularButton } from '../common/components/components';
+import { AppRoute, ButtonType, ButtonVariant } from '../common/enums/enums';
+import {
+    useAppDispatch,
+    useCallback,
+    useEffect,
+    useState,
+} from '../common/hooks/hooks';
+import { TemplateEditor } from '../edit-template/components/template-editor/template-editor';
+import { actions as templateActions } from '../edit-template/store/edit-template.store';
+import { type TemplateDto } from '../templates-page/types/types';
+import { ToastType } from '../toast/enums/show-toast-types.enum';
+import { showToast } from '../toast/helpers/show-toast';
+import styles from './styles.module.scss';
+
+const ViewTemplatePage: React.FC = () => {
+    const { id } = useParams();
+    const [template, setTemplate] = useState<TemplateDto | null>(null);
+    const navigate = useNavigate();
+
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        if (id) {
+            dispatch(templateActions.getTemplateById(id))
+                .unwrap()
+                .then((template) => setTemplate(template))
+                .catch((error) => showToast(error, ToastType.ERROR));
+        }
+    }, [dispatch, id]);
+
+    const editTemplateHandler = useCallback(() => {
+        navigate(`${AppRoute.TEMPLATE_EDITOR}/${template?.id}`);
+    }, [navigate, template]);
+
+    const returnBackHandler = useCallback(() => {
+        navigate(AppRoute.TEMPLATES);
+    }, [navigate]);
+
+    return (
+        <>
+            <Header />
+            <section className={styles.template_section}>
+                {template && (
+                    <TemplateEditor settings={template.templateSettings} />
+                )}
+            </section>
+            <Footer>
+                <div className={styles.footer__controls}>
+                    <RegularButton
+                        type={ButtonType.BUTTON}
+                        variant={ButtonVariant.OUTLINED}
+                        onClick={returnBackHandler}
+                    >
+                        Go back
+                    </RegularButton>
+                    <RegularButton
+                        type={ButtonType.BUTTON}
+                        variant={ButtonVariant.PRIMARY}
+                        onClick={editTemplateHandler}
+                    >
+                        Edit template
+                    </RegularButton>
+                </div>
+            </Footer>
+        </>
+    );
+};
+
+export { ViewTemplatePage };
