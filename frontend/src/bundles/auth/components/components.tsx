a0a538edfaf00b6647782695b3584f478dbe8848
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-03-13 12:36:59 +0100

    rg-62: + exports

--- frontend/src/bundles/auth/components/components.tsx
+++ frontend/src/bundles/auth/components/components.tsx
@@ -1,3 +1,6 @@
+export { EmailConfirmationPage } from './confirmed-email-page/email-confirmation-page.js';
+export { EmailConfirmationFailPage } from './email-confirmation-fail-page/email-confirmation-fail-page.js';
+export { EmailConfirmationSuccessPage } from './email-confirmation-success-page/email-confirmation-success-page.js';
 export { Logo } from './logo/logo';
 export { SignInForm } from './sign-in-form/sign-in-form.js';
 export { SignUpForm } from './sign-up-form/sign-up-form.js';
