31111b7d47d362fcc978a31348408e56f446e287
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-03-13 12:38:08 +0100

    rg-62: * rename pages

---
+++ frontend/src/bundles/auth/components/confirmed-email-page/email-confirmation-page.tsx
@@ -0,0 +1,56 @@
+import { Navigate, useSearchParams } from 'react-router-dom';
+
+import { AppRoute, DataStatus } from '~/bundles/common/enums/enums.js';
+import {
+    useAppDispatch,
+    useCallback,
+    useEffect,
+    useState,
+} from '~/bundles/common/hooks/hooks';
+
+import { actions as authActionCreator } from '../../store/auth.store';
+
+const EmailConfirmationPage = (): JSX.Element => {
+    const dispatch = useAppDispatch();
+
+    const [searchParameters] = useSearchParams();
+    const token = searchParameters.get('token');
+
+    const [isConfirmed, setIsConfirmed] = useState<boolean | undefined>();
+
+    const handleConfirmEmail = useCallback(async (): Promise<void> => {
+        if (token) {
+            const response = await dispatch(
+                authActionCreator.confirmEmail({
+                    emailConfirmToken: token,
+                }),
+            );
+
+            if (response.meta.requestStatus === DataStatus.FULFILLED) {
+                setIsConfirmed(true);
+            } else if (response.meta.requestStatus === DataStatus.REJECTED) {
+                setIsConfirmed(false);
+            }
+        }
+    }, [dispatch, token]);
+
+    useEffect(() => {
+        handleConfirmEmail()
+            .then(() => setIsConfirmed(true))
+            .catch(() => setIsConfirmed(false));
+    }, [dispatch, token, handleConfirmEmail]);
+
+    return (
+        <>
+            {isConfirmed === undefined && <div>Confirming...</div>}
+            {isConfirmed === true && (
+                <Navigate to={AppRoute.EMAIL_CONFIRMATION_SUCCESS} />
+            )}
+            {isConfirmed === false && (
+                <Navigate to={AppRoute.EMAIL_CONFIRMATION_FAIL} />
+            )}
+        </>
+    );
+};
+
+export { EmailConfirmationPage };
