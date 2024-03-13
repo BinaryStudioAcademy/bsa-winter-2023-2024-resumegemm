f8bece8f2d51ce6835792cfa6436dfc3ef0d7afb
Valentyn13 <valentyn13git@gmail.com>
2024-03-13 09:41:35 +0200

    rg-206: + Header to EditTemplatePage

--- frontend/src/bundles/edit-template/edit-template.tsx
+++ frontend/src/bundles/edit-template/edit-template.tsx
@@ -2,8 +2,16 @@ import clsx from 'clsx';
 import React, { type ChangeEvent, useCallback, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { Checkbox, RegularButton } from '../common/components/components';
+import {
+    Checkbox,
+    Header,
+    NavTabs,
+    RegularButton,
+} from '../common/components/components';
+import { headerItems } from '../common/components/layout/header/constants/header-items';
+import { UserProfile } from '../common/components/layout/header/user-profile/user-profile';
 import { ButtonSize, ButtonType, ButtonVariant } from '../common/enums/enums';
+import { getUserAvatar } from '../common/helpers/get-user-avatar';
 import { useAppDispatch, useAppSelector } from '../common/hooks/hooks';
 import editorStyles from '../cv-editor/components/online-editor/online-editor-handler.module.scss';
 import styles from '../resume-preview/components/resume-preview/styles.module.scss';
@@ -17,6 +25,8 @@ const EditTemplatePage: React.FC = () => {
     const dispatch = useAppDispatch();
     const template = useAppSelector((state) => state.editTemplate.template);
     const parameters = useParams<{ id: string }>();
+
+    const { user } = useAppSelector((state) => state.auth);
 
     useEffect(() => {
         if (!parameters.id) {
@@ -62,49 +72,55 @@ const EditTemplatePage: React.FC = () => {
     }, [dispatch]);
 
     return (
-        <section
-            className={clsx(
-                editorStyles.editor__section,
-                templateStyles.edit__section_wrapper,
-            )}
-        >
-            <nav
+        <>
+            <Header>
+                <NavTabs items={headerItems} />
+                <UserProfile image={getUserAvatar(user)} />
+            </Header>
+            <section
                 className={clsx(
-                    editorStyles.editor_sidebar__nav,
-                    templateStyles.editor_sidebar__nav,
+                    editorStyles.editor__section,
+                    templateStyles.edit__section_wrapper,
                 )}
             >
-                <ul className={editorStyles.editor_sidebar__list}>
-                    {templateBlockTitles.map((block) => (
-                        <li
-                            key={block}
-                            style={{ 'display': 'flex' }}
-                            className={editorStyles.editor_sidebar__item}
+                <nav
+                    className={clsx(
+                        editorStyles.editor_sidebar__nav,
+                        templateStyles.editor_sidebar__nav,
+                    )}
+                >
+                    <ul className={editorStyles.editor_sidebar__list}>
+                        {templateBlockTitles.map((block) => (
+                            <li
+                                key={block}
+                                style={{ 'display': 'flex' }}
+                                className={editorStyles.editor_sidebar__item}
+                            >
+                                <Checkbox
+                                    checked={isBlockEnabled(block)}
+                                    label=""
+                                    onChange={handleCheckboxChange}
+                                    name={block}
+                                />
+                                {block}
+                            </li>
+                        ))}
+                    </ul>
+                    <div className={styles.editor_output__block}>
+                        <RegularButton
+                            type={ButtonType.SUBMIT}
+                            size={ButtonSize.MEDIUM}
+                            variant={ButtonVariant.DEFAULT}
+                            onClick={handleSaveTemplate}
+                            className={clsx(templateStyles.output__button)}
                         >
-                            <Checkbox
-                                checked={isBlockEnabled(block)}
-                                label=""
-                                onChange={handleCheckboxChange}
-                                name={block}
-                            />
-                            {block}
-                        </li>
-                    ))}
-                </ul>
-                <div className={styles.editor_output__block}>
-                    <RegularButton
-                        type={ButtonType.SUBMIT}
-                        size={ButtonSize.MEDIUM}
-                        variant={ButtonVariant.DEFAULT}
-                        onClick={handleSaveTemplate}
-                        className={clsx(templateStyles.output__button)}
-                    >
-                        Save Tempalte
-                    </RegularButton>
-                </div>
-            </nav>
-            <TemplateEditor settings={templateSettings} />
-        </section>
+                            Save Tempalte
+                        </RegularButton>
+                    </div>
+                </nav>
+                <TemplateEditor settings={templateSettings} />
+            </section>
+        </>
     );
 };
 
