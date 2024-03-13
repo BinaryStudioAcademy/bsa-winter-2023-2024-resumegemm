fe829170399ea16f567125ae9d0de2a8d4ee01a0
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-03-13 14:28:59 +0100

    rg-138: + subscription status update event handler

--- backend/src/bundles/stripe-events/stripe-events.service.ts
+++ backend/src/bundles/stripe-events/stripe-events.service.ts
@@ -3,8 +3,6 @@ import Stripe from 'stripe';
 import { type IStripeEventsService } from '~/bundles/stripe-events/interfaces/stripe-events-service.interface.js';
 import { type IConfig } from '~/common/config/interfaces/config.interface.js';
 
-import { subscriptionRepository } from '../subscription/subscription.js';
-import { StripeSubscriptionEvents } from './enums/stripe-subscription-events.enum.js';
 import { type StripeEventDispatcher } from './handlers/stripe-event-dispatcher.js';
 import { type StripeEventsResponseDto } from './types/types.js';
 
@@ -35,54 +33,8 @@ class StripeEventsService implements IStripeEventsService {
         );
         await this.eventDispatcher.dispatch(event.type, event.data);
 
-        // switch (event.type) {
-        //     case StripePlanEvents.PLAN_CREATED: {
-        //         await this.handlePlanCreated(event.data);
-        //         break;
-        //     }
-        //     case StripePlanEvents.PLAN_DELETED: {
-        //         await this.handlePlanDeleted(event.data);
-        //         break;
-        //     }
-        //     case StripeSubscriptionEvents.SUBSCRIPTION_STATUS_UPDATED: {
-        //         await this.handleUpdateSubscriptionStatus(event.data);
-        //         break;
-        //     }
-        // }
-
         return { resolved: true };
     }
-    // private async handlePlanCreated(
-    //     data: Stripe.PlanCreatedEvent.Data,
-    // ): Promise<void> {
-    //     const plan: Stripe.Plan = data.object;
-
-    //     await this.subscriptionPlanRepository.create({
-    //         stripePlanId: plan.id,
-    //         stripeProductId: plan.product as string,
-    //     });
-    // }
-
-    // private async handlePlanDeleted(
-    //     data: Stripe.PlanDeletedEvent.Data,
-    // ): Promise<void> {
-    //     const plan: Stripe.Plan = data.object;
-
-    //     await this.subscriptionPlanRepository.delete({
-    //         stripePlanId: plan.id,
-    //     });
-    //     return {
-    //         resolved: true,
-    //     };
-    // }
-
-    // private async handleUpdateSubscriptionStatus(
-    //     data: Stripe.CustomerSubscriptionUpdatedEvent.Data,
-    // ): Promise<void> {
-    //     const subscription: Stripe.Subscription = data.object;
-    //     const { id, status } = subscription;
-    //     await subscriptionRepository.updateStatus(id, status);
-    // }
 }
 
 export { StripeEventsService };
