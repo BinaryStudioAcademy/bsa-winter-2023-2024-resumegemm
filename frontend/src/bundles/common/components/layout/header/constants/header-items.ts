fb9bd0a93b0db175f92e77546dc82984c2ba0fe5
Valentyn13 <valentyn13git@gmail.com>
2024-03-13 09:38:39 +0200

    rg-206: + headerItems constant

---
+++ frontend/src/bundles/common/components/layout/header/constants/header-items.ts
@@ -0,0 +1,8 @@
+import { AppRoute } from '~/bundles/common/enums/enums.js';
+
+const headerItems = [
+    { label: 'Home', path: AppRoute.HOME },
+    { label: 'Templates', path: AppRoute.TEMPLATES },
+];
+
+export { headerItems };
