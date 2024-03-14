5816cae5d6be1cba222d6b05c11b2a0926aacd75
Nazarii <hoolyworkacc@gmail.com>
2024-03-14 13:57:27 +0200

    Revert "rg-190: * collision detection to less buggy"
    This reverts commit a035c80e7e1383ae84909359b43144e9960a024b.

--- frontend/src/bundles/edit-template/components/template-editor/template-editor.tsx
+++ frontend/src/bundles/edit-template/components/template-editor/template-editor.tsx
@@ -2,12 +2,11 @@ import {
     type Active,
     type DragOverEvent,
     type DragStartEvent,
-    DndContext,
-    DragOverlay,
     MouseSensor,
     TouchSensor,
     useSensor,
 } from '@dnd-kit/core';
+import { closestCorners, DndContext, DragOverlay } from '@dnd-kit/core';
 
 import { useCallback, useState } from '~/bundles/common/hooks/hooks';
 
@@ -85,6 +84,7 @@ const TemplateEditor: React.FC<Properties> = ({
                 onDragStart={handleDragStart}
                 onDragEnd={handleDragEnd}
                 onDragOver={handleDragOver}
+                collisionDetection={closestCorners}
                 sensors={[mouseSensor, touchSensor]}
             >
                 <div
