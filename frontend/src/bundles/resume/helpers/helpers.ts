87d9aac829d75edb7b6f179794af7e0a45b58f67
Icerock10 <dp021190dms@gmail.com>
2024-03-14 18:48:04 +0200

    rg-168: + helper

--- frontend/src/bundles/resume/helpers/helpers.ts
+++ frontend/src/bundles/resume/helpers/helpers.ts
@@ -1,2 +1,3 @@
+export { updateSettingsBlocksFromInputs } from './update-settings-blocks-from-inputs.helper.js';
 export { updateTemplateSettings } from './update-template-settings.helper';
 export { updateTemplateSettingsBlocks } from './update-template-settings-blocks.helper.js';
