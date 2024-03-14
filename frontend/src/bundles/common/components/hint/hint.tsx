6984b98f5277ff2d1cc982ec01c3c7f2a3d3658c
RevolveStrm <revolvebattlerage@gmail.com>
2024-03-14 21:01:29 +0200

    rg-202: * used Icon component

--- frontend/src/bundles/common/components/hint/hint.tsx
+++ frontend/src/bundles/common/components/hint/hint.tsx
@@ -2,24 +2,22 @@ import clsx from 'clsx';
 import { type FC } from 'react';
 import { Tooltip as ReactTooltip } from 'react-tooltip';
 
-import checkMark from '~/assets/img/check.svg';
-import questionMark from '~/assets/img/question-circle.svg';
+import { Icon } from '~/bundles/common/components/components';
+import { IconName } from '~/bundles/common/enums/enums';
 import { type HintRow } from '~/bundles/common/types/types';
 
 import styles from './styles.module.scss';
 
 type Properties = {
     className?: string;
-    icon?: string | undefined;
     rows?: HintRow[];
 };
 
-const Hint: FC<Properties> = ({ className = '', icon, rows }) => {
+const Hint: FC<Properties> = ({ className = '', rows }) => {
     return (
         <div className={clsx(className, styles.hint__container)}>
-            <img
-                alt="hint"
-                src={icon ?? questionMark}
+            <Icon
+                name={IconName.QUESTION_CIRCLE}
                 className={styles.hint__anchor_container}
             />
             <ReactTooltip
@@ -29,9 +27,8 @@ const Hint: FC<Properties> = ({ className = '', icon, rows }) => {
                 {rows?.length &&
                     rows.map((row: HintRow) => (
                         <div key={row.text} className={styles.hint__row}>
-                            <img
-                                alt="hint_row"
-                                src={row.icon ?? checkMark}
+                            <Icon
+                                name={IconName.CHECK_CIRCLE}
                                 className={styles.hint__row_icon}
                             />
                             <span className={styles.hint__row_text}>
