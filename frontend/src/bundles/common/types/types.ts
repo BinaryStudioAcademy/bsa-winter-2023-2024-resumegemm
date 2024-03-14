6b044339358b7040a0ef0208df9756d21d7dff18
RevolveStrm <revolvebattlerage@gmail.com>
2024-03-14 12:45:12 +0200

    rg-202: * added hint row type

--- frontend/src/bundles/common/types/types.ts
+++ frontend/src/bundles/common/types/types.ts
@@ -1,6 +1,7 @@
 export { type AsyncThunkConfig } from './async-thunk-config.type.js';
 export { type CalendarDate } from './calendar/calendar-date.type.js';
 export { type CalendarMonth } from './calendar/calendar-month.type.js';
+export { type HintRow } from './hint/hint-row.type.js';
 export {
     type ModalHandlers,
     type ModalProperties,
