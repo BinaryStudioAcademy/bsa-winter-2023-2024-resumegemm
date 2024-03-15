052bb530dd61340ef0b8bdccd94fd66a4f50018e
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-03-15 19:01:03 +0200

    rg-221: * create resume limit modal

--- frontend/src/bundles/resume/components/create-resume-limit-modal/create-resume-limit-modal.tsx
+++ frontend/src/bundles/resume/components/create-resume-limit-modal/create-resume-limit-modal.tsx
@@ -1,3 +1,6 @@
+import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import resume from '~/assets/img/resume.svg';
 import { Icon, RegularButton } from '~/bundles/common/components/components';
 import { Modal } from '~/bundles/common/components/modal/modal';
@@ -8,82 +11,74 @@ import {
     IconName,
     ModalVariant,
 } from '~/bundles/common/enums/enums';
-import { useCallback, useState } from '~/bundles/common/hooks/hooks';
 
 import styles from './styles.module.scss';
 
-const CreateResumeLimitModal: React.FC = () => {
-    const [isOpen, setIsOpen] = useState(false);
+type CreateResumeLimitModalProperties = {
+    isOpen: boolean;
+    onClose: () => void;
+};
 
-    const handleOpen = useCallback((): void => {
-        setIsOpen(true);
-    }, []);
+const CreateResumeLimitModal: React.FC<CreateResumeLimitModalProperties> = ({
+    isOpen,
+    onClose,
+}) => {
+    const navigate = useNavigate();
 
-    const handleClose = useCallback((): void => {
-        setIsOpen(false);
-    }, []);
+    const handleClickUpgrade = useCallback((): void => {
+        navigate('/settings/subscription');
+    }, [navigate]);
 
     return (
-        <div>
-            <RegularButton
-                variant={ButtonVariant.GHOST}
-                size={ButtonSize.MEDIUM}
-                type={ButtonType.BUTTON}
-                onClick={handleOpen}
-            >
-                Open Modal
-            </RegularButton>
-            <Modal
-                title="You have reached the resume limit"
-                variant={ModalVariant.FORM}
-                isOpen={isOpen}
-                onClose={handleClose}
-            >
-                <div className={styles.modal__content_wrapper}>
-                    <Icon
-                        name={IconName.CLOSE_CROSS}
-                        color="grey"
-                        className={styles.modal__icon_close}
-                    />
-                    <div className={styles.modal__content_image}>
-                        <img src={resume} alt="Resume" />
-                    </div>
-                    <div>
-                        <p className={styles.modal__description}>
-                            You have reached the limit of resumes you can
-                            create. Please upgrade your plan to create more
-                            resumes.
-                        </p>
-                        <p>Also unlock:</p>
-                        <ul className={styles.modal__custom_list}>
-                            <li>download resume in pdf format</li>
-                            <li>other features</li>
-                        </ul>
-                        <div className={styles.modal__buttons}>
-                            <RegularButton
-                                className={styles.modal__upgrade_button}
-                                variant={ButtonVariant.GHOST}
-                                size={ButtonSize.MEDIUM}
-                                type={ButtonType.BUTTON}
-                                onClick={handleClose}
-                            >
-                                Upgrade now
-                            </RegularButton>
+        <Modal
+            title="You have reached the resume limit"
+            variant={ModalVariant.FORM}
+            isOpen={isOpen}
+            onClose={onClose}
+        >
+            <div className={styles.modal__content_wrapper}>
+                <Icon
+                    name={IconName.CLOSE_CROSS}
+                    color="grey"
+                    className={styles.modal__icon_close}
+                />
+                <div className={styles.modal__content_image}>
+                    <img src={resume} alt="Resume" />
+                </div>
+                <div>
+                    <p className={styles.modal__description}>
+                        You have reached the limit of resumes you can create.
+                        Please upgrade your plan to create more resumes.
+                    </p>
+                    <p>Also unlock:</p>
+                    <ul className={styles.modal__custom_list}>
+                        <li>download resume in pdf format</li>
+                        <li>other features</li>
+                    </ul>
+                    <div className={styles.modal__buttons}>
+                        <RegularButton
+                            className={styles.modal__upgrade_button}
+                            variant={ButtonVariant.GHOST}
+                            size={ButtonSize.MEDIUM}
+                            type={ButtonType.BUTTON}
+                            onClick={handleClickUpgrade}
+                        >
+                            Upgrade now
+                        </RegularButton>
 
-                            <RegularButton
-                                className={styles.modal__skip_button}
-                                variant={ButtonVariant.GHOST}
-                                size={ButtonSize.MEDIUM}
-                                type={ButtonType.BUTTON}
-                                onClick={handleClose}
-                            >
-                                Skip
-                            </RegularButton>
-                        </div>
+                        <RegularButton
+                            className={styles.modal__skip_button}
+                            variant={ButtonVariant.GHOST}
+                            size={ButtonSize.MEDIUM}
+                            type={ButtonType.BUTTON}
+                            onClick={onClose}
+                        >
+                            Skip
+                        </RegularButton>
                     </div>
                 </div>
-            </Modal>
-        </div>
+            </div>
+        </Modal>
     );
 };
 
