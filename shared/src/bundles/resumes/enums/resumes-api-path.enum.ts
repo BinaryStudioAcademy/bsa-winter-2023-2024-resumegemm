a0a90f0a47da96a758664210d96907f330eae30a
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-03-16 00:26:07 +0200

    rg-221: * resume api path

--- shared/src/bundles/resumes/enums/resumes-api-path.enum.ts
+++ shared/src/bundles/resumes/enums/resumes-api-path.enum.ts
@@ -6,6 +6,7 @@ const ResumesApiPath = {
     ID_SHARE: (id = ':id'): string => `/${id}/share`,
     SHARE_ID: (id = ':id'): string => `/share/${id}`,
     SHARE_ID_DETAILS: (id = ':id'): string => `/share/${id}/details`,
+    USER_ID_VIEWS: (userId = ':userId'): string => `/user/${userId}/views`,
 } as const;
 
 export { ResumesApiPath };
