7dc5d700e8542e93b220a811d42c1f130e60bbf4
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-03-15 19:02:55 +0200

    rg-221: * create resume button

--- frontend/src/bundles/home/components/home-top-section/components/create-resume-button/create-resume-button.tsx
+++ frontend/src/bundles/home/components/home-top-section/components/create-resume-button/create-resume-button.tsx
@@ -1,26 +1,67 @@
+import { useNavigate } from 'react-router-dom';
+
+import { AppRoute } from '~/bundles/common/enums/enums';
+import {
+    useAppSelector,
+    useCallback,
+    useState,
+} from '~/bundles/common/hooks/hooks';
+import { CreateResumeLimitModal } from '~/bundles/resume/components/create-resume-limit-modal/create-resume-limit-modal';
+
 import { PlusCircleIcon } from '../../../icons/plus-circle-icon';
 import { ResumeIcon } from '../../../icons/resume-icon';
 import styles from './styles.module.scss';
 
 const CreateResumeButton: React.FC = () => {
+    const { user } = useAppSelector((state) => state.auth);
+
+    const navigate = useNavigate();
+
+    const [showModal, setShowModal] = useState(false);
+
+    const handleClickCreateResume = useCallback((): void => {
+        if (user?.stripeId === null) {
+            setShowModal(true);
+        } else {
+            navigate(AppRoute.RESUME_CREATE);
+        }
+    }, [user, navigate]);
+
+    const handleClickClose = useCallback((): void => {
+        setShowModal(false);
+    }, []);
+
     return (
-        <div className={styles.container}>
-            <div className={styles.create_resume_button}>
-                <div className={styles.create_resume_button__icon}>
-                    <PlusCircleIcon
-                        className={
-                            styles.create_resume_button__icon__plus_circle
-                        }
-                    />
-                    <ResumeIcon
-                        className={styles.create_resume_button__icon__resume}
-                    />
+        <>
+            <button
+                className={styles.container}
+                onClick={handleClickCreateResume}
+            >
+                <div className={styles.create_resume_button}>
+                    <div className={styles.create_resume_button__icon}>
+                        <PlusCircleIcon
+                            className={
+                                styles.create_resume_button__icon__plus_circle
+                            }
+                        />
+                        <ResumeIcon
+                            className={
+                                styles.create_resume_button__icon__resume
+                            }
+                        />
+                    </div>
+                    <span className={styles.create_resume_button__text}>
+                        Create a resume
+                    </span>
                 </div>
-                <span className={styles.create_resume_button__text}>
-                    Create a resume
-                </span>
-            </div>
-        </div>
+            </button>
+            {showModal && (
+                <CreateResumeLimitModal
+                    isOpen={showModal}
+                    onClose={handleClickClose}
+                />
+            )}
+        </>
     );
 };
 
