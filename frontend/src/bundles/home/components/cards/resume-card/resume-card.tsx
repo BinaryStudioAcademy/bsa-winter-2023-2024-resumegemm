bd597942edabbbdcd6108d7791409266c921d1aa
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-03-16 14:39:20 +0200

    rg-221: * resume-car component

--- frontend/src/bundles/home/components/cards/resume-card/resume-card.tsx
+++ frontend/src/bundles/home/components/cards/resume-card/resume-card.tsx
@@ -1,23 +1,47 @@
+import { Icon } from '~/bundles/common/components/components';
+import { IconName, IconSize } from '~/bundles/common/enums/enums';
+
 import styles from './styles.module.scss';
 
 type Properties = {
     title: string;
     image: string;
     subtitle?: string;
+    resumeViews?: number;
 };
 
 const ResumeCard: React.FC<Properties> = ({
     title,
     image,
     subtitle,
+    resumeViews,
 }: Properties) => {
     return (
         <div className={styles.resume_card}>
             <img src={image} alt="Resume" className={styles.resume_card__img} />
             <div className={styles.resume_card__content}>
-                <span className={styles.resume_card__content_title}>
+                <div className={styles.resume_card__content_title}>
                     {title}
-                </span>
+                    {resumeViews !== undefined && (
+                        <div
+                            className={
+                                styles.resume_card__resume_views_container
+                            }
+                        >
+                            <Icon
+                                size={IconSize.SMALL}
+                                name={IconName.EYE_OPEN}
+                            />
+                            <span
+                                className={
+                                    styles.resume_card__resume_views_number
+                                }
+                            >
+                                {resumeViews}
+                            </span>
+                        </div>
+                    )}
+                </div>
                 {subtitle && (
                     <span className={styles.resume_card__content_text}>
                         {subtitle}
