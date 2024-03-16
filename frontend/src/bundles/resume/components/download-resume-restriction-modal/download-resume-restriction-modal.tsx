75bbc64711c8b4efee67b89b04f573b5b8c6bda1
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-03-16 23:08:44 +0200

    rg-221: * download resume restriction modal

--- frontend/src/bundles/resume/components/download-resume-restriction-modal/download-resume-restriction-modal.tsx
+++ frontend/src/bundles/resume/components/download-resume-restriction-modal/download-resume-restriction-modal.tsx
@@ -1,88 +1,77 @@
 import resume from '~/assets/img/resume.svg';
-import { Icon, RegularButton } from '~/bundles/common/components/components';
+import { RegularButton } from '~/bundles/common/components/components';
 import { Modal } from '~/bundles/common/components/modal/modal';
 import {
     ButtonSize,
     ButtonType,
     ButtonVariant,
-    IconName,
     ModalVariant,
 } from '~/bundles/common/enums/enums';
-import { useCallback, useState } from '~/bundles/common/hooks/hooks';
+import { useCallback, useNavigate } from '~/bundles/common/hooks/hooks';
 
 import styles from './styles.module.scss';
 
-const DownloadResumeRestrictionModal: React.FC = () => {
-    const [isOpen, setIsOpen] = useState(false);
+type Properties = {
+    isOpen: boolean;
+    onClose: () => void;
+};
 
-    const handleOpen = useCallback((): void => {
-        setIsOpen(true);
-    }, []);
+const DownloadResumeRestrictionModal: React.FC<Properties> = ({
+    isOpen,
+    onClose,
+}) => {
+    const navigate = useNavigate();
 
-    const handleClose = useCallback((): void => {
-        setIsOpen(false);
-    }, []);
+    const handleClickUpgrade = useCallback((): void => {
+        navigate('/settings/subscription');
+    }, [navigate]);
 
     return (
-        <div>
-            <RegularButton
-                variant={ButtonVariant.GHOST}
-                size={ButtonSize.MEDIUM}
-                type={ButtonType.BUTTON}
-                onClick={handleOpen}
-            >
-                Open Modal
-            </RegularButton>
-            <Modal
-                title="Upgrade to unlock Microsoft Word export"
-                variant={ModalVariant.FORM}
-                isOpen={isOpen}
-                onClose={handleClose}
-            >
-                <div className={styles.modal__content_wrapper}>
-                    <Icon
-                        name={IconName.CLOSE_CROSS}
-                        color="grey"
-                        className={styles.modal__icon_close}
-                    />
-                    <div className={styles.modal__content_image}>
-                        <img src={resume} alt="Resume" />
-                    </div>
-                    <div>
-                        <p className={styles.modal__description}>
-                            Only subscribed users can download their resume in
-                            pdf. Please upgrade your plan to unlock pdf export.
-                        </p>
-                        <p>Also unlock:</p>
-                        <ul className={styles.modal__custom_list}>
-                            <li>unlimit number of resume creation</li>
-                            <li>other features</li>
-                        </ul>
-                        <div className={styles.modal__buttons}>
-                            <RegularButton
-                                className={styles.modal__upgrade_button}
-                                variant={ButtonVariant.GHOST}
-                                size={ButtonSize.MEDIUM}
-                                type={ButtonType.BUTTON}
-                                onClick={handleClose}
-                            >
-                                Upgrade now
-                            </RegularButton>
+        <Modal
+            title="You are over the PDF download limit"
+            variant={ModalVariant.FORM}
+            isOpen={isOpen}
+            onClose={onClose}
+        >
+            <div className={styles.modal__content_wrapper}>
+                <div className={styles.modal__content_image}>
+                    <img src={resume} alt="Resume" />
+                </div>
+                <div>
+                    <p className={styles.modal__description}>
+                        Only one PDF download is available with the free plan.
+                        Upgrade your plan to download an unlimited number of
+                        resumes in PDF format.
+                    </p>
+                    <p>Also unlock:</p>
+                    <ul className={styles.modal__custom_list}>
+                        <li>unlimit number of resume creation</li>
+                        <li>other features</li>
+                    </ul>
+                    <div className={styles.modal__buttons}>
+                        <RegularButton
+                            className={styles.modal__upgrade_button}
+                            variant={ButtonVariant.GHOST}
+                            size={ButtonSize.MEDIUM}
+                            type={ButtonType.BUTTON}
+                            onClick={handleClickUpgrade}
+                        >
+                            Upgrade now
+                        </RegularButton>
 
-                            <RegularButton
-                                className={styles.modal__skip_button}
-                                variant={ButtonVariant.GHOST}
-                                size={ButtonSize.MEDIUM}
-                                type={ButtonType.BUTTON}
-                                onClick={handleClose}
-                            >
-                                Skip
-                            </RegularButton>
-                        </div>
+                        <RegularButton
+                            className={styles.modal__skip_button}
+                            variant={ButtonVariant.GHOST}
+                            size={ButtonSize.MEDIUM}
+                            type={ButtonType.BUTTON}
+                            onClick={onClose}
+                        >
+                            Skip
+                        </RegularButton>
                     </div>
                 </div>
-            </Modal>
-        </div>
+            </div>
+        </Modal>
     );
 };
 
