85442f10f9510eb0aa938de8a4d1749fa03b0d17
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-03-16 05:52:01 +0100

    rg-240: * fix env

--- backend/src/bundles/stripe-events/stripe-events.service.ts
+++ backend/src/bundles/stripe-events/stripe-events.service.ts
@@ -17,9 +17,7 @@ class StripeEventsService implements IStripeEventsService {
     ) {
         this.eventDispatcher = eventDispatcher;
         this.appConfig = config;
-        this.stripe = new Stripe(
-            this.appConfig.ENV.STRIPE.STRIPE_WEBHOOK_SECRET,
-        );
+        this.stripe = new Stripe(this.appConfig.ENV.STRIPE.STRIPE_SECRET_KEY);
     }
 
     public async handleEvent(
@@ -29,10 +27,9 @@ class StripeEventsService implements IStripeEventsService {
         const event: Stripe.Event = this.stripe.webhooks.constructEvent(
             rawBody,
             signature,
-            this.appConfig.ENV.STRIPE.STRIPE_SECRET_KEY,
+            this.appConfig.ENV.STRIPE.STRIPE_WEBHOOK_SECRET,
         );
         await this.eventDispatcher.dispatch(event.type, event.data);
-
         return { resolved: true };
     }
 }
