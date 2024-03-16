ff9c4520f5e4dcc4fcecc8fefe0b42fec1c3bc14
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-03-16 15:04:27 +0200

    rg-221: + resume actions, slice

---
+++ frontend/src/bundles/resume/store/actions.ts
@@ -0,0 +1,31 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+import { type AsyncThunkConfig } from '~/bundles/common/types/types.js';
+
+import {
+    type ResumeGetAllResponseDto,
+    type ResumeViewsCountResponseDto,
+} from '../types/types.js';
+import { name as sliceName } from './slice.js';
+
+const getAllResumesByUserId = createAsyncThunk<
+    ResumeGetAllResponseDto,
+    { userId: string },
+    AsyncThunkConfig
+>(`${sliceName}/getAllResumesByUserId`, (request, { extra }) => {
+    const { resumeApi } = extra;
+
+    return resumeApi.getAllByUserId(request);
+});
+
+const getViewsCountByUserId = createAsyncThunk<
+    ResumeViewsCountResponseDto[],
+    { userId: string },
+    AsyncThunkConfig
+>(`${sliceName}/getViewsCountByUserId`, (request, { extra }) => {
+    const { resumeApi } = extra;
+
+    return resumeApi.getViewsCount(request.userId);
+});
+
+export { getAllResumesByUserId, getViewsCountByUserId };
---
+++ frontend/src/bundles/resume/store/slice.ts
@@ -0,0 +1,35 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+import {
+    type ResumeGetItemResponseDto,
+    type ResumeViewsCountResponseDto,
+} from '../types/types.js';
+import { getAllResumesByUserId, getViewsCountByUserId } from './actions.js';
+
+type State = {
+    resumeId: string;
+    resumes: ResumeGetItemResponseDto[];
+    resumeViews: ResumeViewsCountResponseDto[];
+};
+
+const initialState: State = {
+    resumeId: '',
+    resumes: [],
+    resumeViews: [],
+};
+
+const { reducer, actions, name } = createSlice({
+    initialState,
+    name: 'resumes',
+    reducers: {},
+    extraReducers(builder) {
+        builder.addCase(getAllResumesByUserId.fulfilled, (state, action) => {
+            state.resumes = action.payload.resumes;
+        });
+        builder.addCase(getViewsCountByUserId.fulfilled, (state, action) => {
+            state.resumeViews = action.payload;
+        });
+    },
+});
+
+export { actions, name, reducer };
