cbfb003c6347cddff95860764e20615d609fe41a
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-03-16 15:11:23 +0100

    rg-219: + type

---
+++ shared/src/bundles/subscription/subscription.ts
@@ -0,0 +1,3 @@
+export { SubscriptionApiPath } from './enums/subscription-api-path.enum.js';
+export { type SubscriptionResponseDto } from './types/subscription-response-dto.js';
+export { type Subscription } from './types/types.js';
---
+++ shared/src/bundles/subscription/types/subscription-response-dto.ts
@@ -0,0 +1,13 @@
+type SubscriptionResponseDto = {
+    id: string;
+    status: string;
+    subscriptionId: string;
+    subscriptionPlanId: string;
+    userId: string;
+    startDate: string;
+    endDate: string;
+    createdAt: string;
+    updatedAt: string;
+};
+
+export { type SubscriptionResponseDto };
