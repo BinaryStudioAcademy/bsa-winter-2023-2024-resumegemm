25730371bbd6378aba94f207f726fb146d6a43e2
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-03-16 14:44:00 +0200

    rg-221: + resume-list-preview component

---
+++ frontend/src/bundles/home/components/resume-section/components/resume-list-preview.tsx
@@ -0,0 +1,53 @@
+import { NavLink } from 'react-router-dom';
+
+import {
+    useAppDispatch,
+    useAppSelector,
+    useEffect,
+} from '~/bundles/common/hooks/hooks';
+import { getViewsCountByUserId } from '~/bundles/resume/store/actions';
+import { ToastType } from '~/bundles/toast/enums/show-toast-types.enum';
+import { showToast } from '~/bundles/toast/helpers/show-toast';
+
+import { ResumeCard } from '../../components';
+
+const ResumeListPreview: React.FC = () => {
+    const dispatch = useAppDispatch();
+
+    const authUser = useAppSelector(({ auth }) => auth.user);
+
+    const resumeViews = useAppSelector(({ resumes }) => resumes.resumeViews);
+
+    useEffect(() => {
+        if (authUser) {
+            dispatch(getViewsCountByUserId({ userId: authUser.id })).catch(
+                (error: Error) => {
+                    showToast(error.message, ToastType.ERROR);
+                },
+            );
+        }
+    }, [authUser, dispatch]);
+
+    return (
+        <>
+            {resumeViews.map((resume) => {
+                return (
+                    <NavLink
+                        key={resume.resumeId}
+                        to={`/resumes/${resume.resumeId}`}
+                    >
+                        <ResumeCard
+                            key={resume.resumeId}
+                            title={resume.title}
+                            image={resume.image}
+                            subtitle={`Updated - ${resume.updatedAt}`}
+                            resumeViews={resume.views}
+                        />
+                    </NavLink>
+                );
+            })}
+        </>
+    );
+};
+
+export { ResumeListPreview };
