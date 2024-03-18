9e934f1c54c408ce76908fa280a839a032d822e4
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-03-18 10:15:51 +0100

    rg-241: + subscription status enums

--- shared/src/bundles/subscription/enums/enums.ts
+++ shared/src/bundles/subscription/enums/enums.ts
@@ -1,2 +1,3 @@
 export { SubscriptionApiPath } from './subscription-api-path.enum.js';
 export { SubscriptionErrorMessage } from './subscription-error-message.enum.js';
+export { SubscriptionStatus } from './subscription-status.js';
---
+++ shared/src/bundles/subscription/enums/subscription-status.ts
@@ -0,0 +1,11 @@
+enum SubscriptionStatus {
+    ACTIVE = 'active',
+    CANCELED = 'canceled',
+    TRIALING = 'trialing',
+    PAST_DUE = 'past_due',
+    INCOMPLETE = 'incomplete',
+    INCOMPLETE_EXPIRED = 'incomplete_expired',
+    UNPAID = 'unpaid',
+}
+
+export { SubscriptionStatus };
