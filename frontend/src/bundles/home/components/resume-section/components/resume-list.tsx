d6bc9e34f005bff42012cbaaf4c4df4b1db0200b
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-03-18 21:16:42 +0200

    rg-192: * resume-list

--- frontend/src/bundles/home/components/resume-section/components/resume-list.tsx
+++ frontend/src/bundles/home/components/resume-section/components/resume-list.tsx
@@ -1,52 +1,40 @@
 import { NavLink } from 'react-router-dom';
 
-import {
-    useAppDispatch,
-    useAppSelector,
-    useEffect,
-    useResumes,
-} from '~/bundles/common/hooks/hooks';
-import { actions as resumeActions } from '~/bundles/resume/store/resume.store';
-import { ToastType } from '~/bundles/toast/enums/show-toast-types.enum';
-import { showToast } from '~/bundles/toast/helpers/show-toast';
+import { useResumes } from '~/bundles/common/hooks/hooks';
 
 import { ResumeCard } from '../../components';
 
 const ResumeList: React.FC = () => {
-    const dispatch = useAppDispatch();
-    const { deleteResume } = useResumes();
+    const { resumes, resumeViews, deleteResume } = useResumes();
 
-    const { authUser, resumeViews } = useAppSelector(({ auth, resumes }) => ({
-        authUser: auth.user,
-        resumeViews: resumes.resumeViews,
-    }));
-
-    useEffect(() => {
-        if (authUser) {
-            dispatch(resumeActions.getViewsCountByUserId()).catch(
-                (error: Error) => {
-                    showToast(error.message, ToastType.ERROR);
-                },
-            );
-        }
-    }, [authUser, dispatch]);
+    const resumeDataToRender = resumeViews.map((resumeWithViewsCount) => {
+        const authUserResumes = resumes.find(
+            (resume) => resume.id === resumeWithViewsCount.resumeId,
+        );
+        return {
+            ...authUserResumes,
+            ...resumeWithViewsCount,
+        };
+    });
 
     return (
         <>
-            {resumeViews.map(({ resumeId, title, image, updatedAt, views }) => {
-                return (
-                    <NavLink key={resumeId} to={`/resumes/${resumeId}`}>
-                        <ResumeCard
-                            key={resumeId}
-                            title={title}
-                            image={image}
-                            subtitle={`Updated - ${updatedAt}`}
-                            viewedResume={views}
-                            onDelete={deleteResume}
-                        />
-                    </NavLink>
-                );
-            })}
+            {resumeDataToRender.map(
+                ({ id, image, updatedAt, views, title }) => {
+                    return (
+                        <NavLink key={id} to={`/resumes/${id}`}>
+                            <ResumeCard
+                                key={id}
+                                title={title}
+                                image={image}
+                                subtitle={`Updated - ${updatedAt}`}
+                                viewedResume={views}
+                                onDelete={deleteResume}
+                            />
+                        </NavLink>
+                    );
+                },
+            )}
         </>
     );
 };
