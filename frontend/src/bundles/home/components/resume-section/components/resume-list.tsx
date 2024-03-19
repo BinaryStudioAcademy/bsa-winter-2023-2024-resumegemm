9bec2e6aad273ecb33a53bcc3c951cf23c6506c8
Serhii Yefimov <sergii.iefimov@gmail.com>
2024-03-19 20:59:01 +0200

    rg-192: * resume-list component

--- frontend/src/bundles/home/components/resume-section/components/resume-list.tsx
+++ frontend/src/bundles/home/components/resume-section/components/resume-list.tsx
@@ -1,40 +1,65 @@
 import { NavLink } from 'react-router-dom';
 
-import { useResumes } from '~/bundles/common/hooks/hooks';
+import {
+    useAppDispatch,
+    useAppSelector,
+    useCallback,
+    useEffect,
+} from '~/bundles/common/hooks/hooks';
+import { actions as resumeActions } from '~/bundles/resume/store/resume.store';
+import { CommonMessage } from '~/bundles/toast/enums/common-message.enum';
+import { ToastType } from '~/bundles/toast/enums/show-toast-types.enum';
+import { showToast } from '~/bundles/toast/helpers/show-toast';
 
 import { ResumeCard } from '../../components';
 
 const ResumeList: React.FC = () => {
-    const { resumes, resumeViews, deleteResume } = useResumes();
+    const dispatch = useAppDispatch();
 
-    const resumeDataToRender = resumeViews.map((resumeWithViewsCount) => {
-        const authUserResumes = resumes.find(
-            (resume) => resume.id === resumeWithViewsCount.resumeId,
+    const { resumeViews } = useAppSelector(({ resumes }) => ({
+        resumeViews: resumes.resumeViews,
+    }));
+
+    const deleteResume = useCallback(
+        (resumeId: string) => {
+            void dispatch(resumeActions.deleteResume(resumeId))
+                .unwrap()
+                .then(() =>
+                    showToast(
+                        CommonMessage.SUCCESS_DELETE_RESUME,
+                        ToastType.INFO,
+                        {
+                            position: 'top-right',
+                        },
+                    ),
+                );
+        },
+        [dispatch],
+    );
+
+    useEffect(() => {
+        dispatch(resumeActions.getViewsCountByUserId()).catch(
+            (error: Error) => {
+                showToast(error.message, ToastType.ERROR);
+            },
         );
-        return {
-            ...authUserResumes,
-            ...resumeWithViewsCount,
-        };
-    });
+    }, [dispatch]);
 
     return (
         <>
-            {resumeDataToRender.map(
-                ({ id, image, updatedAt, views, title }) => {
-                    return (
-                        <NavLink key={id} to={`/resumes/${id}`}>
-                            <ResumeCard
-                                key={id}
-                                title={title}
-                                image={image}
-                                subtitle={`Updated - ${updatedAt}`}
-                                viewedResume={views}
-                                onDelete={deleteResume}
-                            />
-                        </NavLink>
-                    );
-                },
-            )}
+            {resumeViews.map(({ resumeId, image, updatedAt, views, title }) => {
+                return (
+                    <NavLink key={resumeId} to={`/resumes/${resumeId}`}>
+                        <ResumeCard
+                            title={title}
+                            image={image ?? ''}
+                            subtitle={`Updated - ${updatedAt}`}
+                            viewedResume={views}
+                            onDelete={deleteResume}
+                        />
+                    </NavLink>
+                );
+            })}
         </>
     );
 };
