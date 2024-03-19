c803e084ffda0085f666eb9b56a30449b5fe2eda
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-03-19 21:28:24 +0100

    rg-242: * template action

--- frontend/src/bundles/templates-page/store/actions.ts
+++ frontend/src/bundles/templates-page/store/actions.ts
@@ -1,19 +1,20 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { type TemplateGetAllItemResponseDto } from 'shared/build/index.js';
+import {
+    type FindAllOptions,
+    type TemplateGetAllItemResponseDto,
+} from 'shared/build/index.js';
 
 import { type AsyncThunkConfig } from '~/bundles/common/types/types.js';
 
-import { type getAllTemplatesQuery } from '../types/types.js';
-
 const loadAllTemplates = createAsyncThunk<
     TemplateGetAllItemResponseDto[],
-    getAllTemplatesQuery | undefined,
+    FindAllOptions | undefined,
     AsyncThunkConfig
->('templates2/load-all', async (options = {}, { extra }) => {
+>('templates2/load-all', async (options, { extra }) => {
     const { templateApi } = extra;
     const query = {
-        direction: options.direction,
-        filterByName: options.filterByName,
+        direction: options?.direction ?? 'desc',
+        name: options?.name ?? '',
     };
 
     const { items } = await templateApi.getAll(query);
