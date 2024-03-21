4ce04f06f9cf6e8c6ae90ef4bb12691359ae0fdc
ValentinaTenus <120641395+ValentinaTenus@users.noreply.github.com>
2024-03-22 00:33:47 +0100

    rg-300: + helpers, types

--- backend/src/bundles/recently-viewed/types/types.ts
+++ backend/src/bundles/recently-viewed/types/types.ts
@@ -1,9 +1,12 @@
+export { type getResumesViewedByUserQueryResult } from './get-resumes-viewed-by-user-query.type';
 export { type IRecentlyViewedController } from './recently-viewed.controller.type';
 export { type IRecentlyViewedRepository } from './recently-viewed.repository.type';
 export { type IRecentlyViewedService } from './recently-viewed.service.type';
 export { type RecentlyViewedResumesQueryResult } from './recently-viewed-resumes-query-result.type';
 export {
+    type FindAllOptions,
     type RecentlyViewed,
+    type RecentlyViewedQuery,
     type RecentlyViewedRequestDto,
     type RecentlyViewedResponseDto,
     type RecentlyViewedResumesResponseDto,
